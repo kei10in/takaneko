7453139b9b28f5beaf4edb95b11ba0e19a61f020
Keisuke Ijuin <kei10in@gmail.com>
2024-07-17 08:50:46 +0900

    Use crlf for windows

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,7 @@
  * @type {import("prettier").Config}
  */
 const config = {
+  endOfLine: "auto",
   plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
   printWidth: 100,
 };
