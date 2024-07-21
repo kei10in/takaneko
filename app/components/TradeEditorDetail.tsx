5c2be668e80ad9a3a53b0b66021cc8f4da4d75a6
Keisuke Ijuin <kei10in@gmail.com>
2024-07-21 23:00:42 +0900

    Add image caption

--- app/components/TradeEditorDetail.tsx
+++ app/components/TradeEditorDetail.tsx
@@ -18,6 +18,8 @@ export const TradeEditorDetail: React.FC<Props> = (props: Props) => {
   const { productImage, tradeDescriptions, index, onClickPrev, onClickNext, onChangeTradeState } =
     props;
 
+  const photos = productImage.photos;
+  const selPhoto = photos[index];
   const positions = productImage.positions;
   const selPosition = positions[index];
   const tradeDescription = tradeDescriptions.find((td) => td.id == selPosition.id);
@@ -32,8 +34,9 @@ export const TradeEditorDetail: React.FC<Props> = (props: Props) => {
   const scale = width / selPosition.width;
 
   return (
-    <div className="select-none">
-      <div className="flex w-full items-stretch justify-center">
+    <div>
+      {/* Image and image selector */}
+      <div className="flex w-full select-none items-stretch justify-center">
         <div className="flex-none">
           <button
             className="group h-full p-2 hover:bg-gray-100"
@@ -78,74 +81,93 @@ export const TradeEditorDetail: React.FC<Props> = (props: Props) => {
         </div>
       </div>
 
-      <div className="mt-4 flex items-center justify-center gap-0.5">
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "none" }}
-          onClick={handleClickTradeState}
-        >
-          <div className="flex h-10 w-10 items-center justify-center">
-            <TbCircleOff className="h-8 w-8 text-gray-400" />
+      {/* Caption */}
+      <div className="mx-auto mt-4 flex w-60 items-center gap-4">
+        <div className="flex h-14 w-14 items-center justify-center rounded-full bg-gray-400">
+          <div>
+            <p className="text-[0.625rem] leading-none text-white">No.</p>
+            <p className="text-lg leading-none text-white">
+              {selPhoto.id.toString().padStart(3, "0")}
+            </p>
           </div>
-        </TradeStateButton>
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "want" }}
-          onClick={handleClickTradeState}
-        >
-          <img src="/求.svg" alt="求" className="h-10 w-10" />
-        </TradeStateButton>
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "have" }}
-          onClick={handleClickTradeState}
-        >
-          <img src="/譲.svg" alt="譲" className="h-10 w-10" />
-        </TradeStateButton>
+        </div>
+        <div>
+          <p className="text-xl">{selPhoto.name}</p>
+          <p className="text-md text-gray-500">{selPhoto.description}</p>
+        </div>
       </div>
-      <div className="mt-0.5 flex items-center justify-center gap-0.5">
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "have", count: 1 }}
-          onClick={handleClickTradeState}
-        >
-          <img src="/1.svg" alt="1" className="h-10 w-10" />
-        </TradeStateButton>
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "have", count: 2 }}
-          onClick={handleClickTradeState}
-        >
-          <img src="/2.svg" alt="2" className="h-10 w-10" />
-        </TradeStateButton>
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "have", count: 3 }}
-          onClick={handleClickTradeState}
-        >
-          <img src="/3.svg" alt="3" className="h-10 w-10" />
-        </TradeStateButton>
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "have", count: 4 }}
-          onClick={handleClickTradeState}
-        >
-          <img src="/4.svg" alt="4" className="h-10 w-10" />
-        </TradeStateButton>
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "have", count: 5 }}
-          onClick={handleClickTradeState}
-        >
-          <img src="/5.svg" alt="5" className="h-10 w-10" />
-        </TradeStateButton>
-        <TradeStateButton
-          value={tradeState}
-          forValue={{ tag: "have", count: 6 }}
-          onClick={handleClickTradeState}
-        >
-          <img src="/6.svg" alt="6" className="h-10 w-10" />
-        </TradeStateButton>
+
+      {/* Trade state selector */}
+      <div className="select-none">
+        <div className="mt-4 flex items-center justify-center gap-0.5">
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "none" }}
+            onClick={handleClickTradeState}
+          >
+            <div className="flex h-10 w-10 items-center justify-center">
+              <TbCircleOff className="h-8 w-8 text-gray-400" />
+            </div>
+          </TradeStateButton>
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "want" }}
+            onClick={handleClickTradeState}
+          >
+            <img src="/求.svg" alt="求" className="h-10 w-10" />
+          </TradeStateButton>
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "have" }}
+            onClick={handleClickTradeState}
+          >
+            <img src="/譲.svg" alt="譲" className="h-10 w-10" />
+          </TradeStateButton>
+        </div>
+        <div className="mt-0.5 flex items-center justify-center gap-0.5">
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "have", count: 1 }}
+            onClick={handleClickTradeState}
+          >
+            <img src="/1.svg" alt="1" className="h-10 w-10" />
+          </TradeStateButton>
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "have", count: 2 }}
+            onClick={handleClickTradeState}
+          >
+            <img src="/2.svg" alt="2" className="h-10 w-10" />
+          </TradeStateButton>
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "have", count: 3 }}
+            onClick={handleClickTradeState}
+          >
+            <img src="/3.svg" alt="3" className="h-10 w-10" />
+          </TradeStateButton>
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "have", count: 4 }}
+            onClick={handleClickTradeState}
+          >
+            <img src="/4.svg" alt="4" className="h-10 w-10" />
+          </TradeStateButton>
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "have", count: 5 }}
+            onClick={handleClickTradeState}
+          >
+            <img src="/5.svg" alt="5" className="h-10 w-10" />
+          </TradeStateButton>
+          <TradeStateButton
+            value={tradeState}
+            forValue={{ tag: "have", count: 6 }}
+            onClick={handleClickTradeState}
+          >
+            <img src="/6.svg" alt="6" className="h-10 w-10" />
+          </TradeStateButton>
+        </div>
       </div>
     </div>
   );
