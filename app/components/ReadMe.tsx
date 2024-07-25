8e3e4dd34c725a1de41c58c525fa9aa0d68e9c40
Keisuke Ijuin <kei10in@gmail.com>
2024-07-26 08:41:59 +0900

    Update Usage

--- app/components/ReadMe.tsx
+++ app/components/ReadMe.tsx
@@ -8,10 +8,10 @@ export const ReadMe: React.FC = () => {
 
       <h2 className="text-xl font-bold">使い方:</h2>
       <ol className="mx-4 list-outside list-decimal pl-6">
-        <li>メニューから選ぶ。</li>
-        <li>写真を選ぶ。</li>
-        <li className="">求 / 譲 / 1 / 2 / 3 / 4 / 5 / 6 から選ぶ。</li>
-        <li>トレード用画像を表示して保存する。</li>
+        <li>右上のメニューから生写真・ミニフォトの種類を選択します。</li>
+        <li>トレードしたい写真をタップします。。</li>
+        <li className="">求 / 譲 / 1 / 2 / 3 / 4 / 5 / 6 から選択します。</li>
+        <li>トレード用画像を表示して保存します。</li>
       </ol>
     </div>
   );
