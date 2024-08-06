a4b34513cebd6adb8f85116c1cf9ff0c231ff3cf
Keisuke Ijuin <kei10in@gmail.com>
2024-08-06 21:57:22 +0900

    Implement stamp positioning for square items

--- app/features/trade/stampPosition.ts
+++ app/features/trade/stampPosition.ts
@@ -11,5 +11,18 @@ export const stampPosition = (target: Xywh): Xywh => {
   const x = target.x + target.width / 2 - width / 2;
   const y = target.y + target.height - height;
 
+  if (target.height < height * 1.75) {
+    return stampPositionForSquare(target);
+  }
+
+  return { x, y, width, height };
+};
+
+const stampPositionForSquare = (target: Xywh): Xywh => {
+  const width = target.width / 2.25;
+  const height = width;
+  const x = target.x + target.width - width;
+  const y = target.y + target.height - height;
+
   return { x, y, width, height };
 };
