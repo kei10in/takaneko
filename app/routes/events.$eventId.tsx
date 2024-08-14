4f39d9472af4e5ce1c3d6b53f5178a68fb45faac
Keisuke Ijuin <kei10in@gmail.com>
2024-08-14 21:05:37 +0900

    Update per event page

--- app/routes/events.$eventId.tsx
+++ app/routes/events.$eventId.tsx
@@ -1,5 +1,11 @@
 import type { MetaFunction } from "@remix-run/node";
-import { unstable_defineClientLoader as defineClientLoader, useLoaderData } from "@remix-run/react";
+import {
+  unstable_defineClientLoader as defineClientLoader,
+  Link,
+  useLoaderData,
+} from "@remix-run/react";
+import clsx from "clsx";
+import { HiArrowTopRightOnSquare, HiCalendar, HiMapPin } from "react-icons/hi2";
 import { SITE_TITLE } from "~/constants";
 import { EventContent, loadEventContent } from "~/features/events/events";
 
@@ -30,11 +36,46 @@ export const clientLoader = defineClientLoader(async ({ params }): Promise<Event
 
 export default function EventPage() {
   const event = useLoaderData<typeof clientLoader>();
-  const { Content } = event;
+  const { meta, Content } = event;
 
   return (
-    <div className="container prose mx-auto">
-      <Content />
+    <div className="container mx-auto">
+      <div className="space-y-2">
+        {meta.image && (
+          <div>
+            <img src={meta.image} alt="アイキャッチ" className="w-full" />
+          </div>
+        )}
+        <div className="mt-4 px-4 text-lg font-bold">{meta.summary}</div>
+        <div className="flex items-center gap-1 px-4">
+          <HiCalendar />
+          <p>{meta.date}</p>
+        </div>
+        {meta.location && (
+          <Link
+            to={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(meta.location)}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="flex items-center gap-1 px-4">
+              <HiMapPin />
+              <div>{meta.location}</div>
+              <HiArrowTopRightOnSquare />
+            </div>
+          </Link>
+        )}
+      </div>
+      <article
+        className={clsx(
+          "prose mt-4 px-4 py-4",
+          "prose-h1:my-2 prose-h1:text-center prose-h1:text-lg",
+          "prose-h2:my-2 prose-h2:text-lg",
+          "prose-p:text-base",
+          "prose-li:my-0 prose-li:text-base",
+        )}
+      >
+        <Content />
+      </article>
     </div>
   );
 }
