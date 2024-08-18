6e5dbc4432e9a949b2b517829db038ef21ad6ddc
Keisuke Ijuin <kei10in@gmail.com>
2024-08-19 00:58:19 +0900

    色を追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,23 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nadeshiko: {
+          50: "#fffcfd",
+          100: "#fff7fa",
+          200: "#fff0f5",
+          300: "#fddce6", // FC で使われている色
+          400: "#fccada",
+          500: "#fcb9ce", // 公式サイトで使われている色
+          600: "#fa9dbb",
+          700: "#f783aa",
+          800: "#f26894",
+          900: "#ed4f81", // FC で使われている色
+          950: "#d63668",
+        },
+      },
+    },
   },
   plugins: [typography()],
 } satisfies Config;
