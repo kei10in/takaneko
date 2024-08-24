e9f97e1a097b7945059f41043638904d4d734644
Keisuke Ijuin <kei10in@gmail.com>
2024-08-24 16:02:58 +0900

    Update second top bar

--- app/routes/_schedule.tsx
+++ app/routes/_schedule.tsx
@@ -15,7 +15,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div>
-      <div className="sticky top-0 z-10 h-12 w-full border-b border-gray-300 bg-white bg-opacity-90 backdrop-blur lg:top-[var(--header-height)]">
+      <div className="sticky top-0 z-10 h-12 w-full border-b border-nadeshiko-200 bg-nadeshiko-200 bg-opacity-90 backdrop-blur lg:top-[var(--header-height)]">
         <div className="container mx-auto h-full">
           <div className="mx-4 flex h-full items-center justify-between">
             <p className="text-base font-bold text-gray-600">
