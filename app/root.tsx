e9f2d2a9d54f1b2f4b88ec6cb393dc7a4cf4f41e
Keisuke Ijuin <kei10in@gmail.com>
2024-09-01 17:57:40 +0900

    スケジュールで日付を選んだときのフィードバックを追加

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   const close = () => setShowMenu(false);
 
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
