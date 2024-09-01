efcbae8c205a66543fd53040ed36cf2badfd169e
Keisuke Ijuin <kei10in@gmail.com>
2024-09-01 20:05:10 +0900

    ヒーローを更新

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import { Link, MetaFunction } from "@remix-run/react";
+import clsx from "clsx";
 import { HiArrowsRightLeft, HiCalendar } from "react-icons/hi2";
 import { SITE_TITLE } from "~/constants";
 
@@ -17,21 +18,42 @@ export default function Index() {
   return (
     <div>
       <div className="container mx-auto">
-        <div className="relative mx-auto w-fit">
+        <div className="relative mx-auto w-fit lg:static lg:flex lg:w-full lg:max-w-5xl lg:px-4">
           <img
-            className="w-full min-w-96 max-w-md md:max-w-3xl"
+            className="max-h-96 w-full min-w-96 lg:h-96 lg:w-auto lg:max-w-none lg:flex-none lg:object-contain"
             src="/takaneko/hero.jpg"
             alt="ヒーロー画像"
           />
-          <div className="absolute bottom-0 w-full bg-nadeshiko-100 bg-opacity-90 px-6 py-4 md:px-10 md:py-8">
-            <h1 className="text-right font-serif text-2xl font-bold leading-tight text-gray-500 md:text-5xl">
+          <div
+            className={clsx(
+              "absolute bottom-0 w-full bg-nadeshiko-100 bg-opacity-90 px-6 py-4",
+              "lg:static lg:flex-1 lg:px-10 lg:py-8",
+            )}
+          >
+            <h1 className="text-right font-serif text-2xl font-bold leading-tight text-gray-500 lg:text-5xl">
               <span className="text-nadeshiko-900">高嶺のなでしこの</span>
               <br />
               ファンサイト
               <span className="text-nadeshiko-900"></span>
             </h1>
+
+            <div className="mt-2 flex select-none justify-end gap-1 text-sm font-semibold">
+              <Link to="/trade">
+                <div className="flex h-7 items-center gap-1 rounded-md bg-nadeshiko-900 px-2 text-white">
+                  <HiArrowsRightLeft className="h-4 w-4" />
+                  <div>トレード画像つくるやつ</div>
+                </div>
+              </Link>
+              <Link to="/calendar">
+                <div className="flex h-7 w-full items-center gap-1 rounded-md bg-nadeshiko-900 px-4 text-nadeshiko-100">
+                  <HiCalendar className="h-4 w-4" />
+                  <div>スケジュール</div>
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
+
         <section className="mx-auto mt-4 w-full max-w-md pb-12">
           <h2 className="mx-4 text-lg font-semibold text-gray-500">コンテンツ</h2>
           <ul className="mx-4 my-4 space-y-2">
