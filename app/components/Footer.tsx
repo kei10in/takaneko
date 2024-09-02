ab33570f851f5f147f6c2764a98cf535f4c61da9
Keisuke Ijuin <kei10in@gmail.com>
2024-09-02 23:13:57 +0900

    フッターを更新

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Link } from "@remix-run/react";
 import clsx from "clsx";
-import { FaGithub, FaXTwitter } from "react-icons/fa6";
+import { FaGithub, FaInstagram, FaTiktok, FaXTwitter, FaYoutube } from "react-icons/fa6";
 import { SITE_TITLE } from "~/constants";
 
 interface Props {
@@ -12,22 +12,22 @@ export const Footer: React.FC<Props> = (props: Props) => {
 
   return (
     <footer className={clsx(className, "bg-gray-100")}>
-      <div className={"container mx-auto bg-gray-100 text-sm"}>
+      <div className={"container mx-auto bg-gray-100 text-sm text-gray-600"}>
         <div className="mx-4 space-y-8 py-8">
-          <div className="space-y-2">
-            <h2 className="flex items-center text-base font-bold">
-              <img src="/icon.svg" alt="ロゴ" width={36} />
-              <p>{SITE_TITLE}</p>
-            </h2>
-            <p>「{SITE_TITLE}」は非公式のファンコンテンツです。</p>
-            <p>使用されている高嶺のなでしこの画像は INCS・TP に帰属します。©INCS・TP</p>
-            <p>使用されているイベントの画像は各イベントの主催者などの第三者に帰属します。</p>
-          </div>
+          <div className="grid gap-8 lg:grid-cols-5">
+            <section className="col-span-2 space-y-2 pb-4">
+              <h3 className="flex items-center text-base font-bold">
+                <img src="/icon.svg" alt="ロゴ" width={36} />
+                <p>{SITE_TITLE}</p>
+              </h3>
+              <p>「{SITE_TITLE}」は非公式のファンコンテンツです。</p>
+              <p>使用されている高嶺のなでしこの画像は INCS・TP に帰属します。©INCS・TP</p>
+              <p>使用されているイベントの画像は各イベントの主催者などの第三者に帰属します。</p>
+            </section>
 
-          <div className="space-y-4">
-            <section>
-              <h3 className="font-bold">コンテンツ</h3>
-              <ul className="list-inside list-disc px-2 marker:text-gray-400">
+            <section className="space-y-2">
+              <h3 className="font-semibold">コンテンツ</h3>
+              <ul className="space-y-2">
                 <li>
                   <Link to="/trade">トレード画像つくるやつ</Link>
                 </li>
@@ -36,9 +36,10 @@ export const Footer: React.FC<Props> = (props: Props) => {
                 </li>
               </ul>
             </section>
-            <section>
-              <h3 className="font-bold">About</h3>
-              <ul className="list-inside list-disc px-2 marker:text-gray-400">
+
+            <section className="space-y-2">
+              <h3 className="font-semibold">About</h3>
+              <ul className="space-y-2">
                 <li>
                   <Link to="/releases">リリース ノート</Link>
                 </li>
@@ -47,31 +48,78 @@ export const Footer: React.FC<Props> = (props: Props) => {
                 </li>
               </ul>
             </section>
+
+            <section className="space-y-2">
+              <h3 className="font-semibold">高嶺のなでしこ公式</h3>
+              <ul className="space-y-2">
+                <li>
+                  <Link to="https://takanenonadeshiko.jp/" target="_blank" rel="noreferrer">
+                    オフィシャル ウェブサイト
+                  </Link>
+                </li>
+                <li>
+                  <Link to="https://takanekofc.com" target="_blank" rel="noreferrer">
+                    オフィシャル ファン クラブ
+                  </Link>
+                </li>
+                <li>
+                  <Link to="https://takanenonadeshiko-ec.com/" target="_blank" rel="noreferrer">
+                    オフィシャル ショップ
+                  </Link>
+                </li>
+              </ul>
+
+              <ul className="flex gap-1.5 py-1">
+                <li>
+                  <Link to="https://www.youtube.com/@official6743" target="_blank" rel="noreferrer">
+                    <FaYoutube className="h-4 w-4" />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="https://x.com/takanenofficial" target="_blank" rel="noreferrer">
+                    <FaXTwitter className="h-4 w-4" />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="https://www.instagram.com/takanenofficial/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaInstagram className="h-4 w-4" />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="https://www.tiktok.com/@takanenofficial"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaTiktok className="h-4 w-4" />
+                  </Link>
+                </li>
+              </ul>
+            </section>
           </div>
 
-          <div className="flex items-center gap-4">
+          <div className="flex gap-2">
             <div className="flex-1">MIT © 2024</div>
-
-            <div className="flex-0">
-              <Link
-                className="inline-block"
-                to="https://twitter.com/takanekofan"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaXTwitter className="h-6 w-6" />
-              </Link>
-            </div>
-            <div className="flex-0">
-              <Link
-                className="inline-block"
-                to="https://github.com/kei10in/takaneko"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaGithub className="h-6 w-6" />
-              </Link>
-            </div>
+            <Link
+              className="inline-block flex-none"
+              to="https://twitter.com/takanekofan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaXTwitter className="h-6 w-6" />
+            </Link>
+            <Link
+              className="inline-block flex-none"
+              to="https://github.com/kei10in/takaneko"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub className="h-6 w-6" />
+            </Link>
           </div>
         </div>
       </div>
