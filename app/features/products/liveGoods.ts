5e993646454f0a950ba2459ea2128d28b40ec250
Keisuke Ijuin <kei10in@gmail.com>
2024-09-15 11:34:22 +0900

    ライブグッズを追加。

--- app/features/products/liveGoods.ts
+++ app/features/products/liveGoods.ts
@@ -1,8 +1,10 @@
 import { _1st_ファンミーティング_お披露目式 } from "./lives/1st ファンミーティング ～お披露目式～";
 import { たかねこクリスマスパーティ2022 } from "./lives/たかねこクリスマスパーティ－2022";
+import { 全国お招きツアー2023_First_trip } from "./lives/全国お招きツアー2023 〜First trip〜";
 import { LiveGoodsDescription } from "./product";
 
 export const LiveGoods: LiveGoodsDescription[] = [
+  全国お招きツアー2023_First_trip,
   たかねこクリスマスパーティ2022,
   _1st_ファンミーティング_お披露目式,
 ];
