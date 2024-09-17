b6cad388ca58044403318197ce70f91157a5a840
Keisuke Ijuin <kei10in@gmail.com>
2024-09-17 23:30:54 +0900

    Update profile

--- app/routes/members.mikuru-hoshitani.tsx
+++ app/routes/members.mikuru-hoshitani.tsx
@@ -1,4 +1,5 @@
-import { MetaFunction } from "@remix-run/react";
+import { Link, MetaFunction } from "@remix-run/react";
+import { FaInstagram } from "react-icons/fa6";
 import { SITE_TITLE } from "~/constants";
 import { MemberProfile } from "~/routes/members/MemberProfile";
 import { MikuruHoshitani } from "./members/members";
@@ -14,12 +15,33 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  // インスタのアカウントが複数あるので両方載せる。
-  // https://www.instagram.com/min.0o6/
-
   return (
     <div className="container mx-auto">
-      <MemberProfile profile={MikuruHoshitani} />
+      <MemberProfile profile={MikuruHoshitani}>
+        <section className="mt-12 px-4">
+          <h2 className="mb-4 text-center text-3xl font-bold text-gray-400">Link</h2>
+          <div>
+            <Link className="block" to="https://www.instagram.com/min.0o6/">
+              <div className="flex h-28 gap-2 bg-white shadow">
+                <div className="aspect-square h-28 flex-none">
+                  <div className="h-full w-full bg-nadeshiko-700 p-3">
+                    <FaInstagram className="h-full w-full text-white" />
+                  </div>
+                </div>
+                <div className="flex flex-1 flex-col justify-center space-y-1 p-2">
+                  <p className="line-clamp-1 flex-none text-xs text-gray-400">www.instagram.com</p>
+                  <p className="flex-none text-sm font-bold text-gray-600">
+                    旧インスタグラム アカウント
+                  </p>
+                  <p className="line-clamp-2 h-[2rem] text-xs text-gray-400">
+                    2022年12月まではこちらのアカウントが使われていました。
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </section>
+      </MemberProfile>
     </div>
   );
 }
