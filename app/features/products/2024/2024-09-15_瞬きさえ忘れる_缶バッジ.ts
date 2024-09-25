97261ed58b044e0aca321837ac87713942323aab
Keisuke Ijuin <kei10in@gmail.com>
2024-09-25 22:56:25 +0900

    トレード画像をつくるやつの対応を追加

---
+++ app/features/products/2024/2024-09-15_瞬きさえ忘れる_缶バッジ.ts
@@ -0,0 +1,36 @@
+import { ProductImage } from "~/features/products/product";
+
+export const 瞬きさえ忘れる_缶バッジ: ProductImage = {
+  id: "ランダム缶バッジ「瞬きさえ忘れる。」ver.",
+  name: "ランダム缶バッジ「瞬きさえ忘れる。」ver.",
+  year: 2024,
+  series: "瞬きさえ忘れる。アクリルブロック",
+  kind: "アクリルブロック",
+  url: "/takaneko/others/2024-09-15_瞬きさえ忘れる_缶バッジ.webp",
+  width: 362,
+  height: 147,
+  photos: [
+    { id: 1, name: "城月菜央", description: "" },
+    { id: 2, name: "涼海すう", description: "" },
+    { id: 3, name: "橋本桃呼", description: "" },
+    { id: 4, name: "葉月紗蘭", description: "" },
+    { id: 5, name: "春野莉々", description: "" },
+    { id: 6, name: "東山恵里沙", description: "" },
+    { id: 7, name: "日向端ひな", description: "" },
+    { id: 8, name: "星谷美来", description: "" },
+    { id: 9, name: "松本ももな", description: "" },
+    { id: 10, name: "籾山ひめり", description: "" },
+  ],
+  positions: [
+    { id: 1, x: 14, y: 13, width: 63, height: 63 },
+    { id: 2, x: 81, y: 13, width: 63, height: 63 },
+    { id: 3, x: 149, y: 13, width: 63, height: 63 },
+    { id: 4, x: 217, y: 13, width: 63, height: 63 },
+    { id: 5, x: 285, y: 13, width: 63, height: 63 },
+    { id: 6, x: 14, y: 80, width: 63, height: 63 },
+    { id: 7, x: 81, y: 80, width: 63, height: 63 },
+    { id: 8, x: 149, y: 80, width: 63, height: 63 },
+    { id: 9, x: 217, y: 80, width: 63, height: 63 },
+    { id: 10, x: 285, y: 80, width: 63, height: 63 },
+  ],
+};
