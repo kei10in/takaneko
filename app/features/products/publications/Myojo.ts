5ee2248dd2eb164f311012283997b194f1a3b1f5
Keisuke Ijuin <kei10in@gmail.com>
2024-10-10 19:36:38 +0900

    雑誌を追加。

---
+++ app/features/products/publications/Myojo.ts
@@ -0,0 +1,35 @@
+import { PublicationDescription } from "../product";
+
+export const 明星2024年9月号: PublicationDescription = {
+  id: "Myojo 2024年9月号",
+  name: "Myojo 2024年9月号",
+  date: "2024-07-22",
+  kind: "magazines",
+  publisher: "集英社",
+  list_price: 800,
+  url: "",
+  cover_images: [
+    {
+      path: "/publications/2024-07-22_Myojo_2024年9月号_表紙.jpg",
+      ref: "https://x.com/Myojo_henshu/status/1811639771971878948",
+    },
+  ],
+  links: [
+    {
+      text: "明星公式 X 表紙公開",
+      url: "https://x.com/Myojo_henshu/status/1811639771971878948",
+    },
+  ],
+  featured_members: [
+    "城月菜央",
+    "涼海すう",
+    "橋本桃呼",
+    "葉月紗蘭",
+    "春野莉々",
+    "東山恵里沙",
+    "日向端ひな",
+    "星谷美来",
+    "松本ももな",
+    "籾山ひめり",
+  ],
+};
