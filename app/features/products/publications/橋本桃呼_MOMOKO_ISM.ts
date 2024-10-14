eae3547aa1aab3857a83444607d21efd9f5f8283
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 23:25:52 +0900

    デジタル写真集を追加。

---
+++ app/features/products/publications/橋本桃呼_MOMOKO_ISM.ts
@@ -0,0 +1,21 @@
+import { PublicationDescription } from "../product";
+
+export const 橋本桃呼_MOMOKO_ISM: PublicationDescription = {
+  id: "橋本桃呼 MOMOKO ISM",
+  name: "橋本桃呼 MOMOKO ISM",
+  date: "2024-06-22",
+  kind: "magazines", // "books"
+  publisher: "LARME",
+  list_price: 980,
+  code: [],
+  url: "",
+  cover_images: [
+    {
+      path: "/publications/2024-06-22_橋本桃呼_MOMOKO ISM.jpg",
+      ref: "https://x.com/takanenofficial/status/1804107216229740825",
+    },
+  ],
+  featured_members: ["橋本桃呼"],
+  official_x: "https://x.com/takanenofficial/status/1804107216229740825",
+  links: [],
+};
