92acde54f9fb124bf78b5924eade46cc69d32506
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 22:18:09 +0900

    雑誌を追加。

---
+++ app/features/products/publications/nicola.ts
@@ -0,0 +1,26 @@
+import { PublicationDescription } from "../product";
+
+export const nicola_2024年5月号: PublicationDescription = {
+  id: "nicola 2024年5月号",
+  name: "nicola 2024年5月号",
+  date: "2024-04-01",
+  kind: "magazines",
+  publisher: "新潮社",
+  list_price: 690,
+  code: [],
+  url: "https://nicola.jp/article/article-18973/",
+  cover_images: [
+    {
+      path: "/publications/2024-04-01_nicola 2024年5月号.jpg",
+      ref: "https://nicola.jp/article/article-18973/",
+    },
+  ],
+  featured_members: ["東山恵里沙"],
+  official_x: "https://x.com/takanenofficial/status/1773183264485683552",
+  links: [
+    {
+      text: "新潮社",
+      url: "https://www.shinchosha.co.jp/nicola/backnumber/20240401/",
+    },
+  ],
+};
