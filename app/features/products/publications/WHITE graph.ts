19b39f5f25a3cced31fc5dc53c1c364150f9abe3
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 23:04:44 +0900

    雑誌を追加。

---
+++ app/features/products/publications/WHITE graph.ts
@@ -0,0 +1,21 @@
+import { PublicationDescription } from "../product";
+
+export const WHITE_graph_011: PublicationDescription = {
+  id: "WHITE graph 011",
+  name: "WHITE graph 011",
+  date: "2024-05-29",
+  kind: "magazines",
+  publisher: "講談社",
+  list_price: 1980,
+  code: [{ kind: "isbn", value: "978-4-06-535853-5" }],
+  url: "https://bookclub.kodansha.co.jp/product?item=0000386988",
+  cover_images: [
+    {
+      path: "/publications/2024-05-29_WHITE graph 011.jpg",
+      ref: "https://bookclub.kodansha.co.jp/product?item=0000386988",
+    },
+  ],
+  featured_members: ["松本ももな"],
+  official_x: "https://x.com/takanenofficial/status/1785944849981804988",
+  links: [],
+};
