fc50e25e2a2829cadaadaa9b81cfe0d6a77afdb3
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 22:24:45 +0900

    雑誌を追加。

--- app/features/products/publications/B.L.T.ts
+++ app/features/products/publications/B.L.T.ts
@@ -1,10 +1,30 @@
 import { PublicationDescription } from "../product";
 
+export const BLT_2024年9月号: PublicationDescription = {
+  id: "B.L.T 2024年9月号",
+  name: "B.L.T 2024年9月号",
+  date: "2024-07-26",
+  kind: "magazines",
+  publisher: "東京ニュース通信社",
+  list_price: 1210,
+  code: [],
+  url: "https://bltweb.jp/2024/07/22/blt2409_cover/",
+  cover_images: [
+    {
+      path: "/publications/2024-07-26_B.L.T_2024年9月号.jpg",
+      ref: "https://bltweb.jp/2024/07/22/blt2409_cover/",
+    },
+  ],
+  featured_members: ["籾山ひめり"],
+  official_x: "https://x.com/takanenofficial/status/1810260128601387254",
+  links: [],
+};
+
 export const BLT_2023年2月号: PublicationDescription = {
   id: "B.L.T 2023年2月号",
   name: "B.L.T 2023年2月号",
   date: "2022-12-26",
-  kind: "magazines", // "books"
+  kind: "magazines",
   publisher: "東京ニュース通信社",
   list_price: 1300,
   code: [],
