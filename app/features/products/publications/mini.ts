c816ba840324392de06432cc56e322d1a5189d62
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 17:26:30 +0900

    雑誌を追加。

---
+++ app/features/products/publications/mini.ts
@@ -0,0 +1,51 @@
+import { PublicationDescription } from "../product";
+
+export const mini_2023年2月号: PublicationDescription = {
+  id: "mini 2023年2月号",
+  name: "mini 2023年2月号",
+  date: "2023-01-12",
+  kind: "magazines", // "books"
+  publisher: "宝島社",
+  list_price: 820,
+  code: [{ kind: "JAN", value: "4910084210232" }],
+  url: "https://tkj.jp/mini/202302",
+  cover_images: [
+    {
+      path: "/publications/2023-01-12_mini_2023年2月号.jpg",
+      ref: "https://store.tkj.jp/shopdetail/000000015367/",
+    },
+  ],
+  featured_members: ["籾山ひめり"],
+  official_x: "https://x.com/takanenofficial/status/1613491040757108736",
+  links: [
+    {
+      text: "宝島社の通販",
+      url: "https://store.tkj.jp/shopdetail/000000015367/",
+    },
+  ],
+};
+
+export const mini_2022年10月号: PublicationDescription = {
+  id: "mini 2022年10月号",
+  name: "mini 2022年10月号",
+  date: "2022-09-01",
+  kind: "magazines", // "books"
+  publisher: "宝島社",
+  list_price: 950,
+  code: [{ kind: "JAN", value: "4910084211024" }],
+  url: "https://tkj.jp/mini/202210",
+  cover_images: [
+    {
+      path: "/publications/2022-09-01_mini_2022年10月号.jpg",
+      ref: "https://www.amazon.co.jp/dp/B0B7HBZYY8",
+    },
+  ],
+  featured_members: ["籾山ひめり"],
+  official_x: "https://x.com/takanenofficial/status/1565187774474436608",
+  links: [
+    {
+      text: "宝島社の通販",
+      url: "https://store.tkj.jp/shopdetail/000000014799/",
+    },
+  ],
+};
---
+++ app/features/products/publications/月刊ENTAME.ts
@@ -0,0 +1,32 @@
+import { PublicationDescription } from "../product";
+
+export const 月刊ENTAME_2023年3・4月合併号: PublicationDescription = {
+  id: "月刊ENTAME 2023年3・4月合併号",
+  name: "月刊ENTAME 2023年3・4月合併号",
+  date: "2023-01-30",
+  kind: "magazines", // "books"
+  publisher: "徳間書店",
+  list_price: 950,
+  code: [],
+  url: "https://entamenext.com/magazines/detail/54",
+  cover_images: [
+    {
+      path: "/publications/2023-01-30_月刊ENTAME_2023年3・4月合併号.jpg",
+      ref: "https://www.amazon.co.jp/dp/B0BRT758YT",
+    },
+  ],
+  featured_members: [
+    "城月菜央",
+    "涼海すう",
+    "橋本桃呼",
+    "葉月紗蘭",
+    "春野莉々",
+    "東山恵里沙",
+    "日向端ひな",
+    "星谷美来",
+    "松本ももな",
+    "籾山ひめり",
+  ],
+  official_x: "https://x.com/takanenofficial/status/1620014080630358017",
+  links: [],
+};
