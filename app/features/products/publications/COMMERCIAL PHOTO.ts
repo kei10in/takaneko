b4e503b8b7b8a31f41bd62db56397261f75c0961
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 23:18:27 +0900

    雑誌を追加。

---
+++ app/features/products/publications/COMMERCIAL PHOTO.ts
@@ -0,0 +1,21 @@
+import { PublicationDescription } from "../product";
+
+export const COMMERCIAL_PHOTO_2023年10月号: PublicationDescription = {
+  id: "COMMERCIAL PHOTO 2023年10月号",
+  name: "COMMERCIAL PHOTO 2023年10月号",
+  date: "2023-09-14",
+  kind: "magazines",
+  publisher: "玄光社",
+  list_price: 1760,
+  code: [],
+  url: "http://www.genkosha.co.jp/cp/backnumber/5249.html",
+  cover_images: [
+    {
+      path: "/publications/2023-09-14_COMMERCIAL PHOTO 2023年10月号.jpg",
+      ref: "https://www.amazon.co.jp/dp/B0CFVKVMCZ",
+    },
+  ],
+  featured_members: ["橋本桃呼", "東山恵里沙", "星谷美来", "松本ももな", "籾山ひめり"],
+  official_x: "https://x.com/takanenofficial/status/1698984347616112831",
+  links: [],
+};
