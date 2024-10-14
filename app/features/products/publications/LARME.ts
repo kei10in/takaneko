392b72c09b64765bd374ad65105f0f7d0a9f5f77
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 21:15:55 +0900

    雑誌を追加。

---
+++ app/features/products/publications/LARME.ts
@@ -0,0 +1,141 @@
+import { PublicationDescription } from "../product";
+
+export const LARME_062: PublicationDescription = {
+  id: "LARME 062 Autumn 2024",
+  name: "LARME 062 Autumn 2024",
+  date: "2024-09-17",
+  kind: "magazines",
+  publisher: "徳間書店",
+  list_price: 980,
+  code: [],
+  url: "https://www.larme.co/",
+  cover_images: [
+    {
+      path: "/publications/larme/LARME062.jpg",
+      ref: "https://www.larme.co/",
+    },
+  ],
+  featured_members: ["橋本桃呼", "松本ももな"],
+  official_x: "https://x.com/takanenofficial/status/1835982088459460894",
+  links: [],
+};
+
+export const LARME_061: PublicationDescription = {
+  id: "LARME 061 Summer 2024",
+  name: "LARME 061 Summer 2024",
+  date: "2024-06-17",
+  kind: "magazines",
+  publisher: "徳間書店",
+  list_price: 980,
+  code: [],
+  url: "https://www.larme.co/",
+  cover_images: [
+    {
+      path: "/publications/larme/LARME061.jpg",
+      ref: "https://www.larme.co/",
+    },
+  ],
+  featured_members: ["橋本桃呼", "松本ももな"],
+  official_x: "https://x.com/takanenofficial/status/1801932802252017731",
+  links: [],
+};
+
+export const LARME_060: PublicationDescription = {
+  id: "LARME 060 Spring 2024",
+  name: "LARME 060 Spring 2024",
+  date: "2024-03-18",
+  kind: "magazines",
+  publisher: "徳間書店",
+  list_price: 980,
+  code: [],
+  url: "https://www.larme.co/",
+  cover_images: [
+    {
+      path: "/publications/larme/LARME060.jpg",
+      ref: "https://www.larme.co/",
+    },
+  ],
+  featured_members: ["松本ももな"],
+  official_x: "https://x.com/takanenofficial/status/1768985738023252047",
+  links: [],
+};
+
+export const LARME_059: PublicationDescription = {
+  id: "LARME 059 Winter 2023",
+  name: "LARME 059 Winter 2023",
+  date: "2023-12-15",
+  kind: "magazines",
+  publisher: "徳間書店",
+  list_price: 800,
+  code: [],
+  url: "https://x.com/takanenofficial/status/1735255943808754136",
+  cover_images: [
+    {
+      path: "/publications/larme/LARME059.jpg",
+      ref: "https://www.larme.co/",
+    },
+  ],
+  featured_members: ["松本ももな"],
+  official_x: "",
+  links: [],
+};
+
+export const LARME_058: PublicationDescription = {
+  id: "LARME 058 Autumn 2023",
+  name: "LARME 058 Autumn 2023",
+  date: "2023-09-15",
+  kind: "magazines", // "books"
+  publisher: "徳間書店",
+  list_price: 800,
+  code: [],
+  url: "https://x.com/takanenofficial/status/1702291127641206861",
+  cover_images: [
+    {
+      path: "/publications/larme/LARME058.jpg",
+      ref: "https://www.larme.co/",
+    },
+  ],
+  featured_members: ["松本ももな"],
+  official_x: "",
+  links: [],
+};
+
+export const LARME_056: PublicationDescription = {
+  id: "LARME 056 Spring 2023",
+  name: "LARME 056 Spring 2023",
+  date: "2023-03-17",
+  kind: "magazines", // "books"
+  publisher: "徳間書店",
+  list_price: 800,
+  code: [],
+  url: "https://www.larme.co/",
+  cover_images: [
+    {
+      path: "/publications/larme/LARME056.jpg",
+      ref: "https://www.larme.co/",
+    },
+  ],
+  featured_members: ["松本ももな"],
+  official_x: "https://x.com/takanenofficial/status/1636654893246349314",
+  links: [],
+};
+
+export const LARME_054: PublicationDescription = {
+  id: "LARME 054 Autumn 2022",
+  name: "LARME 054 Autumn 2022",
+  date: "2022-09-16",
+  kind: "magazines", // "books"
+  publisher: "徳間書店",
+  list_price: 800,
+  code: [],
+  url: "https://www.larme.co/",
+  cover_images: [
+    {
+      path: "/publications/larme/LARME054.jpg",
+      ref: "https://www.larme.co/",
+    },
+  ],
+  featured_members: ["松本ももな"],
+  official_x: "https://x.com/takanenofficial/status/1636654893246349314",
+  links: [],
+};
