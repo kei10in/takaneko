9d36a6247961694ab10a627124f6a2b901a5c8b3
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 23:08:15 +0900

    デジタル写真集を追加。

--- app/features/products/publications/WHITE graph.ts
+++ app/features/products/publications/WHITE graph.ts
@@ -19,3 +19,23 @@ export const WHITE_graph_011: PublicationDescription = {
   official_x: "https://x.com/takanenofficial/status/1785944849981804988",
   links: [],
 };
+
+export const 松本ももな_WHITE_graph_デジタル写真集: PublicationDescription = {
+  id: "松本ももな WHITE graph デジタル写真集",
+  name: "松本ももな WHITE graph デジタル写真集",
+  date: "2024-05-29",
+  kind: "books",
+  publisher: "講談社",
+  list_price: 2200,
+  code: [],
+  url: "https://bookclub.kodansha.co.jp/product?item=0000393751",
+  cover_images: [
+    {
+      path: "/publications/2024-05-29_松本ももな WHITE graph デジタル写真集.jpg",
+      ref: "https://bookclub.kodansha.co.jp/product?item=0000393751",
+    },
+  ],
+  featured_members: ["松本ももな"],
+  official_x: "https://x.com/takanenofficial/status/1785944849981804988",
+  links: [],
+};
