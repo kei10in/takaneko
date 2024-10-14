f0e7a61827cd8598f1ced01f891bb1732f962c21
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 22:46:35 +0900

    雑誌を追加。

---
+++ app/features/products/publications/COSPLAY MODE.ts
@@ -0,0 +1,21 @@
+import { PublicationDescription } from "../product";
+
+export const COSPLAY_MODE_2024年9月号: PublicationDescription = {
+  id: "COSPLAY MODE 2024年9月号",
+  name: "COSPLAY MODE 2024年9月号",
+  date: "2024-08-02",
+  kind: "magazines",
+  publisher: "シムサム・メディア",
+  list_price: 1320,
+  code: [],
+  url: "https://cosplaymode.net/feature/29392/",
+  cover_images: [
+    {
+      path: "/publications/2024-08-02_COSPLAY MODE 2024年9月号.jpg",
+      ref: "https://x.com/cosplay_mode/status/1819161195418693695",
+    },
+  ],
+  featured_members: ["春野莉々"],
+  official_x: "https://x.com/takanenofficial/status/1819161381738107081",
+  links: [],
+};
