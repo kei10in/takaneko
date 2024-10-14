261f8dc3ffdbd1ea1a551ab3cf71a0eaa909a521
Keisuke Ijuin <kei10in@gmail.com>
2024-10-14 22:58:08 +0900

    雑誌を追加。

---
+++ app/features/products/publications/FINEBOYS.ts
@@ -0,0 +1,21 @@
+import { PublicationDescription } from "../product";
+
+export const FINEBOYS_2024年7月号: PublicationDescription = {
+  id: "FINEBOYS 2024年7月号",
+  name: "FINEBOYS 2024年7月号",
+  date: "2024-06-07",
+  kind: "magazines",
+  publisher: "日之出出版",
+  list_price: 820,
+  code: [],
+  url: "https://hinode-publishing.jp/?newmag=fineboys-2024年7月号",
+  cover_images: [
+    {
+      path: "/publications/2024-06-07_FINEBOYS_2024年7月号.jpg",
+      ref: "https://hinode-publishing.jp/?newmag=fineboys-2024年7月号",
+    },
+  ],
+  featured_members: ["松本ももな"],
+  official_x: "https://x.com/takanenofficial/status/1798573120107987146",
+  links: [],
+};
