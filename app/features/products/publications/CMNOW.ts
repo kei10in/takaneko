321501ba94db4a6b1e397c5c5675495110873031
Keisuke Ijuin <kei10in@gmail.com>
2024-10-16 19:25:57 +0900

    Fix id

--- app/features/products/publications/CMNOW.ts
+++ app/features/products/publications/CMNOW.ts
@@ -1,8 +1,8 @@
 import { PublicationDescription } from "../product";
 
 export const CMNOW_vol220_2023年1_2月号: PublicationDescription = {
-  id: "CM NOW vol.220 2023年1/2月号",
-  name: "CM NOW vol.220 2023年1/2月号",
+  id: "CM NOW vol.220 2023年1-2月号",
+  name: "CM NOW vol.220 2023年1-2月号",
   date: "2022-12-09",
   kind: "magazines",
   publisher: "玄光社",
@@ -11,11 +11,11 @@ export const CMNOW_vol220_2023年1_2月号: PublicationDescription = {
   url: "http://www.genkosha.co.jp/cm/backnumber/2400.html",
   cover_images: [
     {
-      path: "/publications/2022-12-09_CMNOW_vol220_2023年1・2月号_表紙.jpg",
+      path: "/publications/2022-12-09_CMNOW_vol220_2023年1-2月号_表紙.jpg",
       ref: "https://www.amazon.co.jp/dp/B00A7ZCLGO",
     },
     {
-      path: "/publications/2022-12-09_CMNOW_vol220_2023年1・2月号_裏表紙.jpg",
+      path: "/publications/2022-12-09_CMNOW_vol220_2023年1-2月号_裏表紙.jpg",
       ref: "https://www.amazon.co.jp/dp/B00A7ZCLGO",
     },
   ],
