662261181f1106c4bc4a6f7b5275376bc531131c
Keisuke Ijuin <kei10in@gmail.com>
2024-10-31 00:08:36 +0900

    誕生日記念グッズを追加。

---
+++ app/features/products/birthday/2024-10-30_日向端ひな誕生日記念.ts
@@ -0,0 +1,57 @@
+import { BirthdayGoodsCollection, OfficialGoods } from "../product";
+
+export const 日向端ひな誕生日記念_2024年10月30日: BirthdayGoodsCollection = {
+  id: "日向端ひな誕生日記念 2024年10月30日",
+  name: "日向端ひな誕生日記念 2024年10月30日",
+  memberName: "日向端ひな",
+  lineup: [
+    "18㌢のひにゃねこアクスタ🐈‍⬛",
+    "ひにゃねこかがみ(=^H×H^=)",
+    "がんばってかいた3連キンホルダー！",
+  ],
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-10-30_日向端ひな誕生日記念グッズ.jpg",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const _18センチのひにゃねこアクスタ: OfficialGoods = {
+  id: "18㌢のひにゃねこアクスタ",
+  name: "18㌢のひにゃねこアクスタ🐈‍⬛",
+  date: "2024-10-30",
+  listPrice: 1900,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-10-30_18㌢のにひにゃねこアクスタ.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const ひにゃねこかがみ: OfficialGoods = {
+  id: "ひにゃねこかがみ(=^H×H^=)",
+  name: "ひにゃねこかがみ(=^H×H^=)",
+  date: "2024-10-30",
+  listPrice: 2000,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-10-30_ひにゃねこかがみ(=^H×H^=).webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const がんばってかいた3連キンホルダー: OfficialGoods = {
+  id: "がんばってかいた3連キンホルダー！",
+  name: "がんばってかいた3連キンホルダー！",
+  date: "2024-10-30",
+  listPrice: 1800,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-10-30_がんばってかいた3連キンホルダー！.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
