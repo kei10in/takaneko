116ddc2cf6de6825723917aa825750fc9e3a0e61
Keisuke Ijuin <kei10in@gmail.com>
2024-11-06 23:52:53 +0900

    誕生日記念グッズを追加。

---
+++ app/features/products/birthday/2024-11-06_星谷美来誕生日記念.ts
@@ -0,0 +1,53 @@
+import { BirthdayGoodsCollection, OfficialGoods } from "../product";
+
+export const 星谷美来誕生日記念_2024年11月6日: BirthdayGoodsCollection = {
+  id: "星谷美来誕生日記念 2024年11月6日",
+  name: "星谷美来誕生日記念 2024年11月6日",
+  memberName: "星谷美来",
+  lineup: ["♡まいにちエコるん", "☆みくるんお守り写真", "◇シュシュるん"],
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-11-06_星谷美来誕生日記念グッズ.jpg",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const まいにちエコるん: OfficialGoods = {
+  id: "♡まいにちエコるん",
+  name: "♡まいにちエコるん",
+  date: "2024-11-06",
+  listPrice: 3000,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-11-06_♡まいにちエコるん.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const みくるんお守り写真: OfficialGoods = {
+  id: "☆みくるんお守り写真",
+  name: "☆みくるんお守り写真",
+  date: "2024-11-06",
+  listPrice: 1000,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-11-06_☆みくるんお守り写真.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const シュシュるん: OfficialGoods = {
+  id: "◇シュシュるん",
+  name: "◇シュシュるん",
+  date: "2024-11-06",
+  listPrice: 1800,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-11-06_◇シュシュるん.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
