b6c84a0a3955b32f6c478c08edbb1a77a9c30067
Keisuke Ijuin <kei10in@gmail.com>
2024-12-02 00:05:08 +0900

    短くするやつを修正

--- app/routes/shortlink._index/route.tsx
+++ app/routes/shortlink._index/route.tsx
@@ -4,32 +4,29 @@ import { BsBoxArrowUp } from "react-icons/bs";
 import { CopyButton } from "~/components/CopyButton";
 import { SITE_TITLE } from "~/constants";
 import { shouldUseWebShareApi } from "~/utils/browser/webShareApi";
-import { shortlink } from "~/utils/shortlink";
+import { shortlink, ShortLinkResult } from "~/utils/shortlink";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: `ツールだよ - ${SITE_TITLE}` },
+    { title: `短い URL を作るやつ - ${SITE_TITLE}` },
     {
       name: "description",
-      content: "高嶺のなでしこ 関係のツールだよ",
+      content:
+        "高嶺のなでしこ公式サイトの URL を高嶺のなでしこ公式 X のポストにあるような短い URL に変換します。",
     },
   ];
 };
 
-export async function loader({ request }: LoaderFunctionArgs): Promise<{ url?: string }> {
+export async function loader({ request }: LoaderFunctionArgs): Promise<ShortLinkResult> {
   const requestUrl = new URL(request.url);
   const url = requestUrl.searchParams.get("url");
   if (url == undefined) {
     return {};
   }
 
-  if (!url.startsWith("https://takanenonadeshiko.jp")) {
-    return {};
-  }
-
   const result = await shortlink(url);
 
-  return { url: result };
+  return result;
 }
 
 export default function Index() {
@@ -39,47 +36,67 @@ export default function Index() {
 
   const showShareButton = shouldUseWebShareApi();
 
+  let error: string | undefined = undefined;
+  if (urlInQuery != undefined) {
+    if (data.error == "Invalid URL") {
+      error = "高嶺のなでしこ公式サイトの URL を入力してください。";
+    } else if (data.error == "Page Not Found") {
+      error = "高嶺のなでしこ公式サイトにページが見つかりませんでした。";
+    } else if (data.error == "Shortlink Not Found") {
+      error = "短くできないページです。";
+    } else if (data.error == "Unknown Error") {
+      error = "エラーが発生しました。";
+    }
+  }
+
   return (
     <div className="container mx-auto min-h-[calc(100svh-var(--header-height))] lg:max-w-5xl">
       <section className="px-4 py-8">
         <section className="text-gray-700">
-          <h2 className="center my-4 text-3xl font-semibold text-gray-600">
-            <code>?p=XXXX</code> みたいな URL を作るやつ
-          </h2>
+          <h2 className="center my-4 text-3xl font-semibold text-gray-600">短い URL を作るやつ</h2>
           <p className="mt-8">
             高嶺のなでしこ公式のツイートの https://takanenonadeshiko.jp/?p=55 のような短い URL
             を作ります。
           </p>
-          <Form className="mt-8 flex items-center space-x-2" method="get">
+          <p className="mt-2">
+            高嶺のなでしこ公式サイトの URL を X にポストしたいんだけどどうやったら公式 X みたいな
+            URL になるの？という疑問にお応えします。
+          </p>
+          <Form
+            className="mt-8 items-center space-y-2 lg:flex lg:space-x-2 lg:space-y-0"
+            method="get"
+          >
             <input
-              className="flex-1 text-wrap rounded-md border px-2 py-2 outline-none"
+              className="block h-8 w-full rounded-md border px-2 font-mono text-sm outline-none lg:flex-1"
               type="text"
-              placeholder="URL"
+              placeholder="高嶺のなでしこ公式サイト内のページの URL"
               name="url"
               defaultValue={urlInQuery ?? undefined}
             />
             <button
-              className="rounded-md bg-nadeshiko-800 px-6 py-2 font-bold text-white"
+              className="ml-auto block h-8 rounded-md bg-nadeshiko-800 px-6 font-bold text-white lg:ml-0 lg:flex-none"
               type="submit"
             >
               短くする
             </button>
           </Form>
-          {urlInQuery != "" && urlInQuery != undefined && data.url == undefined && (
-            <p className="mx-2 my-2 text-nadeshiko-900">
-              高嶺のなでしこ公式サイトの URL を入力してください。
-            </p>
-          )}
+          {error != undefined && <p className="mx-2 my-2 text-nadeshiko-900">{error}</p>}
 
           {data.url != undefined && (
             <section className="mt-12">
               <h3 className="text-bold mb-2 mt-12 text-xl text-gray-700">結果</h3>
-              <div className="rounded-md border p-4">
-                <p className="flex items-center gap-2 text-lg text-gray-600">
-                  <span className="flex-1">{data.url}</span>
+              <div className="mt-2 items-center gap-2 space-y-1 text-gray-600 lg:flex lg:space-y-0">
+                <div className="h-8 flex-1">
+                  <input
+                    className="h-full w-full rounded-md border px-2 font-mono text-sm"
+                    readOnly
+                    value={data.url}
+                  />
+                </div>
+                <div className="flex flex-none items-center justify-end">
                   {showShareButton && (
                     <button
-                      className="group flex h-9 w-9 items-center justify-center overflow-hidden rounded-md p-2 hover:bg-gray-100"
+                      className="group flex h-8 w-8 items-center justify-center overflow-hidden rounded-md p-2 hover:bg-gray-100"
                       onClick={async () => {
                         if (window?.navigator?.share == undefined) {
                           return;
@@ -92,11 +109,11 @@ export default function Index() {
                         });
                       }}
                     >
-                      <BsBoxArrowUp className="h-5 w-5 text-gray-600" />
+                      <BsBoxArrowUp className="h-4 w-4 text-gray-600" />
                     </button>
                   )}
                   <CopyButton className="flex-none" data={data.url} />
-                </p>
+                </div>
               </div>
             </section>
           )}
--- app/utils/shortlink.ts
+++ app/utils/shortlink.ts
@@ -1,15 +1,32 @@
 import * as cheerio from "cheerio";
 
-export const shortlink = async (url: string): Promise<string | undefined> => {
-  try {
-    const response = await fetch(url);
-    const responseBuffer = await response.arrayBuffer();
-    const body = new TextDecoder().decode(responseBuffer);
+export type ShortLinkResult = {
+  url?: string | undefined;
+  error?: "Invalid URL" | "Page Not Found" | "Shortlink Not Found" | "Unknown Error";
+};
 
-    const $ = cheerio.load(body);
-
-    return $("link[rel=shortlink]").attr("href");
-  } catch (e) {
-    return undefined;
+export const shortlink = async (url: string): Promise<ShortLinkResult> => {
+  if (!url.startsWith("https://takanenonadeshiko.jp")) {
+    return { error: "Invalid URL" };
   }
+
+  const response = await fetch(url, {});
+
+  if (400 <= response.status && response.status < 500) {
+    return { error: "Page Not Found" };
+  } else if (!response.ok) {
+    return { error: "Unknown Error" };
+  }
+
+  const responseBuffer = await response.arrayBuffer();
+  const body = new TextDecoder().decode(responseBuffer);
+
+  const $ = cheerio.load(body);
+
+  const shortlink = $("link[rel=shortlink]").attr("href");
+  if (shortlink == undefined) {
+    return { error: "Shortlink Not Found" };
+  }
+
+  return { url: shortlink };
 };
