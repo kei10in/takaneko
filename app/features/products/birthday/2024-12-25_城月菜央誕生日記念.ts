0f4c655f4f8ad8caa0c72373ff9a3fbfca6ec4ca
Keisuke Ijuin <kei10in@gmail.com>
2024-12-25 22:16:52 +0900

    城月菜央誕生日記念グッズを追加。

---
+++ app/features/products/birthday/2024-12-25_城月菜央誕生日記念.ts
@@ -0,0 +1,65 @@
+import { BirthdayGoodsCollection, OfficialGoods } from "../product";
+
+export const 城月菜央誕生日記念_2024年12月25日: BirthdayGoodsCollection = {
+  id: "城月菜央誕生日記念 2024年12月25日",
+  name: "城月菜央誕生日記念 2024年12月25日",
+  memberName: "城月菜央",
+  lineup: ["誰でも着やすいTシャツ", "かっこいいおしゃれなグラス", "サイズ的にも使いやすいポーチ"],
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-12-25_城月菜央誕生日記念グッズ.jpg",
+      ref: "https://x.com/takanenofficial/status/1871888632954708367",
+    },
+  ],
+};
+
+export const 誰でも着やすいTシャツ: OfficialGoods = {
+  id: "誰でも着やすいTシャツ",
+  name: "誰でも着やすいTシャツ",
+  date: "2024-12-25",
+  listPrice: 4500,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-12-25_誰でも着やすいTシャツ_1.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+    {
+      path: "/takaneko/birthday-goods/2024-12-25_誰でも着やすいTシャツ_2.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+    {
+      path: "/takaneko/birthday-goods/2024-12-25_誰でも着やすいTシャツ_3.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const かっこいいおしゃれなグラス: OfficialGoods = {
+  id: "かっこいいおしゃれなグラス",
+  name: "かっこいいおしゃれなグラス",
+  date: "2024-12-25",
+  listPrice: 4000,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-12-25_かっこいいおしゃれなグラス_1.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+    {
+      path: "/takaneko/birthday-goods/2024-12-25_かっこいいおしゃれなグラス_2.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const サイズ的にも使いやすいポーチ: OfficialGoods = {
+  id: "サイズ的にも使いやすいポーチ",
+  name: "サイズ的にも使いやすいポーチ",
+  date: "2024-12-25",
+  listPrice: 2000,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2024-12-25_サイズ的にも使いやすいポーチ.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
--- app/features/products/birthdayGoods.ts
+++ app/features/products/birthdayGoods.ts
@@ -2,6 +2,7 @@ import { 涼海すう誕生日記念_2024年8月22日 } from "./birthday/2024-08
 import { 松本ももな誕生日記念_2024年10月12日 } from "./birthday/2024-10-12_松本ももな誕生日記念";
 import { 日向端ひな誕生日記念_2024年10月30日 } from "./birthday/2024-10-30_日向端ひな誕生日記念";
 import { 星谷美来誕生日記念_2024年11月6日 } from "./birthday/2024-11-06_星谷美来誕生日記念";
+import { 城月菜央誕生日記念_2024年12月25日 } from "./birthday/2024-12-25_城月菜央誕生日記念";
 import { BirthdayGoodsCollection } from "./product";
 
 export const BirthdayGoods: BirthdayGoodsCollection[] = [
@@ -9,4 +10,5 @@ export const BirthdayGoods: BirthdayGoodsCollection[] = [
   松本ももな誕生日記念_2024年10月12日,
   日向端ひな誕生日記念_2024年10月30日,
   星谷美来誕生日記念_2024年11月6日,
+  城月菜央誕生日記念_2024年12月25日,
 ];
