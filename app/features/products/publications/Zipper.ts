4290e2e289018f9840c9c006d5f599368ad74ac8
Keisuke Ijuin <kei10in@gmail.com>
2024-12-27 21:38:14 +0900

    雑誌を更新

--- app/features/products/publications/Zipper.ts
+++ app/features/products/publications/Zipper.ts
@@ -1,5 +1,30 @@
 import { Publication } from "../product";
 
+export const Zipper_2024_2025年冬号: Publication = {
+  id: "Zipper 2024-2025年秋号",
+  name: "Zipper 2024-2025年秋号",
+  date: "2024-12-26",
+  kind: "magazines",
+  publisher: "祥伝社",
+  listPrice: 700,
+  code: [{ kind: "", value: "" }],
+  url: "https://www.zipper.jp/news/news-20241218",
+  coverImages: [
+    {
+      path: "/publications/2024/2024-12-26_Zipper 2024-2025年秋号.webp",
+      ref: "https://www.zipper.jp/news/news-20241218",
+    },
+  ],
+  featuredMembers: ["籾山ひめり"],
+  officialTwitter: "https://x.com/takanenofficial/status/1869261320169869632",
+  links: [
+    {
+      text: "公式 X 発売告知",
+      url: "https://x.com/takanenofficial/status/1872600843486470641",
+    },
+  ],
+};
+
 export const Zipper_2024年秋号: Publication = {
   id: "Zipper 2024年秋号",
   name: "Zipper 2024年秋号",
@@ -38,24 +63,4 @@ export const Zipper_2024年秋号: Publication = {
       url: "https://x.com/takanenofficial/status/1839892810696405084",
     },
   ],
-};
-
-export const Zipper_2024_2025年冬号: Publication = {
-  id: "Zipper 2024-2025年秋号",
-  name: "Zipper 2024-2025年秋号",
-  date: "2024-12-26",
-  kind: "magazines",
-  publisher: "祥伝社",
-  listPrice: 700,
-  code: [{ kind: "", value: "" }],
-  url: "https://www.zipper.jp/news/news-20241218",
-  coverImages: [
-    {
-      path: "/publications/2024/2024-12-26_Zipper 2024-2025年秋号.webp",
-      ref: "https://www.zipper.jp/news/news-20241218",
-    },
-  ],
-  featuredMembers: ["籾山ひめり"],
-  officialTwitter: "https://x.com/takanenofficial/status/1869261320169869632",
-  links: [],
 };
