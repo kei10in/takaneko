5028c258ed627882f496604a5426023053ac2841
Keisuke Ijuin <kei10in@gmail.com>
2025-01-02 01:53:41 +0900

    その日のイベントの数がわかるようにする

--- app/features/calendars/CalendarCell.tsx
+++ app/features/calendars/CalendarCell.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import { categoryToColor } from "../events/EventType";
 import { CalendarEvent, uniqueEventRegions } from "./calendarEvents";
 
 interface Props {
@@ -20,12 +21,7 @@ export const CalendarCell: React.FC<Props> = (props: Props) => {
   const isWeekday = !isSunday && !isSaturday;
 
   return (
-    <div
-      className={clsx(
-        "h-11 w-full lg:h-[6.5rem]",
-        selected && "overflow-hidden bg-blue-500 text-white",
-      )}
-    >
+    <div className={clsx("w-full", selected && "overflow-hidden bg-blue-500 text-white")}>
       <div
         className={clsx(
           "pt-px",
@@ -39,39 +35,38 @@ export const CalendarCell: React.FC<Props> = (props: Props) => {
         <div
           data-today={today ? true : undefined}
           className={clsx(
-            "mx-auto flex h-[1.375rem] w-[1.375rem] items-center justify-center rounded-full text-sm",
+            "mx-auto flex h-[1.125rem] w-[1.125rem] items-center justify-center rounded-full text-xs",
             "data-[today]:bg-nadeshiko-900 data-[today]:text-white",
           )}
         >
           {date}
         </div>
       </div>
-      {regions[0] && (
-        <div className={clsx("text-center text-sm", !currentMonth && "text-gray-300")}>
-          {regions[0]}
-        </div>
-      )}
-      {regions[1] && (
+      {[0, 1, 2, 3].map((index) => (
         <div
-          className={clsx("hidden text-center text-sm lg:block", !currentMonth && "text-gray-300")}
+          key={index}
+          className={clsx(
+            "h-[1.125rem] text-center text-xs leading-normal lg:block",
+            index != 0 && "hidden",
+            !currentMonth && "text-gray-300",
+          )}
         >
-          {regions[1]}
+          {regions[index]}
         </div>
-      )}
-      {regions[2] && (
-        <div
-          className={clsx("hidden text-center text-sm lg:block", !currentMonth && "text-gray-300")}
-        >
-          {regions[2]}
-        </div>
-      )}
-      {regions[3] && (
-        <div
-          className={clsx("hidden text-center text-sm lg:block", !currentMonth && "text-gray-300")}
-        >
-          {regions[3]}
-        </div>
-      )}
+      ))}
+
+      <div className="pb-1 pt-0.5">
+        {events.length <= 5 ? (
+          <div className="flex h-1 items-center justify-center gap-0.5">
+            {events.map((event) => {
+              const color = categoryToColor(event.meta.category);
+              return <div className={clsx("h-1 w-1 rounded-full", color)} key={event.slug} />;
+            })}
+          </div>
+        ) : (
+          <div className="mx-auto h-1 w-7 rounded bg-nadeshiko-700" />
+        )}
+      </div>
     </div>
   );
 };
