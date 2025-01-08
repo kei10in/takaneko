a3dade42994d63896ff892dbf45d727bd3a5ed10
Keisuke Ijuin <kei10in@gmail.com>
2025-01-08 21:47:27 +0900

    画像の表示に生誕グッズのコンポーネントと同じものを使う。

--- app/routes/products.live-goods.$slug._index/route.tsx
+++ app/routes/products.live-goods.$slug._index/route.tsx
@@ -1,5 +1,8 @@
-import { Link, MetaFunction, useParams } from "@remix-run/react";
-import { Navigation, Pagination, Thumbs } from "swiper/modules";
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Link, MetaFunction, useLocation, useNavigate, useParams } from "@remix-run/react";
+import clsx from "clsx";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { SITE_TITLE } from "~/constants";
 import { LiveGoods } from "~/features/products/liveGoods";
@@ -30,6 +33,11 @@ const findLive = (slug: string | undefined): LiveGoodsCollection => {
 export default function Index() {
   const params = useParams<"slug">();
   const live = findLive(params.slug);
+  const images = live.images;
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const close = () => navigate(".", { replace: true, preventScrollReset: true });
 
   return (
     <div className="container mx-auto min-h-[calc(100svh-var(--header-height))]">
@@ -38,21 +46,55 @@ export default function Index() {
 
         <div className="mt-8">
           <Swiper
+            className={clsx(
+              "[&_.swiper-button-next]:text-nadeshiko-800",
+              "[&_.swiper-button-prev]:text-nadeshiko-800",
+            )}
             loop={true}
-            slidesPerView="auto"
             navigation={true}
-            pagination={true}
-            thumbs={{}}
-            modules={[Navigation, Pagination, Thumbs]}
+            modules={[Navigation]}
           >
-            {live.images.map((image) => (
-              <SwiperSlide key={image.path} className="w-fit">
-                <img
-                  key={image.path}
-                  src={image.path}
-                  alt={live.name}
-                  className="aspect-square w-full bg-gray-100 object-contain lg:w-96"
-                />
+            {images.map((image, i) => (
+              <SwiperSlide className="w-full bg-gray-50" key={image.path}>
+                <Link
+                  className="mx-auto block w-fit outline-none"
+                  to={`#photo-${i}`}
+                  replace={true}
+                  preventScrollReset={true}
+                >
+                  <img
+                    src={image.path}
+                    alt={live.name}
+                    className="aspect-[3/4] w-72 object-contain lg:w-96"
+                  />
+                </Link>
+
+                <Dialog
+                  open={location.hash == `#photo-${i}`}
+                  onClose={close}
+                  className="relative z-50"
+                >
+                  <div className="fixed inset-0 flex w-screen items-center justify-center bg-black/50 p-4">
+                    <DialogPanel className="max-w-lg">
+                      <img
+                        src={image.path}
+                        alt={live.name}
+                        className="block w-full object-contain"
+                      />
+                      <p className="text-right text-xs font-semibold text-white/80">
+                        <Link
+                          to={image.ref}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="inline-flex items-center gap-1"
+                        >
+                          <span>画像の引用元</span>
+                          <BsBoxArrowUpRight />
+                        </Link>
+                      </p>
+                    </DialogPanel>
+                  </div>
+                </Dialog>
               </SwiperSlide>
             ))}
           </Swiper>
