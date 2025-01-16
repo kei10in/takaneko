cfb3c2eb0fbd00656c4b5f7864ffb8a79d015c0d
Keisuke Ijuin <kei10in@gmail.com>
2025-01-16 21:51:08 +0900

    春野莉々誕生日記念グッズを追加。

---
+++ app/features/products/birthday/2025-01-16_春野莉々誕生日記念.ts
@@ -0,0 +1,66 @@
+import { BirthdayGoodsCollection, OfficialGoods } from "../product";
+
+export const 契約結べるアクスタ: OfficialGoods = {
+  slug: "契約結べるアクスタ",
+  name: "契約結べるアクスタ",
+  date: "2025-01-16",
+  listPrice: 1500,
+  description: "王様になりたいので、従者募集中です。募集条件: すぷりんにゃーの部下になれる方",
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2025-01-16_契約結べるアクスタ.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const 本気出したTシャツ: OfficialGoods = {
+  slug: "本気出したTシャツ",
+  name: "本気出したTシャツ",
+  date: "2025-01-16",
+  description:
+    "春野が描いた本気の絵です。裏にはたかねこちゃんが描かれています！どの子が誰か当ててみてね。表もちゃんと自分で書いてるんだよ！！頑張りました",
+  listPrice: 4000,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2025-01-16_本気出したTシャツ_1.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+    {
+      path: "/takaneko/birthday-goods/2025-01-16_本気出したTシャツ_2.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+    {
+      path: "/takaneko/birthday-goods/2025-01-16_本気出したTシャツ_3.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const 生写真: OfficialGoods = {
+  slug: "生写真",
+  name: "生写真 (ランダム10種)",
+  date: "2025-01-16",
+  description:
+    "ここでしか見れない写真が10種類も！中にはTシャツの絵とリンクしている写真もあります！ランダムでサイン入り生写真も封入するよー！推し活にぜひ",
+  listPrice: 2000,
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2025-01-16_生写真.webp",
+      ref: "https://takanenonadeshiko-ec.com/",
+    },
+  ],
+};
+
+export const 春野莉々誕生日記念_2025年01月16日: BirthdayGoodsCollection = {
+  slug: "春野莉々誕生日記念 2025年01月16日",
+  name: "春野莉々誕生日記念 2025年01月16日",
+  memberName: "春野莉々",
+  lineup: [契約結べるアクスタ, 本気出したTシャツ, 生写真],
+  images: [
+    {
+      path: "/takaneko/birthday-goods/2025-01-16_春野莉々誕生日記念グッズ.jpg",
+      ref: "https://x.com/takanenofficial/status/1879861156036370689",
+    },
+  ],
+};
--- app/features/products/birthdayGoods.ts
+++ app/features/products/birthdayGoods.ts
@@ -3,9 +3,11 @@ import { 松本ももな誕生日記念_2024年10月12日 } from "./birthday/202
 import { 日向端ひな誕生日記念_2024年10月30日 } from "./birthday/2024-10-30_日向端ひな誕生日記念";
 import { 星谷美来誕生日記念_2024年11月6日 } from "./birthday/2024-11-06_星谷美来誕生日記念";
 import { 城月菜央誕生日記念_2024年12月25日 } from "./birthday/2024-12-25_城月菜央誕生日記念";
+import { 春野莉々誕生日記念_2025年01月16日 } from "./birthday/2025-01-16_春野莉々誕生日記念";
 import { BirthdayGoodsCollection } from "./product";
 
 export const BirthdayGoods: BirthdayGoodsCollection[] = [
+  春野莉々誕生日記念_2025年01月16日,
   城月菜央誕生日記念_2024年12月25日,
   星谷美来誕生日記念_2024年11月6日,
   日向端ひな誕生日記念_2024年10月30日,
