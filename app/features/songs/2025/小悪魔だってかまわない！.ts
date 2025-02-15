3520bc174318094889b26cce1d5ce7d731c6202e
Keisuke Ijuin <kei10in@gmail.com>
2025-02-15 15:17:40 +0900

    楽曲を更新

---
+++ app/features/songs/2025/小悪魔だってかまわない！.ts
@@ -0,0 +1,17 @@
+import { SongMetaDescriptor } from "../types";
+
+export const 小悪魔だってかまわない: SongMetaDescriptor = {
+  slug: "小悪魔だってかまわない！",
+  name: "小悪魔だってかまわない！",
+  image: {
+    path: "/takaneko/songs/2025/小悪魔だってかまわない！.webp",
+    ref: "https://www.youtube.com/watch?v=OZycs0TBwKY",
+  },
+  officialSite: "",
+  youtube: [
+    {
+      text: "MV",
+      videoId: "OZycs0TBwKY",
+    },
+  ],
+};
