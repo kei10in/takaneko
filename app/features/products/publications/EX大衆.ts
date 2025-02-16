1e8a0bfb7b712e2711f300f6f7bdd6530ac13c22
Keisuke Ijuin <kei10in@gmail.com>
2025-02-17 00:04:05 +0900

    雑誌を追加。

--- app/features/products/publications/EX大衆.ts
+++ app/features/products/publications/EX大衆.ts
@@ -1,5 +1,41 @@
 import { Publication } from "../product";
 
+export const EX大衆_2025年03月号: Publication = {
+  slug: "EX大衆 2025年3月号",
+  name: "EX大衆 2025年3月号",
+  date: "2025-02-15",
+  kind: "magazines", // "books"
+  publisher: "双葉社",
+  listPrice: 909,
+  priceWithTax: 1000,
+  code: [{ kind: "JAN", value: "4910117710357" }],
+  url: "https://www.futabasha.co.jp/book/magazine/49101177103570000000",
+  coverImages: [
+    {
+      path: "/publications/2025/2025-02-15_EX大衆 2025年3月号.jpg",
+      ref: "https://x.com/EXtaishu/status/1887743343825768691",
+    },
+    {
+      path: "/publications/2025/2025-02-15_EX大衆 2025年3月_告知.jpg",
+      ref: "https://x.com/EXtaishu/status/1890217369102078259",
+    },
+  ],
+  featuredMembers: [
+    "城月菜央",
+    "涼海すう",
+    "橋本桃呼",
+    "葉月紗蘭",
+    "春野莉々",
+    "東山恵里沙",
+    "日向端ひな",
+    "星谷美来",
+    "松本ももな",
+    "籾山ひめり",
+  ],
+  officialTwitter: "https://x.com/takanenofficial/status/1890596899947684114",
+  links: [{ text: "EX大衆 X 告知", url: "https://x.com/EXtaishu/status/1890217369102078259" }],
+};
+
 export const EX大衆_2024年12月号: Publication = {
   slug: "EX大衆 2024年12月号",
   name: "EX大衆 2024年12月号",
