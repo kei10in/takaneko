9755bcd069acbd12280317b1da2d47b03c563d40
Keisuke Ijuin <kei10in@gmail.com>
2025-02-19 21:32:07 +0900

    城月菜央ウェブサイトへのリンクを追加。

--- app/routes/members.nao-kizuki.tsx
+++ app/routes/members.nao-kizuki.tsx
@@ -1,4 +1,4 @@
-import { MetaFunction } from "@remix-run/react";
+import { Link, MetaFunction } from "@remix-run/react";
 import { SITE_TITLE } from "~/constants";
 import { MemberProfile } from "~/routes/members/MemberProfile";
 import { NaoKizuki } from "./members/members";
@@ -17,6 +17,14 @@ export default function Index() {
   return (
     <div className="container mx-auto">
       <MemberProfile profile={NaoKizuki} />
+      <section className="mb-20 mt-12 px-4">
+        <h2 className="mb-4 text-center text-3xl font-bold text-gray-400">Link</h2>
+        <div className="text-center">
+          <Link to="https://kizukinao.my.canva.site/" target="_blank" rel="noopener noreferrer">
+            城月菜央ウェブサイト
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
