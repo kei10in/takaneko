697ec3ca54fa59080d715e7e8fa18eadfc3ce5ca
Keisuke Ijuin <kei10in@gmail.com>
2025-03-02 22:51:16 +0900

    Apply future flag

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
           v3_throwAbortReason: true,
+          v3_lazyRouteDiscovery: true,
         },
         buildEnd: (args) => {
           const buildPath = args.viteConfig.build.outDir;
