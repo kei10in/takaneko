dede7ae594e70f48da948c50c3158bc600c4c21e
Keisuke Ijuin <kei10in@gmail.com>
2025-04-08 16:29:32 +0900

    アプリ系のページからフッターを削除

--- app/routes/_app/route.tsx
+++ app/routes/_app/route.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import { Outlet } from "react-router";
-import { Footer } from "~/components/Footer";
 
 export default function AppLayout() {
   return (
@@ -8,7 +7,6 @@ export default function AppLayout() {
       <div className="min-h-[calc(100svh-var(--header-height))]">
         <Outlet />
       </div>
-      <Footer />
     </Fragment>
   );
 }
