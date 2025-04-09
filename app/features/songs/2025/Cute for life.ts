3696418d19689a910ed706d44df8ded023e31b14
Keisuke Ijuin <kei10in@gmail.com>
2025-04-09 22:13:41 +0900

    Cute for life の情報を更新

--- app/features/songs/2025/Cute for life.ts
+++ app/features/songs/2025/Cute for life.ts
@@ -3,7 +3,15 @@ import { SongMetaDescriptor } from "../types";
 export const CuteForLife: SongMetaDescriptor = {
   slug: "Cute for life",
   name: "Cute for life",
-  image: undefined,
+  image: {
+    path: "/takaneko/songs/2025/Cute for life.webp",
+    ref: "https://music.youtube.com/playlist?list=OLAK5uy_lBvE2GoZprXRl5YYNOOED0igtrhvOzzUs",
+  },
   officialSite: "",
-  youtube: [],
+  youtube: [
+    {
+      text: "MV",
+      videoId: "9tN1esSj2Tg",
+    },
+  ],
 };
