18ff487552d1819f2c2fef4fca29e39b9ddc899b
Keisuke Ijuin <kei10in@gmail.com>
2025-04-11 23:05:11 +0900

    画像検索に乗せない対応。

--- app/root.tsx
+++ app/root.tsx
@@ -42,6 +42,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#fffcfd" />
         <Meta />
+        <meta name="robots" content="noimageindex" />
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml " />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
