3bde1205cf056401fa0c64a258ebce8be5874e73
Keisuke Ijuin <kei10in@gmail.com>
2025-04-13 23:22:33 +0900

    文言を更新

--- app/routes/_site.media._index/route.tsx
+++ app/routes/_site.media._index/route.tsx
@@ -18,13 +18,13 @@ import { validateYouTubeOEmbedResponse } from "~/utils/youtube/youtubeOEmbed";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: formatTitle("メディアへの出演情報") },
+    { title: formatTitle("メディア") },
     {
       name: "description",
       content:
         "高嶺のなでしこ (たかねこ) メンバー、 " +
         "涼海すう / 城月菜央 / 橋本桃呼 / 葉月紗蘭 / 春野莉々 / 東山恵里沙 / 日向端ひな / 星谷美来 / 松本ももな / 籾山ひめり、" +
-        "が出演している YouTube やウェブ記事を一覧にしました。",
+        "が出演しているメディア、YouTube やウェブ記事を一覧にしました。",
     },
   ];
 };
@@ -136,9 +136,7 @@ export default function MediaIndex() {
   return (
     <div className="container mx-auto max-w-3xl">
       <section className="p-4">
-        <h1 className="text-nadeshiko-800 my-4 text-5xl font-semibold lg:mt-12">
-          メディアへの出演情報
-        </h1>
+        <h1 className="text-nadeshiko-800 my-4 text-5xl font-semibold lg:mt-12">メディア</h1>
         <ul className="max-w-2xl space-y-6 py-2">
           {items.map((video) => {
             return (
