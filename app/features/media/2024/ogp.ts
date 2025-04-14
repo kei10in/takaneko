32b917f60a5d2a3b6cd8313d74c1008d34f195fa
Keisuke Ijuin <kei10in@gmail.com>
2025-04-14 23:04:25 +0900

    Podcast で聞けるメディアコンテンツを追加。

--- app/features/media/2024/ogp.ts
+++ app/features/media/2024/ogp.ts
@@ -8,6 +8,18 @@ export const Ogp2024: OgpMediaDescriptor[] = [
     presents: ["日向端ひな"],
   },
   {
+    mediaUrl: "https://www.radionikkei.jp/toraken/42_takakneko.html",
+    publishedAt: "2024-12-11",
+    category: "audio",
+    presents: ["松本ももな", "籾山ひめり"],
+  },
+  {
+    mediaUrl: "https://www.radionikkei.jp/toraken/41nadeshiko.html",
+    publishedAt: "2024-12-04",
+    category: "audio",
+    presents: ["松本ももな", "籾山ひめり"],
+  },
+  {
     mediaUrl: "https://abema.tv/video/episode/88-77_s1_p281",
     publishedAt: "2024-10-18",
     category: "video",
@@ -18,5 +30,17 @@ export const Ogp2024: OgpMediaDescriptor[] = [
     publishedAt: "2024-10-11",
     category: "video",
     presents: ["橋本桃呼"],
+  },
+  {
+    mediaUrl: "https://www.radionikkei.jp/toraken/3.html",
+    publishedAt: "2024-02-29",
+    category: "audio",
+    presents: ["葉月紗蘭", "東山恵里沙"],
+  },
+  {
+    mediaUrl: "https://www.radionikkei.jp/toraken/2.html",
+    publishedAt: "2024-02-22",
+    category: "audio",
+    presents: ["葉月紗蘭", "東山恵里沙"],
   },
 ];
