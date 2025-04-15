4468fca646b4904909befe74e0933df7090ad838
Keisuke Ijuin <kei10in@gmail.com>
2025-04-15 23:13:55 +0900

    記事を追加。

--- app/features/media/2025/ogp.ts
+++ app/features/media/2025/ogp.ts
@@ -2,6 +2,12 @@ import { OgpMediaDescriptor } from "../types";
 
 export const Ogp2025: OgpMediaDescriptor[] = [
   {
+    mediaUrl: "https://natalie.mu/music/pp/churaku-takaneko02",
+    publishedAt: "2025-03-28",
+    category: "article",
+    presents: ["涼海すう", "葉月紗蘭", "東山恵里沙"],
+  },
+  {
     mediaUrl: "https://popnroll.tv/articles/42189",
     publishedAt: "2025-01-27",
     siteName: "Pop'n'Roll",
