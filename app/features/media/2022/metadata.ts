c907351a815d9514685567d35eb29a0f8e77b663
Keisuke Ijuin <kei10in@gmail.com>
2025-04-19 15:32:44 +0900

    メタデータは静的に持つ。

---
+++ app/features/media/2022/metadata.ts
@@ -0,0 +1,34 @@
+import { MediaDetails } from "~/features/media/types";
+
+export const metadata2022: MediaDetails[] = [
+  {
+    kind: "youtube",
+    title: "#22【本大会まとめ⑩】学生フォーミュラ日本大会：リポーターが伝える会場の様子",
+    authorName: "(公社)自動車技術会",
+    publishedAt: "2022-12-27",
+    mediaUrl: "https://youtu.be/d3uN2mkIovE",
+    imageUrl: "https://i.ytimg.com/vi/d3uN2mkIovE/hqdefault.jpg",
+    category: "youtube",
+    presents: ["籾山ひめり", "松本ももな"],
+  },
+  {
+    kind: "youtube",
+    title: "#11【2022年大会参戦チームのリアル】東京農工大学③_学生フォーミュラ日本大会",
+    authorName: "(公社)自動車技術会",
+    publishedAt: "2022-10-06",
+    mediaUrl: "https://youtu.be/Jqjk6IZE3qk",
+    imageUrl: "https://i.ytimg.com/vi/Jqjk6IZE3qk/hqdefault.jpg",
+    category: "youtube",
+    presents: ["橋本桃呼", "松本ももな"],
+  },
+  {
+    kind: "youtube",
+    title: "#10【2022年大会参戦チームのリアル】豊橋技術科学大学②_学生フォーミュラ日本大会",
+    authorName: "(公社)自動車技術会",
+    publishedAt: "2022-09-29",
+    mediaUrl: "https://youtu.be/lu0x7gQKNSQ",
+    imageUrl: "https://i.ytimg.com/vi/lu0x7gQKNSQ/hqdefault.jpg",
+    category: "youtube",
+    presents: ["籾山ひめり"],
+  },
+];
