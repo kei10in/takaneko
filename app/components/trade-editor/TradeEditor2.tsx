f3d3d5cb40217f74fb17b8ee7e0c2d1b7a6223ed
Keisuke Ijuin <kei10in@gmail.com>
2025-04-20 09:24:25 +0900

    ボーダーの色を修正

--- app/components/trade-editor/TradeEditor2.tsx
+++ app/components/trade-editor/TradeEditor2.tsx
@@ -305,12 +305,10 @@ export const TradeEditor2: React.FC<Props> = (props: Props) => {
         onClose={() => setShowConfirmClear(false)}
       >
         <div className="fixed inset-0 flex w-screen items-center justify-center bg-black/50 p-4 backdrop-blur-xs">
-          <DialogPanel className="max-w-lg min-w-64 border bg-white text-lg text-gray-700">
+          <DialogPanel className="max-w-lg min-w-64 divide-y divide-gray-200 border border-gray-200 bg-white text-lg text-gray-700">
             <div className="flex w-full items-center justify-between p-4">
               <p className="text-center">トレード設定をクリアしますか？</p>
             </div>
-
-            <hr />
 
             <CloseButton
               className="block w-full p-2 text-center font-bold text-red-500 hover:bg-gray-100"
@@ -318,8 +316,6 @@ export const TradeEditor2: React.FC<Props> = (props: Props) => {
             >
               クリアする
             </CloseButton>
-
-            <hr />
 
             <CloseButton className="block w-full p-2 text-center font-bold text-blue-500 hover:bg-gray-100">
               キャンセル
@@ -331,7 +327,7 @@ export const TradeEditor2: React.FC<Props> = (props: Props) => {
       {/* Preview Dialog */}
       <Dialog open={preview} className="relative z-50" onClose={() => setPreview(false)}>
         <div className="fixed inset-0 flex w-screen items-center justify-center bg-black/50 p-4 backdrop-blur-xs">
-          <DialogPanel className="max-w-lg border bg-white p-4">
+          <DialogPanel className="max-w-lg border border-gray-200 bg-white p-4">
             <TradeImagePreview productImage={productImage} tradeDescriptions={tradeDescriptions} />
             {/* <img alt="Preview" className="mx-auto" src={stageRef.current?.toDataURL()} /> */}
           </DialogPanel>
