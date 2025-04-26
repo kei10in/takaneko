009e8bc9ae8afe292f6bd7a220e3adc302a402f5
Keisuke Ijuin <kei10in@gmail.com>
2025-04-26 10:12:22 +0900

    サムネイル用の画像を使う。

--- app/routes/_site.products.publications._index/PublicationCard.tsx
+++ app/routes/_site.products.publications._index/PublicationCard.tsx
@@ -5,15 +5,21 @@ interface Props {
   name: string;
   date: NaiveDate;
   image: string;
+  imageSet?: string;
 }
 
 export const PublicationCard: React.FC<Props> = (props: Props) => {
-  const { name, date, image } = props;
+  const { name, date, image, imageSet } = props;
 
   return (
     <div className="w-full bg-white shadow-sm">
       <div className="aspect-square w-full flex-0">
-        <img src={image} alt={name} className="h-full w-full object-contain object-center" />
+        <img
+          src={image}
+          srcSet={imageSet}
+          alt={name}
+          className="h-full w-full object-contain object-center"
+        />
       </div>
       <div className="space-y-1 bg-gray-50 px-4 py-2">
         <div className="border-nadeshiko-800 text-nadeshiko-800 w-fit border px-2 py-px text-sm leading-none">
