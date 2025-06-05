6aa4e44ce522cc34cbca7e42a4855f6c4825b2e8
Keisuke Ijuin <kei10in@gmail.com>
2025-06-05 22:32:27 +0900

    楽曲にアイドル衣装を追加。

---
+++ app/features/songs/2025/アイドル衣装.ts
@@ -0,0 +1,17 @@
+import { SongMetaDescriptor } from "../types";
+
+export const アイドル衣装: SongMetaDescriptor = {
+  slug: "アイドル衣装",
+  name: "アイドル衣装",
+  image: {
+    path: "",
+    ref: "",
+  },
+  officialSite: "https://takanenonadeshiko.jp/?p=3841",
+  youtube: [
+    {
+      text: "MV",
+      videoId: "a7CAn_FIfSk",
+    },
+  ],
+};
--- app/features/songs/songs.ts
+++ app/features/songs/songs.ts
@@ -40,6 +40,7 @@ import { 私は怪物 } from "./2024/私は怪物";
 import { 私より好きでいて } from "./2024/私より好きでいて";
 import { 美しく生きろ } from "./2024/美しく生きろ";
 import { CuteForLife } from "./2025/Cute for life";
+import { アイドル衣装 } from "./2025/アイドル衣装";
 import { メランコリックハニー } from "./2025/メランコリックハニー";
 import { 小悪魔だってかまわない } from "./2025/小悪魔だってかまわない！";
 import { 東京サニーパーティー } from "./2025/東京サニーパーティー";
@@ -47,6 +48,7 @@ import { SongMetaDescriptor } from "./types";
 
 export const ALL_SONGS: SongMetaDescriptor[] = [
   // 2025
+  アイドル衣装,
   メランコリックハニー,
   東京サニーパーティー,
   CuteForLife,
