f8f596af3cceb92ab7b97ce19fbeab2b30ebf6f9
Keisuke Ijuin <kei10in@gmail.com>
2025-06-08 08:51:13 +0900

    曲名を修正

--- app/features/songs/2024/センパイ.ts
+++ app/features/songs/2024/センパイ.ts
@@ -2,7 +2,7 @@ import { SongMetaDescriptor } from "../types";
 
 export const センパイ: SongMetaDescriptor = {
   slug: "センパイ",
-  name: "センパイ",
+  name: "センパイ。",
   image: {
     path: "/takaneko/songs/2024/センパイ.webp",
     ref: "https://music.youtube.com/watch?v=HwSVyD1Vs8M",
