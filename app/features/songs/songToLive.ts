1fac6321341f6f980d1d54936e874cf18bfd982b
Keisuke Ijuin <kei10in@gmail.com>
2025-06-11 20:06:05 +0900

    複数回公演のあるライブの表示に対応

--- app/features/songs/songToLive.ts
+++ app/features/songs/songToLive.ts
@@ -1,4 +1,5 @@
 import { EventModule } from "../events/eventModule";
+import { EventRecap } from "../events/eventRecap";
 import { ALL_EVENTS } from "../events/events";
 
 export const makeSongToLiveMapFromAllEvents = () => {
@@ -27,3 +28,39 @@ export const makeSongToLiveMapFromAllEvents = () => {
 };
 
 export const SongToLiveMap = makeSongToLiveMapFromAllEvents();
+
+export const makeSongToLiveMapFromAllEvents2 = () => {
+  const result: Record<string, { recaps: EventRecap[]; event: EventModule }[]> = {};
+
+  Object.values(ALL_EVENTS).forEach((event) => {
+    const { meta } = event;
+    if (meta.recaps == undefined) {
+      return;
+    }
+
+    const recapMap: Record<string, EventRecap[]> = {};
+    meta.recaps.flatMap((recap) => {
+      recap.setlist
+        .flatMap((x) => x.songs)
+        .forEach((song) => {
+          if (!recapMap[song]) {
+            recapMap[song] = [];
+          }
+
+          recapMap[song].push(recap);
+        });
+    });
+
+    Object.entries(recapMap).forEach(([song, recaps]) => {
+      if (!result[song]) {
+        result[song] = [];
+      }
+
+      result[song].push({ recaps, event });
+    });
+  });
+
+  return result;
+};
+
+export const SongToLiveMap2 = makeSongToLiveMapFromAllEvents2();
