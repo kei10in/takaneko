a2fe4cb546adac75fdf18793c2cb239f23e55612
Keisuke Ijuin <kei10in@gmail.com>
2025-06-21 20:07:00 +0900

    関数をインライン化

--- app/vite/thumbnailsBuilder.ts
+++ app/vite/thumbnailsBuilder.ts
@@ -3,21 +3,19 @@ import path from "node:path";
 import { Plugin } from "vite";
 
 export const thumbnailsBuilder = (): Plugin => {
-  const buildCalendar = () => {
-    const script = path.resolve(__dirname, "..", "..", "scripts", "gen-thumbnails.ts");
-    const result = spawnSync("pnpm", ["tsx", script], { shell: true });
-    if (result.error) {
-      throw result.error;
-    }
-    if (result.status != 0) {
-      throw new Error(result.stderr.toString());
-    }
-  };
-
   return {
     name: "takanekono/build-thumbnails",
+
     buildEnd: () => {
-      buildCalendar();
+      const script = path.resolve(__dirname, "..", "..", "scripts", "gen-thumbnails.ts");
+
+      const result = spawnSync("pnpm", ["tsx", script], { shell: true });
+      if (result.error) {
+        throw result.error;
+      }
+      if (result.status != 0) {
+        throw new Error(result.stderr.toString());
+      }
     },
   };
 };
