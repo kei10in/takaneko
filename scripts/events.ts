297caf30acf3f5d1c9744b87279177c0cbb3407a
Keisuke Ijuin <kei10in@gmail.com>
2025-06-22 19:30:33 +0900

    セトリの CSV を出力するスクリプトを実装

--- scripts/events.ts
+++ scripts/events.ts
@@ -1,5 +1,6 @@
 import { glob } from "glob";
-import { basename } from "node:path";
+import path, { basename } from "node:path";
+import { EventModule } from "~/features/events/eventModule";
 import { EventMeta, validateEventMeta } from "~/features/events/meta";
 import { stem } from "~/utils/string";
 
@@ -20,4 +21,20 @@ export const listAllEventFiles = async () => {
 export const loadEventMeta = async (file: string): Promise<EventMeta | undefined> => {
   const module = await import(file);
   return validateEventMeta(module.meta);
+};
+
+export const loadAllEventsAsEventModule = async (): Promise<Record<string, EventModule>> => {
+  const files = await listAllEventFiles();
+  const events = (
+    await Promise.all(
+      files.map(async (f) => {
+        const meta = await loadEventMeta(f);
+        const filename = path.basename(f);
+        const slug = stem(f);
+        return meta ? [[filename, { slug, filename, meta, Content: () => null }]] : [];
+      }),
+    )
+  ).flatMap((x) => x);
+
+  return Object.fromEntries(events);
 };
---
+++ scripts/setlist-db.ts
@@ -0,0 +1,38 @@
+import * as csv from "@fast-csv/format";
+import { register } from "node:module";
+import * as process from "node:process";
+import { makeSongToLiveMap } from "~/features/songs/songActivities";
+import { ALL_SONGS } from "~/features/songs/songs";
+import { loadAllEventsAsEventModule } from "./events";
+
+register("@mdx-js/node-loader", import.meta.url);
+
+const main = async () => {
+  const events = await loadAllEventsAsEventModule();
+  const songToLiveMap = makeSongToLiveMap(Object.values(events), ALL_SONGS);
+
+  const records = Object.values(songToLiveMap).flatMap((summary) => {
+    return summary.events.flatMap((event) => {
+      return event.recaps.map((recap) => {
+        return { song: summary.name, event: event.event, recap };
+      });
+    });
+  });
+
+  const csvStream = csv.format({ headers: true });
+
+  csvStream.pipe(process.stdout).on("end", () => process.exit());
+
+  records.forEach((record) => {
+    csvStream.write({
+      song: record.song,
+      date: record.event.meta.date,
+      region: record.event.meta.region,
+      location: record.event.meta.location,
+      event: record.event.meta.title,
+      part: record.recap.title,
+    });
+  });
+};
+
+main();
