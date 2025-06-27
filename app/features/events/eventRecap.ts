11482f6cc6bfedbb3547c8587c309d3228d34136
Keisuke Ijuin <kei10in@gmail.com>
2025-06-27 20:38:04 +0900

    使われなくなった型、フィールドを削除

--- app/features/events/eventRecap.ts
+++ app/features/events/eventRecap.ts
@@ -2,20 +2,9 @@ import { z } from "zod";
 import { LinkDescription } from "~/utils/types/LinkDescription";
 import { parseStagePlan, StagePart } from "./stagePlan";
 
-const PerformanceDescription = z.object({
-  costume: z.string().optional(),
-  songs: z.array(z.string()),
-});
-
-export type PerformanceDescription = z.infer<typeof PerformanceDescription>;
-
 export const EventRecapDescription = z.object({
   title: z.string().optional(),
   stagePlan: z.array(z.string()).optional(),
-  costume: z.union([z.string(), z.array(z.string())]).optional(),
-  setlist: z
-    .union([z.array(z.string()), z.array(PerformanceDescription), PerformanceDescription])
-    .optional(),
 
   // みくるんの #たかねこセトリを指定します。
   url: z.string().optional(),
@@ -27,7 +16,6 @@ type EventRecapDescription = z.infer<typeof EventRecapDescription>;
 export interface EventRecap {
   title?: string | undefined;
   stagePlan: StagePart[];
-  setlist: PerformanceDescription[];
   links: LinkDescription[];
 }
 
@@ -48,39 +36,13 @@ export const validateEventRecapDescription = (
   const recaps = Array.isArray(data) ? data : [data];
 
   return recaps.flatMap((recap) => {
-    const { title, costume, setlist, url, links } = recap;
+    const { title, url, links } = recap;
 
     //
     // Validate stagePlan field
     //
     const stagePlan = recap.stagePlan ?? [];
     const validatedStagePlan = parseStagePlan(stagePlan);
-
-    //
-    // Validate setlist field
-    //
-    const validatedSetlist = (() => {
-      if (setlist == undefined) {
-        return [];
-      }
-
-      const sl = Array.isArray(setlist) ? setlist : [setlist];
-
-      if (sl.length === 0) {
-        return [];
-      }
-
-      if (typeof sl[0] === "string") {
-        const validatedCostumes =
-          costume == undefined ? [] : Array.isArray(costume) ? costume : [costume];
-        const validatedCostume = validatedCostumes[0];
-        // setlist は string[] を期待していいが推論されない。
-        return [{ costume: validatedCostume, songs: sl as string[] }];
-      }
-
-      // setlist は PerformanceDescription[] であると期待できるが推論が効かない。
-      return sl as PerformanceDescription[];
-    })();
 
     //
     // Validate links field
@@ -104,7 +66,7 @@ export const validateEventRecapDescription = (
 
     if (
       title == undefined &&
-      validatedSetlist.length === 0 &&
+      validatedStagePlan.length === 0 &&
       linkDescriptionsForUrl.length === 0 &&
       validatedLinks.length === 0
     ) {
@@ -115,7 +77,6 @@ export const validateEventRecapDescription = (
       {
         title,
         stagePlan: validatedStagePlan,
-        setlist: validatedSetlist,
         links: [...linkDescriptionsForUrl, ...validatedLinks],
       },
     ];
