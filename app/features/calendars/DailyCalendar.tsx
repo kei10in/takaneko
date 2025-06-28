d26b11193868d846ff445c2c8a98242caef794ea
Keisuke Ijuin <kei10in@gmail.com>
2025-06-28 19:58:17 +0900

    デイリーの予定のページにパンくずリストを追加。

--- app/features/calendars/DailyCalendar.tsx
+++ app/features/calendars/DailyCalendar.tsx
@@ -1,11 +1,12 @@
 import { useMemo } from "react";
+import { BsChevronRight } from "react-icons/bs";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { Link } from "react-router";
-import { displayDateWithDayOfWeek } from "~/utils/dateDisplay";
+import { displayDateWithDayOfWeek, displayMonth } from "~/utils/dateDisplay";
 import { NaiveDate } from "~/utils/datetime/NaiveDate";
 import { loadEventsInDay } from "../events/events";
 import { CalendarEventItem } from "./CalendarEventItem";
-import { dateHref } from "./utils";
+import { calendarMonthHref, dateHref } from "./utils";
 
 interface Props {
   year: number;
@@ -22,12 +23,25 @@ export const DailyCalendar: React.FC<Props> = (props: Props) => {
   }, [day, month, year]);
 
   const d = new NaiveDate(year, month, day);
+  const m = d.naiveMonth();
 
   return (
-    <div className="container mx-auto min-h-[calc(100svh-var(--header-height)-3rem)] p-4">
-      <div className="mx-auto max-w-2xl space-y-2">
-        <div className="flex items-center justify-between">
-          <h1 className="px-2 text-lg font-bold">{displayDateWithDayOfWeek(d)}</h1>
+    <div className="container mx-auto min-h-[calc(100svh-var(--header-height)-3rem)] px-4">
+      <div className="mx-auto max-w-2xl space-y-4">
+        <div className="py-2">
+          <p className="flex items-center gap-2 text-sm">
+            <Link className="text-nadeshiko-800 font-semibold" to="/calendar">
+              スケジュール
+            </Link>
+            <BsChevronRight className="inline-block" />
+            <Link className="text-nadeshiko-800 font-semibold" to={calendarMonthHref(m)}>
+              {displayMonth(m)}
+            </Link>
+          </p>
+        </div>
+
+        <div className="my-4 flex items-center justify-between">
+          <h1 className="text-lg font-bold">{displayDateWithDayOfWeek(d)}</h1>
           <div className="flex h-8 w-36 items-stretch divide-x divide-gray-200 overflow-hidden rounded-md border border-gray-200">
             <Link
               className="inline-flex h-full grow items-center justify-center"
@@ -52,6 +66,7 @@ export const DailyCalendar: React.FC<Props> = (props: Props) => {
             </Link>
           </div>
         </div>
+
         <div>
           {calendarEvents.length !== 0 ? (
             calendarEvents.map((event) => (
@@ -65,7 +80,7 @@ export const DailyCalendar: React.FC<Props> = (props: Props) => {
               </Link>
             ))
           ) : (
-            <div className="mx-auto w-fit px-2 py-4 text-gray-800">予定はありません。</div>
+            <div className="mx-auto w-fit py-4 text-gray-800">予定はありません。</div>
           )}
         </div>
       </div>
