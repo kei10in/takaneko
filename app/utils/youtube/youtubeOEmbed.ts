23f83e30a7abc3a6e709ce8f35da10479c04cd0d
Keisuke Ijuin <kei10in@gmail.com>
2025-07-03 23:19:59 +0900

    Fix deprecated

--- app/utils/youtube/youtubeOEmbed.ts
+++ app/utils/youtube/youtubeOEmbed.ts
@@ -3,16 +3,16 @@ import { z } from "zod/v4";
 export const YouTubeOEmbedResponseSchema = z.object({
   title: z.string(),
   author_name: z.string(),
-  author_url: z.string().url(),
+  author_url: z.url(),
   type: z.literal("video"),
   height: z.number().optional(),
   width: z.number().optional(),
   version: z.string(),
   provider_name: z.literal("YouTube"),
-  provider_url: z.string().url(),
+  provider_url: z.url(),
   thumbnail_height: z.number(),
   thumbnail_width: z.number(),
-  thumbnail_url: z.string().url(),
+  thumbnail_url: z.url(),
   html: z.string(),
 });
 
