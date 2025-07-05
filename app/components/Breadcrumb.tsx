afb387418cce1d87dc8b8246a3e38eba05af7e3b
Keisuke Ijuin <kei10in@gmail.com>
2025-07-05 21:46:19 +0900

    レイアウトを調整

--- app/components/Breadcrumb.tsx
+++ app/components/Breadcrumb.tsx
@@ -13,13 +13,15 @@ export interface BreadcrumbProps {
 
 export function Breadcrumb({ items }: BreadcrumbProps) {
   return (
-    <p className="flex items-center gap-2 text-sm">
+    <p className="flex items-center gap-1.5 text-sm">
       {items.map((item, index) => (
         <Fragment key={index}>
           <Link className="text-nadeshiko-800 font-semibold" to={item.to}>
             {item.label}
           </Link>
-          {index < items.length - 1 && <BsChevronRight className="inline-block" />}
+          {index < items.length - 1 && (
+            <BsChevronRight className="inline-block h-3 text-gray-600" />
+          )}
         </Fragment>
       ))}
     </p>
