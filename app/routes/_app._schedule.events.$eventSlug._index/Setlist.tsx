30d8f6b5dd39dd292b3a17a4e399a3dfebf176a9
Keisuke Ijuin <kei10in@gmail.com>
2025-07-08 21:19:32 +0900

    イベントページのセットリストに楽曲へのリンクを追加。消えてたのを修正

--- app/routes/_app._schedule.events.$eventSlug._index/Setlist.tsx
+++ app/routes/_app._schedule.events.$eventSlug._index/Setlist.tsx
@@ -1,6 +1,9 @@
+import { clsx } from "clsx";
 import { Fragment } from "react";
+import { BsChevronRight } from "react-icons/bs";
 import { Link } from "react-router";
 import { StagePart } from "~/features/events/setlist";
+import { ALL_SONGS } from "~/features/songs/songs";
 import { LinkDescription } from "~/utils/types/LinkDescription";
 
 interface Props {
@@ -13,44 +16,66 @@ export const Setlist: React.FC<Props> = (props: Props) => {
   const { title, setlist, links } = props;
 
   return (
-    <section className="m-6">
-      {title && <h3 className="m-2 pb-1 text-lg leading-tight font-semibold">{title}</h3>}
+    <section className="">
+      <h3 className="my-2 pb-1 text-lg leading-tight font-semibold text-gray-500">
+        {title ?? "セットリスト"}
+      </h3>
 
-      <ul className="">
-        {setlist.map((part, i) => {
-          if (part.kind == "costume") {
-            return null;
-          }
+      <div className="pl-4">
+        <ul className="divide-y divide-gray-100">
+          {setlist.map((part, i) => {
+            if (part.kind == "costume") {
+              return null;
+            }
 
-          const { n, name, desc } =
-            part.kind == "talk"
-              ? { n: "", name: "MC", desc: "" }
-              : part.kind == "song"
-                ? {
-                    n: `${part.index + 1}.`,
-                    name: part.songTitle,
-                    desc: part.costumeName || "衣装不明",
-                  }
-                : part.kind == "encore"
-                  ? { n: "", name: "アンコール", desc: "" }
-                  : { n: "", name: part.title || "企画", desc: part.costumeName || "衣装不明" };
+            const { n, name, desc } =
+              part.kind == "talk"
+                ? { n: "", name: "MC", desc: undefined }
+                : part.kind == "song"
+                  ? {
+                      n: `${part.index + 1}`,
+                      name: part.songTitle,
+                      desc: part.costumeName || "衣装不明",
+                    }
+                  : part.kind == "encore"
+                    ? { n: "", name: "アンコール", desc: undefined }
+                    : { n: "", name: part.title || "企画", desc: part.costumeName || "衣装不明" };
 
-          return (
-            <li key={i} className="">
-              <div className="flex min-h-8 items-center gap-3">
-                <div className="w-6 text-right font-semibold text-gray-400">{n}</div>
-                <div className="p-1">
-                  <div>{name}</div>
-                  <div className="text-xs text-gray-400">{desc}</div>
+            const slug =
+              part.kind == "song"
+                ? ALL_SONGS.find((track) => track.name === name)?.slug
+                : undefined;
+
+            const child = (
+              <div className={clsx("group flex items-center gap-3")}>
+                <p className="w-8 flex-none">
+                  <p className="flex-1 text-right font-semibold text-gray-400">{n}</p>
+                </p>
+                <div className="min-w-0 flex-1 px-2 py-2">
+                  <p className={clsx("line-clamp-1")}>{name}</p>
+                  {desc && <p className="line-clamp-1 text-xs text-gray-400">{desc}</p>}
                 </div>
+                {slug && (
+                  <div className="flex flex-none items-center justify-center px-1">
+                    <div className="flex items-center rounded-full p-1.5 text-gray-500 group-hover:bg-gray-100">
+                      <BsChevronRight className="h-4 w-4" />
+                    </div>
+                  </div>
+                )}
               </div>
-            </li>
-          );
-        })}
-      </ul>
+            );
+
+            return (
+              <li key={i}>
+                {slug == undefined ? child : <Link to={`/songs/${slug}`}>{child} </Link>}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
       {links.length > 0 && (
-        <p className="mt-4 pl-4 text-xs text-gray-400">
+        <p className="mt-4 pl-6 text-xs text-gray-400">
           出典:{" "}
           <Link
             className="text-nadeshiko-600"
