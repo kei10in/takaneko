87ffb4cdfe71f39faba14eda6610da49d79c5df3
Keisuke Ijuin <kei10in@gmail.com>
2025-07-13 15:50:19 +0900

    ビルド時のサムネイル生成をやめる。

--- app/vite/plugin.ts
+++ app/vite/plugin.ts
@@ -2,8 +2,7 @@ import { Plugin } from "vite";
 import { calendarBuilder } from "./calendarBuilder";
 import { croppingProductImages } from "./croppingProductImages";
 import { sitemapBuilder } from "./sitemapBuilder";
-import { thumbnailsBuilder } from "./thumbnailsBuilder";
 
 export const takanekono = (): Plugin[] => {
-  return [croppingProductImages(), calendarBuilder(), sitemapBuilder(), thumbnailsBuilder()];
+  return [croppingProductImages(), calendarBuilder(), sitemapBuilder()];
 };
