d74c4b7bb68ec8e8cdec230855f3b98a3083b3af
Keisuke Ijuin <kei10in@gmail.com>
2025-07-13 16:52:12 +0900

    ビルド時の画像クロッピングを廃止する。

--- app/vite/plugin.ts
+++ app/vite/plugin.ts
@@ -1,8 +1,7 @@
 import { Plugin } from "vite";
 import { calendarBuilder } from "./calendarBuilder";
-import { croppingProductImages } from "./croppingProductImages";
 import { sitemapBuilder } from "./sitemapBuilder";
 
 export const takanekono = (): Plugin[] => {
-  return [croppingProductImages(), calendarBuilder(), sitemapBuilder()];
+  return [calendarBuilder(), sitemapBuilder()];
 };
