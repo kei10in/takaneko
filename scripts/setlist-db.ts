b9fe3d264221c6f3606447035fb08b0b3d968cce
Keisuke Ijuin <kei10in@gmail.com>
2025-07-21 17:24:23 +0900

    セットリストデータを出力するスクリプトを追加。

--- scripts/setlist-db.ts
+++ scripts/setlist-db.ts
@@ -1,38 +1,85 @@
 import * as csv from "@fast-csv/format";
+import fs from "node:fs";
 import { register } from "node:module";
-import * as process from "node:process";
-import { makeSongToLiveMap } from "~/features/songs/songActivities";
-import { ALL_SONGS } from "~/features/songs/songs";
+import { SongSegment } from "~/features/events/setlist";
 import { loadAllEventsAsEventModule } from "./events";
 
 register("@mdx-js/node-loader", import.meta.url);
 
+interface Record {
+  section: string;
+  order: number;
+  song: string;
+  costume: string;
+  date: string;
+  event: string;
+  act: string;
+  region: string;
+  location: string;
+}
+
 const main = async () => {
   const events = await loadAllEventsAsEventModule();
-  const songToLiveMap = makeSongToLiveMap(Object.values(events), ALL_SONGS);
 
-  const records = Object.values(songToLiveMap).flatMap((summary) => {
-    return summary.events.flatMap((event) => {
-      return event.acts.map((act) => {
-        return { song: summary.name, event: event.event, act };
-      });
+  const records = Object.entries(events).flatMap(([_slug, event]) => {
+    return event.meta.acts.flatMap((act) => {
+      return act.setlist
+        .filter((p): p is SongSegment => p.kind == "song")
+        .map((segment) => {
+          return {
+            section: segment.section,
+            order: segment.index + 1,
+            song: segment.songTitle,
+            costume: segment.costumeName ?? "",
+            date: event.meta.date.toString(),
+            event: event.meta.summary,
+            act: act.title ?? "",
+            region: event.meta.region ?? "",
+            location: event.meta.location ?? "",
+          } satisfies Record;
+        });
     });
   });
 
+  fs.mkdirSync("public/data", { recursive: true });
+
+  writeAsCsv("public/data/setlists.csv", records);
+  writeAsCsvUtf8WithBOM("public/data/setlists_utf8-with-bom.csv", records);
+  fs.writeFileSync("public/data/setlists.json", JSON.stringify(records));
+};
+
+/**
+ * writeAsCsv は指定したファイルに CSV 形式でレコードを出力します。
+ */
+const writeAsCsv = (filepath: string, records: Record[]) => {
+  const writeStream = fs.createWriteStream(filepath);
+  try {
+    writeCsvToStream(writeStream, records);
+  } finally {
+    writeStream.close();
+  }
+};
+
+/**
+ * writeAsCsv は指定したファイルに CSV 形式でレコードを出力します。
+ */
+const writeAsCsvUtf8WithBOM = async (filepath: string, records: Record[]) => {
+  const writeStream = fs.createWriteStream(filepath);
+  const bom = Buffer.from([0xef, 0xbb, 0xbf]); // UTF-8 BOM
+  writeStream.write(bom);
+  try {
+    writeCsvToStream(writeStream, records);
+  } finally {
+    writeStream.close();
+  }
+};
+
+const writeCsvToStream = (writeStream: fs.WriteStream, records: Record[]) => {
   const csvStream = csv.format({ headers: true });
+  csvStream.pipe(writeStream);
 
-  csvStream.pipe(process.stdout).on("end", () => process.exit());
-
-  records.forEach((record) => {
-    csvStream.write({
-      song: record.song,
-      date: record.event.meta.date,
-      region: record.event.meta.region,
-      location: record.event.meta.location,
-      event: record.event.meta.title,
-      part: record.act.title,
-    });
-  });
+  records.forEach((record) => csvStream.write(record));
+  csvStream.end();
 };
 
 main();
