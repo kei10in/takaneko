dadcd5e9046ffdd0634f46f68130570b0ce3b548
Keisuke Ijuin <kei10in@gmail.com>
2025-07-21 17:40:52 +0900

    ビルド時にセットリストのデータセットを生成する

---
+++ app/vite/datasetBuilder.ts
@@ -0,0 +1,24 @@
+import { spawnSync } from "node:child_process";
+import path from "node:path";
+import { Plugin } from "vite";
+
+export const datasetBuilder = (): Plugin => {
+  const buildSetlistData = () => {
+    const buildSetlistDataScript = path.resolve(__dirname, "..", "..", "scripts", "setlist-db.ts");
+
+    const result = spawnSync("pnpm", ["tsx", buildSetlistDataScript], { shell: true });
+    if (result.error) {
+      throw result.error;
+    }
+    if (result.status != 0) {
+      throw new Error(result.stderr.toString());
+    }
+  };
+
+  return {
+    name: "takanekono/build-dataset",
+    buildEnd: () => {
+      buildSetlistData();
+    },
+  };
+};
--- app/vite/plugin.ts
+++ app/vite/plugin.ts
@@ -1,7 +1,8 @@
 import { Plugin } from "vite";
 import { calendarBuilder } from "./calendarBuilder";
+import { datasetBuilder } from "./datasetBuilder";
 import { sitemapBuilder } from "./sitemapBuilder";
 
 export const takanekono = (): Plugin[] => {
-  return [calendarBuilder(), sitemapBuilder()];
+  return [calendarBuilder(), sitemapBuilder(), datasetBuilder()];
 };
