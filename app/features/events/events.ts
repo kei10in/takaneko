1c3fe979bd8b2e0ce33e9024a8ea94f1fca2759d
Keisuke Ijuin <kei10in@gmail.com>
2025-08-13 23:26:23 +0900

    不要になった関数を削除

--- app/features/events/events.ts
+++ app/features/events/events.ts
@@ -1,35 +1,3 @@
-import { NaiveMonth } from "~/utils/datetime/NaiveMonth";
 import { importEventFilesAsEventModule } from "./eventFiles";
-import { EventModule } from "./eventModule";
 
 export const ALL_EVENTS = importEventFilesAsEventModule();
-
-export const isEventExists = (eventId: string): boolean => {
-  const [year, month] = eventId.split("_")[0].split("-");
-  const path = `./${year}/${month}/${eventId}.mdx`;
-  return path in ALL_EVENTS;
-};
-
-export const loadEvents = (month: NaiveMonth | NaiveMonth[]): EventModule[] => {
-  if (!Array.isArray(month)) {
-    return loadEvents([month]);
-  }
-
-  const prefixes = month.map((m) => {
-    return `./${m.year}/${m.month.toString().padStart(2, "0")}/`;
-  });
-
-  const events = Object.values(ALL_EVENTS).filter(({ filename }) => {
-    return prefixes.some((prefix) => filename.startsWith(prefix));
-  });
-
-  return events;
-};
-
-export const loadEventModule = (slug: string): EventModule | undefined => {
-  const [year, month] = slug.split("_")[0].split("-");
-
-  return (
-    ALL_EVENTS[`./${year}/${month}/${slug}.mdx`] ?? ALL_EVENTS[`./${year}/${month}/${slug}.tsx`]
-  );
-};
