7e91552476317ff6ca65b1f7f5e23eed5f5f9802
Keisuke Ijuin <kei10in@gmail.com>
2025-08-25 23:16:14 +0900

    不要になったファイルを削除

--- app/features/songs/songToLive.ts
+++
@@ -1,5 +0,0 @@
-import { ALL_EVENTS } from "../events/events";
-import { makeSongToLiveMap } from "./songActivities";
-import { ALL_SONGS } from "./songs";
-
-export const SongToLiveMap = makeSongToLiveMap(Object.values(ALL_EVENTS), ALL_SONGS);
