eea61d073b4fae2e604cb1b2a26b7dab7e746a92
Keisuke Ijuin <kei10in@gmail.com>
2025-09-10 23:55:33 +0900

    雑誌の関連画像を追加。

--- app/features/products/publications/グラビアプレス.ts
+++ app/features/products/publications/グラビアプレス.ts
@@ -19,6 +19,78 @@ export const グラビアプレスVol12: Publication = {
       path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_裏表紙.jpg",
       ref: "https://x.com/SHIBUYA_T_MUSIC/status/1957744356296679641",
     },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_城月菜央1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1960175366406119522",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_城月菜央2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1960175366406119522",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_涼海すう1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1960296169592213630",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_涼海すう2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1960296169592213630",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_橋本桃呼1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1961020942928494733",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_橋本桃呼2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1961020942928494733",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_葉月紗蘭1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1961383329191584045",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_葉月紗蘭2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1961383329191584045",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_東山恵里沙1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1961745714415018409",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_東山恵里沙2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1961745714415018409",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_日向端ひな1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1962108105954558226",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_日向端ひな2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1962108105954558226",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_星谷美来1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1964161642834632751",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_星谷美来2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1964161642834632751",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_松本ももな1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1964282430941974680",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_松本ももな2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1964282430941974680",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_籾山ひめり1.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1964644818346463575",
+    // },
+    // {
+    //   path: "/publications/2025/2025-09-05_グラビアプレス Vol.12_籾山ひめり2.jpg",
+    //   ref: "https://x.com/GRAVURE_PRESS/status/1964644818346463575",
+    // },
   ],
   featuredMembers: [
     "城月菜央",
