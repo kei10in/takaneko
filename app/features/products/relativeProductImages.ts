d96c05632464a8a65bb8a1f7ae11a2163edc2f3a
Keisuke Ijuin <kei10in@gmail.com>
2025-09-15 00:49:59 +0900

    関連アイテムの計算を修正

--- app/features/products/relativeProductImages.ts
+++ app/features/products/relativeProductImages.ts
@@ -1,12 +1,16 @@
 import { RandomGoods } from "./product";
-import { TAKANEKO_PHOTOS } from "./productImages";
+import {
+  isRegularTakanekoMiniPhotoCard,
+  isRegularTakanekoPhoto,
+  TAKANEKO_PHOTOS,
+} from "./productImages";
 
 export const relativeProductImages = (item: RandomGoods): RandomGoods[] => {
   const sameSeriesItems = TAKANEKO_PHOTOS.filter((x) => x.series === item.series).filter(
     (x) => x.id != item.id,
   );
 
-  if (item.category != "ミニフォト" && item.category != "生写真") {
+  if (!isRegularTakanekoPhoto(item) && !isRegularTakanekoMiniPhotoCard(item)) {
     return sameSeriesItems;
   }
 
@@ -16,26 +20,26 @@ export const relativeProductImages = (item: RandomGoods): RandomGoods[] => {
   const prevItems = TAKANEKO_PHOTOS.slice(i + 1);
 
   const nearMiniPhoto0 = nextItems
-    .filter((x) => x.category === "ミニフォト")
+    .filter((x) => isRegularTakanekoMiniPhotoCard(x))
     .filter((x) => x.series != item.series)
     .slice(0, 2)
     .toReversed();
   const nearMiniPhoto1 = prevItems
-    .filter((x) => x.category === "ミニフォト")
+    .filter((x) => isRegularTakanekoMiniPhotoCard(x))
     .filter((x) => x.series != item.series)
     .slice(0, 2);
 
   const nearPhoto0 = nextItems
-    .filter((x) => x.category === "生写真")
+    .filter((x) => isRegularTakanekoPhoto(x))
     .filter((x) => x.series != item.series)
     .slice(0, 2)
     .toReversed();
   const nearPhoto1 = prevItems
-    .filter((x) => x.category === "生写真")
+    .filter((x) => isRegularTakanekoPhoto(x))
     .filter((x) => x.series != item.series)
     .slice(0, 2);
 
-  if (item.category == "ミニフォト") {
+  if (isRegularTakanekoMiniPhotoCard(item)) {
     return [...sameSeriesItems, ...nearMiniPhoto0, ...nearMiniPhoto1, ...nearPhoto0, ...nearPhoto1];
   }
 
