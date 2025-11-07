431b186e5480b7c6daa59e6e73722aedf27467a4
Keisuke Ijuin <kei10in@gmail.com>
2025-11-07 21:19:48 +0900

    不要なコメントを削除

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,6 @@
 import mdx from "@mdx-js/rollup";
 import gfm from "remark-gfm";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-// vitest 用の property の型を導入するために vite ではなく vitest の`defineConfig` を使います。
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
