8948542249f748a2a1dd631474191e1dfb459b3b
Keisuke Ijuin <kei10in@gmail.com>
2025-11-08 19:43:46 +0900

    不要になった workaround を削除

--- vite.config.ts
+++ vite.config.ts
@@ -9,15 +9,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { takanekono } from "./app/vite/plugin";
 
 export default defineConfig({
-  // 開発サーバーで実行したときに `entry.server.tsx` で `renderToReadableStream`
-  // が見つからないエラーが出る問題を解決するための設定です。
-  // https://zenn.dev/caprolactam/articles/9aa8c4f6d4f85c
-  ssr: {
-    resolve: {
-      conditions: ["workerd", "worker", "browser"],
-      externalConditions: ["workerd", "worker"],
-    },
-  },
   plugins: [
     Inspect(),
     takanekono(),
