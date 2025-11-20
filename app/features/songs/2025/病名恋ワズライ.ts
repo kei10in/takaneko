5f9135390955aae970a5d4cd616d04c32cb30096
Keisuke Ijuin <kei10in@gmail.com>
2025-11-20 21:29:04 +0900

    病名恋ワズライを更新

--- app/features/songs/2025/病名恋ワズライ.ts
+++ app/features/songs/2025/病名恋ワズライ.ts
@@ -5,15 +5,21 @@ export const 病名恋ワズライ: SongMetaDescriptor = {
   slug: "病名恋ワズライ",
   name: "病名恋ワズライ",
 
-  lyricsBy: "shito",
+  lyricsBy: "shito, Gom",
   composedBy: "shito",
   arrangedBy: "HoneyWorks",
 
-  choreographedBy: undefined,
+  choreographedBy: "らん先生",
 
   tags: [TakanekoVersion, Repertoire, Limited],
 
+  coverArt: "/takaneko/songs/2025/病名恋ワズライ.jpg",
+
   youtube: [
+    {
+      text: "Dance Performance Video",
+      videoId: "rYlY5-Wv_xA",
+    },
     {
       text: "GUMI",
       videoId: "mwBGD2WT70I",
