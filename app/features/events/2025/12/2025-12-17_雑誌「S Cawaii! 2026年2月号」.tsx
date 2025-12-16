34593495907c4eff777c3f4fddc72c4e8aeef519
Keisuke Ijuin <kei10in@gmail.com>
2025-12-16 23:15:55 +0900

    雑誌の発売日を追加。

---
+++ app/features/events/2025/12/2025-12-17_雑誌「S Cawaii! 2026年2月号」.tsx
@@ -0,0 +1,9 @@
+import { SCawaii_2026年2月号 } from "~/features/products/publications/S Cawaii!";
+import { EventMetaDescriptor } from "../../eventMeta";
+import { convertPublicationToEventMeta } from "../../publicationToEventMeta";
+
+export const meta: EventMetaDescriptor = convertPublicationToEventMeta(SCawaii_2026年2月号);
+
+export const Content = () => {};
+
+export default Content;
