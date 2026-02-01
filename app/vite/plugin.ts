272e83754f36d27a7b5169c594f4743b4c7ace4a
Keisuke Ijuin <kei10in@gmail.com>
2026-02-02 00:16:27 +0900

    ビルド時に楽曲のメタデータを生成する。

--- app/vite/plugin.ts
+++ app/vite/plugin.ts
@@ -2,7 +2,8 @@ import { Plugin } from "vite";
 import { calendarBuilder } from "./calendarBuilder";
 import { datasetBuilder } from "./datasetBuilder";
 import { sitemapBuilder } from "./sitemapBuilder";
+import { songsMetadataBuilder } from "./songsMetadataBuilder";
 
 export const takanekono = (): Plugin[] => {
-  return [calendarBuilder(), sitemapBuilder(), datasetBuilder()];
+  return [calendarBuilder(), sitemapBuilder(), datasetBuilder(), songsMetadataBuilder()];
 };
