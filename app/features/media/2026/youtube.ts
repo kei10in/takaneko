12522c27b69011d4304a5bbf683d318640849a26
Keisuke Ijuin <kei10in@gmail.com>
2026-02-01 22:18:36 +0900

    メディアを追加。

--- app/features/media/2026/youtube.ts
+++ app/features/media/2026/youtube.ts
@@ -2,6 +2,18 @@ import { YouTubeVideoDescriptor } from "../types";
 
 export const YouTube2026 = [
   {
+    videoId: "Ip9nyLB1m-I",
+    publishedAt: "2026-01-28",
+    presents: ["葉月紗蘭"],
+    officialTwitter: "https://x.com/takanenofficial/status/2016363505704894865",
+  },
+  {
+    videoId: "HTZuILRv7lQ",
+    publishedAt: "2026-01-28",
+    presents: ["葉月紗蘭"],
+    officialTwitter: "https://x.com/takanenofficial/status/2016364615459070285",
+  },
+  {
     videoId: "qF7TrecoG3I",
     publishedAt: "2026-01-26",
     presents: ["葉月紗蘭"],
