85846b4cda08c0f97b4804b82e8c7d25533774d8
Keisuke Ijuin <kei10in@gmail.com>
2026-02-03 21:46:25 +0900

    Crop 画像のテストを追加。

--- app/features/products/productImages.test.ts
+++ app/features/products/productImages.test.ts
@@ -1,7 +1,11 @@
 import { describe, expect, it } from "vitest";
+import { allAssetFiles } from "~/utils/tests/asset";
+import { croppedImagePath } from "./croppedProductImage";
 import { TAKANEKO_PHOTOS } from "./productImages";
 
 describe("TAKANEKO_PHOTOS", () => {
+  const AllAssets = allAssetFiles();
+
   it("should have unique ids for all photos", () => {
     const EXCEPTIONS = ["FAVE IDOLS オンラインくじ"];
 
@@ -17,5 +21,13 @@ describe("TAKANEKO_PHOTOS", () => {
     const slugs = TAKANEKO_PHOTOS.map((x) => x.slug).toSorted();
     const uniqueSlugs = Array.from(new Set(slugs)).toSorted();
     expect(slugs).toEqual(uniqueSlugs);
+  });
+
+  it("should have cropped images for all photos", () => {
+    const croppedImages = TAKANEKO_PHOTOS.flatMap((photo) =>
+      photo.positions.map((p) => croppedImagePath(photo.url, p.id)),
+    );
+    const notFoundImages = croppedImages.filter((imagePath) => !AllAssets.includes(imagePath));
+    expect(notFoundImages).toEqual([]);
   });
 });
