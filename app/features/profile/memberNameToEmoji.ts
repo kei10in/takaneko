bb57800d4386b014d76143d04c613a419f88ac48
Keisuke Ijuin <kei10in@gmail.com>
2026-03-07 13:23:04 +0900

    ファイルに分割

---
+++ app/features/profile/memberNameToEmoji.ts
@@ -0,0 +1,24 @@
+export const memberNameToEmoji = (name: string): string => {
+  switch (name) {
+    case "城月菜央":
+      return "💛";
+    case "涼海すう":
+      return "🩵";
+    case "橋本桃呼":
+      return "🩷";
+    case "葉月紗蘭":
+      return "🤍";
+    case "東山恵里沙":
+      return "🧡";
+    case "日向端ひな":
+      return "💜";
+    case "星谷美来":
+      return "❤️";
+    case "松本ももな":
+      return "🎀";
+    case "籾山ひめり":
+      return "💙";
+    default:
+      return "";
+  }
+};
--- app/routes/_site.special.2025-summer-radio._index/route.tsx
+++ app/routes/_site.special.2025-summer-radio._index/route.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { BsBroadcastPin, BsStopwatch } from "react-icons/bs";
 import { Link, MetaFunction } from "react-router";
 import { DOMAIN, SITE_TITLE } from "~/constants";
+import { memberNameToEmoji } from "~/features/profile/memberNameToEmoji";
 import { NaiveDate } from "~/utils/datetime/NaiveDate";
 import { RadioAppearances } from "./content";
 
@@ -224,28 +225,3 @@ export default function Index() {
     </div>
   );
 }
-
-const memberNameToEmoji = (name: string): string => {
-  switch (name) {
-    case "城月菜央":
-      return "💛";
-    case "涼海すう":
-      return "🩵";
-    case "橋本桃呼":
-      return "🩷";
-    case "葉月紗蘭":
-      return "🤍";
-    case "東山恵里沙":
-      return "🧡";
-    case "日向端ひな":
-      return "💜";
-    case "星谷美来":
-      return "❤️";
-    case "松本ももな":
-      return "🎀";
-    case "籾山ひめり":
-      return "💙";
-    default:
-      return "";
-  }
-};
