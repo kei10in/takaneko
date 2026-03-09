622487eb6da799464e895692a48b958f90b7eb25
Keisuke Ijuin <kei10in@gmail.com>
2026-03-10 00:17:34 +0900

    MV衣装を追加。

--- app/features/costumes/costumesMv.ts
+++ app/features/costumes/costumesMv.ts
@@ -1,5 +1,59 @@
 import { MvCostume } from "./types";
 
+export const 病名恋ワズライ衣装 = {
+  kind: "mv",
+  name: "「病名恋ワズライ」衣装",
+  slug: "病名恋ワズライ衣装",
+} satisfies MvCostume;
+
+export const この世界は嘘でできている白衣装 = {
+  kind: "mv",
+  name: "「この世界は嘘でできている」白衣装",
+  slug: "この世界は嘘でできている白衣装",
+} satisfies MvCostume;
+
+export const この世界は嘘でできている黒衣装 = {
+  kind: "mv",
+  name: "「この世界は嘘でできている」黒衣装",
+  slug: "この世界は嘘でできている黒衣装",
+} satisfies MvCostume;
+
+export const この世界は嘘でできている私服衣装 = {
+  kind: "mv",
+  name: "「この世界は嘘でできている」私服衣装",
+  slug: "この世界は嘘でできている私服衣装",
+} satisfies MvCostume;
+
+export const ライフクエストお仕事衣装 = {
+  kind: "mv",
+  name: "「ライフクエスト」お仕事衣装",
+  slug: "ライフクエストお仕事衣装",
+} satisfies MvCostume;
+
+export const ライフクエスト部屋着衣装 = {
+  kind: "mv",
+  name: "「ライフクエスト」部屋着衣装",
+  slug: "ライフクエスト部屋着衣装",
+} satisfies MvCostume;
+
+export const アイドル衣装白衣装 = {
+  kind: "mv",
+  name: "「アイドル衣装」白衣装",
+  slug: "アイドル衣装白衣装",
+} satisfies MvCostume;
+
+export const メランコリックハニーMV衣装 = {
+  kind: "mv",
+  name: "「メランコリックハニー」MV衣装",
+  slug: "メランコリックハニーMV衣装",
+} satisfies MvCostume;
+
+export const CuteForLife私服衣装 = {
+  kind: "mv",
+  name: "「Cute For Life」私服衣装",
+  slug: "CuteForLife私服衣装",
+} satisfies MvCostume;
+
 export const ImYourIdolMV衣装 = {
   kind: "mv",
   name: "I’M YOUR IDOL MV 衣装",
