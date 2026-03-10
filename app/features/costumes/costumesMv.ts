937f75f183ce929e8af0a5655d0b58d4f51fa32a
Keisuke Ijuin <kei10in@gmail.com>
2026-03-10 22:52:58 +0900

    MV 衣装を追加

--- app/features/costumes/costumesMv.ts
+++ app/features/costumes/costumesMv.ts
@@ -36,6 +36,12 @@ export const ライフクエスト部屋着衣装 = {
   slug: "ライフクエスト部屋着衣装",
 } satisfies MvCostume;
 
+export const 初恋のこたえ浴衣衣装 = {
+  kind: "mv",
+  name: "「初恋のこたえ」浴衣衣装",
+  slug: "初恋のこたえ浴衣衣装",
+} satisfies MvCostume;
+
 export const アイドル衣装白衣装 = {
   kind: "mv",
   name: "「アイドル衣装」白衣装",
@@ -54,6 +60,24 @@ export const CuteForLife私服衣装 = {
   slug: "CuteForLife私服衣装",
 } satisfies MvCostume;
 
+export const 東京サニーパーティー制服衣装 = {
+  kind: "mv",
+  name: "「東京サニーパーティー」制服衣装",
+  slug: "東京サニーパーティー制服衣装",
+} satisfies MvCostume;
+
+export const 東京サニーパーティーパジャマ衣装 = {
+  kind: "mv",
+  name: "「東京サニーパーティー」パジャマ衣装",
+  slug: "東京サニーパーティーパジャマ衣装",
+} satisfies MvCostume;
+
+export const 小悪魔だってかまわない衣装 = {
+  kind: "mv",
+  name: "「小悪魔だってかまわない」衣装",
+  slug: "小悪魔だってかまわない衣装",
+} satisfies MvCostume;
+
 export const ImYourIdolMV衣装 = {
   kind: "mv",
   name: "I’M YOUR IDOL MV 衣装",
@@ -66,10 +90,106 @@ export const アドレナリンゲームMV衣装 = {
   slug: "アドレナリンゲームMV衣装",
 } satisfies MvCostume;
 
+export const LoveAnthemMV衣装 = {
+  kind: "mv",
+  name: "「LOVE ANTHEM」MV 衣装",
+  slug: "LOVE ANTHEM MV衣装",
+} satisfies MvCostume;
+
 export const モテチェンMV衣装 = {
   kind: "mv",
   name: "モテチェン！ MV 衣装",
   slug: "モテチェンMV衣装",
+} satisfies MvCostume;
+
+export const 私より好きでいてMV衣装 = {
+  kind: "mv",
+  name: "「私より好きでいて」MV 衣装",
+  slug: "私より好きでいてMV衣装",
+} satisfies MvCostume;
+
+export const メイド至上主義MV衣装 = {
+  kind: "mv",
+  name: "「メイド至上☆主義」MV衣装",
+  slug: "メイド至上主義MV衣装",
+} satisfies MvCostume;
+
+export const 推しの魔法MV衣装 = {
+  kind: "mv",
+  name: "「推しの魔法」MV 衣装",
+  slug: "推しの魔法MV衣装",
+} satisfies MvCostume;
+
+export const 私は怪物MV衣装 = {
+  kind: "mv",
+  name: "「私は怪物」MV 衣装",
+  slug: "私は怪物MV衣装",
+} satisfies MvCostume;
+
+export const 可愛いっていわれたいMV衣装 = {
+  kind: "mv",
+  name: "「可愛いっていわれたい」MV 衣装",
+  slug: "可愛いっていわれたいMV衣装",
+} satisfies MvCostume;
+
+export const いつか私がママになったら制服衣装 = {
+  kind: "mv",
+  name: "「いつか私がママになったら」制服衣装",
+  slug: "いつか私がママになったら制服衣装",
+} satisfies MvCostume;
+
+export const いつか私がママになったらワンピース衣装 = {
+  kind: "mv",
+  name: "「いつか私がママになったら」MV ワンピース衣装",
+  slug: "いつか私がママになったらMVワンピース衣装",
+} satisfies MvCostume;
+
+export const _17歳MV衣装 = {
+  kind: "mv",
+  name: "「17歳」MV 衣装",
+  slug: "17歳MV衣装",
+} satisfies MvCostume;
+
+export const すきっちゅーの私服衣装 = {
+  kind: "mv",
+  name: "「すきっちゅーの」私服衣装",
+  slug: "すきっちゅーの私服衣装",
+};
+
+export const すきっちゅーのパジャマ衣装 = {
+  kind: "mv",
+  name: "「すきっちゅーの！」パジャマ衣装",
+  slug: "すきっちゅーのパジャマ衣装",
+};
+
+export const 月曜日の憂鬱MV衣装 = {
+  kind: "mv",
+  name: "「月曜日の憂鬱」MV 衣装",
+  slug: "月曜日の憂鬱MV衣装",
+} satisfies MvCostume;
+
+export const 決戦スピリットスポーツウェア衣装 = {
+  kind: "mv",
+  name: "「決戦スピリット」スポーツウェア 衣装",
+  slug: "決戦スピリットスポーツウェア衣装",
+} satisfies MvCostume;
+
+export const 革命の女王黒衣装 = {
+  kind: "mv",
+  name: "「革命の女王」黒衣装",
+  slug: "革命の女王黒衣装",
+} satisfies MvCostume;
+
+export const 僕は君になれない私服衣装 = {
+  kind: "mv",
+  name: "「僕は君になれない」私服衣装",
+  slug: "僕は君になれない私服衣装",
+} satisfies MvCostume;
+
+export const 男の子の目的は何MV衣装 = {
+  kind: "mv",
+  name: "「男の子の目的は何？」MV 衣装",
+  slug: "男の子の目的は何MV衣装",
 } satisfies MvCostume;
 
 export const 可愛くてごめんMV衣装 = {
@@ -78,9 +198,52 @@ export const 可愛くてごめんMV衣装 = {
   slug: "可愛くてごめんMV衣装",
 } satisfies MvCostume;
 
+export const 可愛くてごめんパジャマ衣装 = {
+  kind: "mv",
+  name: "「可愛くてごめん」パジャマ衣装",
+  slug: "可愛くてごめんパジャマ衣装",
+} satisfies MvCostume;
+
+export const ユメムスビMV衣装 = {
+  kind: "mv",
+  name: "「ユメムスビ」MV 衣装",
+  slug: "ユメムスビMV衣装",
+} satisfies MvCostume;
+
 export const AllMvCostumes = [
+  病名恋ワズライ衣装,
+  この世界は嘘でできている白衣装,
+  この世界は嘘でできている黒衣装,
+  この世界は嘘でできている私服衣装,
+  ライフクエストお仕事衣装,
+  ライフクエスト部屋着衣装,
+  初恋のこたえ浴衣衣装,
+  アイドル衣装白衣装,
+  メランコリックハニーMV衣装,
+  CuteForLife私服衣装,
+  東京サニーパーティー制服衣装,
+  東京サニーパーティーパジャマ衣装,
+  小悪魔だってかまわない衣装,
   ImYourIdolMV衣装,
   アドレナリンゲームMV衣装,
+  LoveAnthemMV衣装,
   モテチェンMV衣装,
+  私より好きでいてMV衣装,
+  メイド至上主義MV衣装,
+  推しの魔法MV衣装,
+  私は怪物MV衣装,
+  可愛いっていわれたいMV衣装,
+  いつか私がママになったら制服衣装,
+  いつか私がママになったらワンピース衣装,
+  _17歳MV衣装,
+  すきっちゅーの私服衣装,
+  すきっちゅーのパジャマ衣装,
+  月曜日の憂鬱MV衣装,
+  決戦スピリットスポーツウェア衣装,
+  革命の女王黒衣装,
+  僕は君になれない私服衣装,
+  男の子の目的は何MV衣装,
   可愛くてごめんMV衣装,
+  可愛くてごめんパジャマ衣装,
+  ユメムスビMV衣装,
 ];
