932a2136b220c3228b615ecc0623a7b0185c5665
Keisuke Ijuin <kei10in@gmail.com>
2026-03-25 23:09:43 +0900

    雑誌を追加。

---
+++ app/features/events/2026/03/2026-03-24_雑誌「B.L.T.graduation2026中学・高校卒業」.tsx
@@ -0,0 +1,10 @@
+import { BLT_graduation_2026中学高校卒業 } from "~/features/publications/publications/B.L.T";
+import { EventMetaDescriptor } from "../../eventMeta";
+import { convertPublicationToEventMeta } from "../../publicationToEventMeta";
+
+export const meta: EventMetaDescriptor =
+  convertPublicationToEventMeta(BLT_graduation_2026中学高校卒業);
+
+export const Content = () => {};
+
+export default Content;
--- app/features/publications/publications/B.L.T.ts
+++ app/features/publications/publications/B.L.T.ts
@@ -1,5 +1,58 @@
 import { Publication } from "../types";
 
+export const BLT_graduation_2026中学高校卒業: Publication = {
+  slug: "B.L.T.graduation2026中学・高校卒業",
+  name: "B.L.T.graduation2026中学・高校卒業",
+  date: "2026-03-24",
+  kind: "mooks",
+  publisher: "東京ニュース通信社",
+  listPrice: 2273,
+  priceWithTax: 2500,
+  code: [],
+  url: "https://bltweb.jp/2026/03/12/graduation_cover/",
+  coverImages: [
+    {
+      path: "/publications/2026/2026-03-24_B.L.T.graduation2026中学・高校卒業_表紙.webp",
+      ref: "https://bltweb.jp/2026/03/12/graduation_cover/",
+    },
+    {
+      path: "/publications/2026/2026-03-24_B.L.T.graduation2026中学・高校卒業_裏表紙.webp",
+      ref: "https://bltweb.jp/2026/03/12/graduation_cover/",
+    },
+    {
+      path: "/publications/2026/2026-03-24_B.L.T.graduation2026中学・高校卒業_涼海すうポストカード.jpg",
+      ref: "https://bltweb.jp/2026/03/12/graduation_cover/",
+    },
+  ],
+  bonuses: [
+    {
+      name: "涼海すう (高嶺のなでしこ) ポストカード",
+      category: "セブンネット限定特典",
+      store: "https://7net.omni7.jp/detail/1107692146",
+    },
+  ],
+  featuredMembers: ["涼海すう"],
+  officialTwitter: "",
+  links: [
+    {
+      text: "「B.L.T.graduation2026中学・高校卒業」3/24(火)に発売決定！ 表紙・巻頭を飾るのはこの春高校を卒業する乃木坂46・小川彩。さらに、中学を卒業する乃木坂46・川端晃菜も登場。 | B.L.T.web",
+      url: "https://bltweb.jp/2026/03/06/bltgraduation2026_0306/",
+    },
+    {
+      text: "掲載内容紹介 (動画あり) - B.L.T.編集部 X",
+      url: "https://x.com/BLTTV/status/2029883272738787574",
+    },
+    {
+      text: "ポストカード解禁 - B.L.T.編集部 X",
+      url: "https://x.com/BLTTV/status/2032297624968335671",
+    },
+    {
+      text: "インタビュー記事紹介 - ライター X",
+      url: "https://x.com/matrisanez/status/2036397537469645164",
+    },
+  ],
+};
+
 export const BLT_2024年9月号: Publication = {
   slug: "B.L.T 2024年9月号",
   name: "B.L.T 2024年9月号",
