6153040c1f1c8692a926db9302fc5404c0af4dcb
Keisuke Ijuin <kei10in@gmail.com>
2026-04-01 22:37:13 +0900

    雑誌の発売日を追加。

---
+++ app/features/events/2026/03/2026-03-31_雑誌「KIDDY LAND FAN BOOK」.tsx
@@ -0,0 +1,9 @@
+import { KIDDY_LAND_FAN_BOOK } from "~/features/publications/publications/KIDDY LAND FAN BOOK";
+import { EventMetaDescriptor } from "../../eventMeta";
+import { convertPublicationToEventMeta } from "../../publicationToEventMeta";
+
+export const meta: EventMetaDescriptor = convertPublicationToEventMeta(KIDDY_LAND_FAN_BOOK);
+
+export const Content = () => {};
+
+export default Content;
