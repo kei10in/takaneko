502af21d1831fe68211439a68e6918ad7de25fd1
Keisuke Ijuin <kei10in@gmail.com>
2026-04-04 18:29:43 +0900

    不要な console.log を削除

--- app/utils/rehype/allowBrTags.ts
+++ app/utils/rehype/allowBrTags.ts
@@ -5,8 +5,6 @@ export const allowBrTags = () => {
   return (tree: Nodes) => {
     return map(tree, (node) => {
       if (node.type === "html" && /<br\s*\/?>/.test(node.value)) {
-        console.log("Found <br> tag in markdown. Converting to line break.");
-
         const newNode: Break = { type: "break" };
         return newNode;
       }
