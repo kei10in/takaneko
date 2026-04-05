52b6a41544385dc058b0bd431e44d2795d193bf3
Keisuke Ijuin <kei10in@gmail.com>
2026-04-05 16:36:24 +0900

    テンプレートを追加。

---
+++ app/features/events/_event-template.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "./eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "",
+  category: "LIVE",
+  liveType: undefined,
+  date: "2026-",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "",
+  location: "",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - []
+`;
