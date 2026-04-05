0a2f89f728d5f7967ae77c3eacb5006da8b13300
Keisuke Ijuin <kei10in@gmail.com>
2026-04-05 17:49:44 +0900

    配信出演を追加。

---
+++ app/features/events/2026/04/2026-04-17_Podcasts Membership「たかねこナイト」.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "Podcasts Membership「可憐なアイボリーのカレアイ放送局」",
+  category: "STREAMING",
+  date: "2026-04-17",
+  start: "22:00",
+  end: undefined,
+  region: "配信",
+  present: ["籾山ひめり"],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "番組サイト",
+    url: "https://audee-membership.jp/karennaivory",
+  },
+  updatedAt: "2026-04-05",
+};
+
+export const content = /* md */ `
+  ## コラボ放送
+
+  「可憐なアイボリーのカレアイ放送局」とのコラボ放送。
+
+  可憐なアイボリーからは永尾梨央さんが出演。
+
+  永尾梨央さんと籾山ひめりのリーダー × キャプテンコンビでスペシャル対談🎤✨
+
+  X ハッシュタグは「[#カレラジ]」
+
+  [#カレラジ]: http://x.com/search?q=%23カレラジ
+
+  ## リンク
+
+  - [メッセージ投稿フォーム](https://www.tfm.co.jp/f/karennaivory/message)
+  - [コラボ放送告知 - 公式 X](https://x.com/takanenofficial/status/2039998921104306442)
+  - [告知 - 可憐なアイボリー X](https://x.com/Karennaivory/status/2039998859070656822)
+`;
