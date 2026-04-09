3ad9e0ecf29be4464a098bd66719f15fd89b3d10
Keisuke Ijuin <kei10in@gmail.com>
2026-04-09 22:59:35 +0900

    イベントを更新

--- app/features/events/2026/04/2026-04-09_HANiKAMi Vol.3.ts
+++ app/features/events/2026/04/2026-04-09_HANiKAMi Vol.3.ts
@@ -21,6 +21,10 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2036408939198169137",
       tags: ["timetable"],
     },
+    {
+      path: "/events/2026/2026-04-09_HANiKAMi Vol.3_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2042209584040046887",
+    },
   ],
   link: {
     text: "",
@@ -29,21 +33,32 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://t.pia.jp/pia/event/event.do?eventCd=2609329",
   streamings: undefined,
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["終演後"],
+    lineup: ["ミニフォトカード「セーラー服2026」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2042112865159802931",
   },
   acts: {
     start: "19:55",
     end: "20:20",
-    setlist: [],
-    url: "",
+    setlist: [
+      "衣装: 2025 春衣装",
+      "初恋のこたえ。",
+      "メランコリックハニー",
+      "MC",
+      "センパイ。",
+      "ファンサ",
+      "美しく生きろ",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2042215862753792275",
   },
-  updatedAt: "2026-04-05",
+  updatedAt: "2026-04-09",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [出演報告 (ダイジェスト動画あり) - 公式 X](https://x.com/takanenofficial/status/2042219699107398031)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2042209584040046887)
+  - [きょうのたかねこ](https://x.com/takanenofficial/status/2042112865159802931)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2032373601371090999)
 `;
