92c8a282f04f71ffd03565b96ab773c33b1b52f2
Keisuke Ijuin <kei10in@gmail.com>
2026-04-09 22:22:03 +0900

    イベントを追加

---
+++ app/features/events/2026/04/2026-04-11_「Live Tour - Bouquet of 9 Flowers -」開催記念イベント@ソウル.ts
@@ -0,0 +1,76 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "「Live Tour - Bouquet of 9 Flowers -」開催記念イベント@ソウル",
+  title: "「Live Tour - Bouquet of 9 Flowers -」開催記念『個別TikTok撮影会』@ソウル",
+  category: "EVENT",
+  date: "2026-04-11",
+  open: undefined,
+  start: "11:00",
+  end: undefined,
+  region: "ソウル",
+  location: "Cafe flat (플랫 홍대)",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-04-11_「Live Tour - Bouquet of 9 Flowers -」開催記念イベント@ソウル.webp",
+      ref: "https://takanenonadeshiko.jp/?p=5165",
+    },
+  ],
+  link: {
+    text: "イベント詳細",
+    url: "https://takanenonadeshiko.jp/?p=5165",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  meetAndGreet: {
+    title: "個別 TikTok 撮影会",
+    sessions: [
+      {
+        title: "1 部",
+        start: "11:00",
+        end: "12:00",
+        lastEntry: "11:40",
+        lanes: ["橋本桃呼", "星谷美来", "東山恵里沙", "日向端ひな", "城月菜央"],
+        costume: "Bouquet of 9 Flowers 衣装",
+      },
+      {
+        title: "2 部",
+        start: "12:10",
+        end: "13:10",
+        lastEntry: "12:50",
+        lanes: ["涼海すう", "葉月紗蘭", "松本ももな", "籾山ひめり"],
+        costume: "Bouquet of 9 Flowers 衣装",
+      },
+      {
+        title: "3 部",
+        start: "14:00",
+        end: "15:00",
+        lastEntry: "14:40",
+        lanes: ["橋本桃呼", "星谷美来", "東山恵里沙", "日向端ひな", "城月菜央"],
+        costume: "Bouquet of 9 Flowers 衣装",
+      },
+      {
+        title: "4 部",
+        start: "15:10",
+        end: "16:10",
+        lastEntry: "15:50",
+        lanes: ["涼海すう", "葉月紗蘭", "松本ももな", "籾山ひめり"],
+        costume: "Bouquet of 9 Flowers 衣装",
+      },
+    ],
+  },
+  updatedAt: "2026-04-09",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [イベント詳細 - 公式ニュース](https://takanenonadeshiko.jp/?p=5165)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2041833504275886185)
+`;
