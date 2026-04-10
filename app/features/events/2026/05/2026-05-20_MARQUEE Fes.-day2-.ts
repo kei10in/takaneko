278b917810053c9348ea7d929e590240681f6c5e
Keisuke Ijuin <kei10in@gmail.com>
2026-04-10 22:12:22 +0900

    イベントを追加

---
+++ app/features/events/2026/05/2026-05-20_MARQUEE Fes.-day2-.ts
@@ -0,0 +1,49 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "MARQUEE Fes. -day2-",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-05-20",
+  open: "14:00",
+  start: "14:30",
+  end: undefined,
+  region: "東京",
+  location: "Spotify O-EAST",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-05-20_MARQUEE Fes..jpg",
+      ref: "https://x.com/MARQUEE_matsuri/status/2041712652683284667",
+    },
+    {
+      path: "/events/2026/2026-05-20_MARQUEE Fes.-day2-.jpg",
+      ref: "https://x.com/MARQUEE_matsuri/status/2042467615344296324",
+      tags: ["timetable"],
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://eplus.jp/sf/word/0000131445",
+  streamings: undefined,
+  goods: {
+    time: ["20:45", "21:55"],
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    start: "20:15",
+    end: "20:40",
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-04-10",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2041742987651379384)
+`;
