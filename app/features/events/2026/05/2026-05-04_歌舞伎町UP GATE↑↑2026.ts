fc29d9c2b32a344741bbc1a0fa3fefefa52bccd5
Keisuke Ijuin <kei10in@gmail.com>
2026-04-15 22:39:47 +0900

    イベントを追加

---
+++ app/features/events/2026/05/2026-05-04_歌舞伎町UP GATE↑↑2026.ts
@@ -0,0 +1,46 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "歌舞伎町UP GATE↑↑2026",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-05-04",
+  open: "12:00",
+  start: "13:00",
+  end: undefined,
+  region: "東京",
+  location: "新宿",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-05-04_歌舞伎町UP GATE↑↑2026.webp",
+      ref: "https://kabukicho-upgate.com/",
+    },
+    {
+      path: "/events/2026/2026-05-04_歌舞伎町UP GATE↑↑2026_1.jpg",
+      ref: "https://x.com/takanenofficial/status/2042860595129815306",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://kabukicho-upgate.com/",
+  },
+  ticket: "https://ticket.rakuten.co.jp/music/jpop/idle/RTZPBBP/?scid=su_16699",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-04-15",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2042860595129815306)
+`;
