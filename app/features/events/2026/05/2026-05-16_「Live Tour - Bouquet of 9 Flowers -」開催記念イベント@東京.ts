92416583d68a5cd0e4aed76eb6cddaa6f9996e27
Keisuke Ijuin <kei10in@gmail.com>
2026-04-15 22:47:28 +0900

    イベントを追加。

---
+++ app/features/events/2026/05/2026-05-16_「Live Tour - Bouquet of 9 Flowers -」開催記念イベント@東京.ts
@@ -0,0 +1,86 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "「Live Tour - Bouquet of 9 Flowers -」開催記念イベント@東京",
+  title:
+    "「Live Tour - Bouquet of 9 Flowers -」開催記念 『個別2ショット撮影会』・『個別TikTok撮影会』・『個別サイン会』@東京",
+  category: "EVENT",
+  date: "2026-05-16",
+  open: undefined,
+  start: "11:00",
+  end: "18:30",
+  region: "東京",
+  location: "東京流通センター 第一展示場Aホール",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "イベント詳細",
+    url: "https://takanenonadeshiko.jp/?p=5180",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  meetAndGreet: {
+    sessions: [
+      {
+        title: "1 部 個別 2 ショット撮影会",
+        start: "11:00",
+        end: "12:00",
+        lastEntry: "11:40",
+        lanes: ["橋本桃呼", "星谷美来", "東山恵里沙", "日向端ひな", "城月菜央"],
+      },
+      {
+        title: "2 部 個別 2 ショット撮影会",
+        start: "12:10",
+        end: "13:10",
+        lastEntry: "12:50",
+        lanes: ["涼海すう", "葉月紗蘭", "松本ももな", "籾山ひめり"],
+      },
+      {
+        title: "3 部 個別 TikTok 撮影会",
+        start: "13:20",
+        end: "14:20",
+        lastEntry: "14:00",
+        lanes: ["橋本桃呼", "星谷美来", "東山恵里沙", "日向端ひな", "城月菜央"],
+      },
+      {
+        title: "4 部 個別 TikTok 撮影会",
+        start: "14:30",
+        end: "15:30",
+        lastEntry: "15:10",
+        lanes: ["涼海すう", "葉月紗蘭", "松本ももな", "籾山ひめり"],
+      },
+      {
+        title: "5 部 個別サイン会",
+        start: "16:20",
+        end: "17:20",
+        lastEntry: "17:00",
+        lanes: ["橋本桃呼", "星谷美来", "東山恵里沙", "日向端ひな", "城月菜央"],
+      },
+      {
+        title: "6 部 個別サイン会",
+        start: "17:30",
+        end: "18:30",
+        lastEntry: "18:10",
+        lanes: ["涼海すう", "葉月紗蘭", "松本ももな", "籾山ひめり"],
+      },
+    ],
+  },
+  updatedAt: "2026-04-15",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [イベント詳細 - 公式ニュース](https://takanenonadeshiko.jp/?p=5180)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2044332398431916471)
+`;
