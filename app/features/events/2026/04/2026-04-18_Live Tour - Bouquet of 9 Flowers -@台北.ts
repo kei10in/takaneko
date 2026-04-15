f9ea79495df1308f3a90508922195394b5b78769
Keisuke Ijuin <kei10in@gmail.com>
2026-04-15 22:09:42 +0900

    物販情報を追加。

--- app/features/events/2026/04/2026-04-18_Live Tour - Bouquet of 9 Flowers -@台北.ts
+++ app/features/events/2026/04/2026-04-18_Live Tour - Bouquet of 9 Flowers -@台北.ts
@@ -20,6 +20,21 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-04-12_Live Tour - Bouquet of 9 Flowers -.webp",
       ref: "https://takanenonadeshiko.jp/?p=4969",
     },
+    {
+      path: "/takaneko/live-goods/2026-04-18_Live Tour - Bouquet of 9 Flowers -@台北_グッズメニュー1.jpg",
+      ref: "https://x.com/takanenofficial/status/2043690862094450777",
+      tags: ["merch-list"],
+    },
+    {
+      path: "/takaneko/live-goods/2026-04-18_Live Tour - Bouquet of 9 Flowers -@台北_グッズメニュー2.jpg",
+      ref: "https://x.com/takanenofficial/status/2043690862094450777",
+      tags: ["merch-list"],
+    },
+    {
+      path: "/takaneko/live-goods/2026-04-18_Live Tour - Bouquet of 9 Flowers -@台北_グッズメニュー3.jpg",
+      ref: "https://x.com/takanenofficial/status/2043690862094450777",
+      tags: ["merch-list"],
+    },
   ],
   link: {
     text: "開催地告知 - 公式ニュース",
@@ -30,7 +45,7 @@ export const meta: EventMetaDescriptor = {
   goods: {
     time: undefined,
     lineup: undefined,
-    url: undefined,
+    url: "https://x.com/takanenofficial/status/2043690862094450777",
   },
   acts: [
     {
@@ -48,12 +63,13 @@ export const meta: EventMetaDescriptor = {
       url: "",
     },
   ],
-  updatedAt: "2026-03-04",
+  updatedAt: "2026-04-15",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [GOODS MENU 公開 - 公式 X](https://x.com/takanenofficial/status/2043690862094450777)
   - [FC 先行チケット受付](https://takanekofc.com/topics/public/_/umyvcsrvjyj6gg6c.html)
   - [イベント詳細 - 公式ニュース](https://takanenonadeshiko.jp/?p=5061)
   - [詳細告知 - 公式 X](https://x.com/takanenofficial/status/2025541227144269997)
