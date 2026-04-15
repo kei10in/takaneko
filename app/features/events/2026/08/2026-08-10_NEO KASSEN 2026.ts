0a08b082d31cebefcb73ed50986c7b5327f7b715
Keisuke Ijuin <kei10in@gmail.com>
2026-04-15 22:25:21 +0900

    イベントを追加

---
+++ app/features/events/2026/08/2026-08-10_NEO KASSEN 2026.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "NEO KASSEN 2026",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-08-10",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "東京",
+  location: "Spotify O-EAST",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-10_NEO KASSEN 2026.jpg",
+      ref: "https://x.com/NEOJAPONISMinfo/status/2043600076262838480",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://neojaponism.com/neokassen/2026",
+  },
+  ticket: "https://l-tike.com/concert/mevent/?mid=781806",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-04-15",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - NEO JAPONISM X](https://x.com/NEOJAPONISMinfo/status/2043600076262838480)
+`;
