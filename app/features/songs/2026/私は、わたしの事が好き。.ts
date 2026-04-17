8c745c826d7204eb1d652840fd7133024d94a5c0
Keisuke Ijuin <kei10in@gmail.com>
2026-04-17 23:26:58 +0900

    楽曲を追加。

---
+++ app/features/songs/2026/私は、わたしの事が好き。.ts
@@ -0,0 +1,39 @@
+import { Repertoire, TakanekoVersion } from "../tags";
+import { SongMetaDescriptor } from "../types";
+
+export const 私はわたしの事が好き: SongMetaDescriptor = {
+  slug: "私はわたしの事が好き",
+  name: "私は、わたしの事が好き。",
+
+  lyricsBy: "shito, Gom",
+  composedBy: "shito, 中西",
+  arrangedBy: "HoneyWorks",
+
+  choreographedBy: "らん先生",
+
+  videoRelease: "2026-04-14",
+  digitalRelease: "2026-04-15",
+  liveDebut: "2025-04-12",
+
+  tags: [TakanekoVersion, Repertoire],
+
+  coverArt: "/takaneko/songs/2026/私は、わたしの事が好き。.jpg",
+
+  youtube: [
+    {
+      text: "Dance Performance Video",
+      videoId: "IoCC9aOnX-E",
+      publishedAt: "2026-04-14",
+    },
+    {
+      text: "3D ライブ映像 (超かぐや姫！)",
+      videoId: "pnb_BVChIaw",
+      publishedAt: "2026-03-12",
+    },
+    {
+      text: "Official MV (超かぐや姫！)",
+      videoId: "J5tRnYiHifE",
+      publishedAt: "2025-12-12",
+    },
+  ],
+};
