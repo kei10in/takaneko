fea65e75b145f851f032026eec9d4ff77a287bd2
Keisuke Ijuin <kei10in@gmail.com>
2026-04-19 19:59:22 +0900

    イベントを追加。

---
+++ app/features/events/2026/05/2026-05-27_NATSLIVE「たかねこ初恋キッチン」.ts
@@ -0,0 +1,73 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "NATSLIVE「たかねこ初恋キッチン。#5」",
+  category: "STREAMING",
+  liveType: undefined,
+  date: "2026-05-27",
+  open: "18:30",
+  start: "19:00",
+  end: "20:00",
+  region: "東京",
+  location: "NATSLIVE CAFE 表参道",
+  present: ["橋本桃呼", "葉月紗蘭", "松本ももな"],
+  images: [
+    // {
+    //   path: "/events/2026/2026-05-27_たかねこ初恋キッチン_1.jpg",
+    //   ref: "",
+    // },
+    // {
+    //   path: "/events/2026/2026-05-27_たかねこ初恋キッチン_2.jpg",
+    //   ref: "",
+    // },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: {
+    text: "NATSLIVE",
+    url: "https://natslive.jp/episodes/17718",
+  },
+  goods: {
+    time: undefined,
+    lineup: [
+      // "レシピカード「初恋親子丼。カード A タイプ」 660 円 (税込)",
+      // "レシピカード「初恋親子丼。カード B タイプ」 660 円 (税込)",
+      // "レシピカード「初恋親子丼。カード C タイプ」 660 円 (税込)",
+    ],
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-04-19",
+};
+
+export const content = /* md */ `
+  ## 内容
+
+  生配信。ライブ観覧あり。
+
+  配信の視聴には NATSLIVE アプリのインストールが必要です。
+
+  ## アーカイブ配信
+
+  無料会員は配信後 7 日間アーカイブ視聴可能。
+
+  ゴールド会員は配信後 1 年間アーカイブ視聴可能。
+
+  ## グッズの販売期間
+
+  2026年04月06日 19:00 〜 2026年04月07日 18:59
+
+  ## リンク
+
+  <!-- - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2041112828170633330) -->
+  <!-- - [直前告知 - 公式 X](https://x.com/takanenofficial/status/2041092500518236302) -->
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2045442264722559197)
+  - [告知 - NATSLIVE X](https://x.com/NATSLIVE_app/status/2045381817579061711)
+`;
