535968fcb8f8a834e22a982cec0cc96a8f6970e7
Keisuke Ijuin <kei10in@gmail.com>
2026-04-19 21:31:57 +0900

    配信を更新

---
+++ app/features/events/2026/04/2026-04-17_Podcasts Membership「可憐なアイボリーのカレアイ放送局」.ts
@@ -0,0 +1,61 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "Podcasts Membership「可憐なアイボリーのカレアイ放送局」",
+  category: "STREAMING",
+  date: "2026-04-17",
+  start: "22:00",
+  end: undefined,
+  region: "配信",
+  present: ["籾山ひめり"],
+  images: [
+    {
+      path: "/events/2026/2026-04-17_Podcasts Membership「可憐なアイボリーのカレアイ放送局」_1.jpg",
+      ref: "https://x.com/takanenofficial/status/2044996828438085963",
+    },
+    {
+      path: "/events/2026/2026-04-17_Podcasts Membership「可憐なアイボリーのカレアイ放送局」_2.jpg",
+      ref: "https://x.com/Karennaivory/status/2045049706263576881",
+    },
+    {
+      path: "/events/2026/2026-04-17_Podcasts Membership「可憐なアイボリーのカレアイ放送局」_3.jpg",
+      ref: "https://x.com/Karennaivory/status/2045147836493488177",
+    },
+    {
+      path: "/events/2026/2026-04-17_Podcasts Membership「可憐なアイボリーのカレアイ放送局」_4.jpg",
+      ref: "https://x.com/Karennaivory/status/2045147836493488177",
+    },
+  ],
+  link: {
+    text: "番組サイト",
+    url: "https://audee-membership.jp/karennaivory",
+  },
+  updatedAt: "2026-04-19",
+};
+
+export const content = /* md */ `
+  ## コラボ放送
+
+  「可憐なアイボリーのカレアイ放送局」とのコラボ放送 後編。
+
+  可憐なアイボリーからは永尾梨央さんが出演。
+
+  永尾梨央さんと籾山ひめりのリーダー × キャプテンコンビでスペシャル対談🎤✨
+
+  X ハッシュタグは「[#カレラジ]」
+
+  [#カレラジ]: http://x.com/search?q=%23カレラジ
+
+  - [【🤍 可憐なアイボリーのカレアイ放送局 #28🐘】ゲスト：高嶺のなでしこ 籾山 ひめり](https://audee-membership.jp/karennaivory/audio/smFKsgTgqVQa3E7BBGNcJ79M)
+  - [【🤍可憐なアイボリーのカレアイ放送局 MS限定 #28🐘 】解決！可憐にアイボリー🧦](https://audee-membership.jp/karennaivory/audio/smXRhsSiXmf3GJrgvwXygmDp)
+
+  ## リンク
+
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2045147854793232662)
+  - [当日告知 - 公式 X](https://x.com/takanenofficial/status/2044996828438085963)
+  - [放送後報告 - 可憐なアイボリー X](https://x.com/Karennaivory/status/2045147836493488177)
+  - [当日告知 - 可憐なアイボリー X](https://x.com/Karennaivory/status/2045049706263576881)
+  - [メッセージ投稿フォーム](https://www.tfm.co.jp/f/karennaivory/message)
+  - [コラボ放送告知 - 公式 X](https://x.com/takanenofficial/status/2039998921104306442)
+  - [告知 - 可憐なアイボリー X](https://x.com/Karennaivory/status/2039998859070656822)
+`;
