feb7fc01557db2c35d9e4b9d2d26fbf3ab009854
Keisuke Ijuin <kei10in@gmail.com>
2026-04-20 23:44:34 +0900

    タイムテーブルを追加。

--- app/features/events/2026/05/2026-05-10_ICONIC MOMENTS FES!.ts
+++ app/features/events/2026/05/2026-05-10_ICONIC MOMENTS FES!.ts
@@ -21,6 +21,11 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-05-10_ICONIC MOMENTS FES!_1.jpg",
       ref: "https://x.com/takanenofficial/status/2032385150840103077",
     },
+    {
+      path: "/events/2026/2026-05-10_ICONIC MOMENTS FES!_タイムテーブル.jpg",
+      ref: "https://x.com/iml_sp/status/2046061291001806908",
+      tags: ["timetable"],
+    },
   ],
   link: {
     text: "イベント サイト",
@@ -29,20 +34,23 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://tiget.net/events/473697",
   streamings: undefined,
   goods: {
-    time: undefined,
+    time: ["19:50", "20:50"],
     lineup: undefined,
     url: undefined,
   },
   acts: {
+    start: "21:05",
+    end: "21:30",
     setlist: [],
     url: "",
   },
-  updatedAt: "2026-04-19",
+  updatedAt: "2026-04-20",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [タイムテーブル公開 - ICONIC MOMENTS FES！ X](https://x.com/iml_sp/status/2046061291001806908)
   - [推しのぼり旗発売告知 - ICONIC MOMENTS FES！ X](https://x.com/iml_sp/status/2044717444544266625)
   - [コメント動画 - ICONIC MOMENTS FES！ X](https://x.com/iml_sp/status/2044626842636734824)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2032385150840103077)
