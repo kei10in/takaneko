0b5fb4901ca75aef0264887a04a9ea6aa3abd8a4
Keisuke Ijuin <kei10in@gmail.com>
2026-04-20 22:30:34 +0900

    お話会を追加。

---
+++ app/features/events/2026/04/2026-04-21_オンライン個別お話し会_受付.ts
@@ -0,0 +1,54 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "オンライン個別お話し会 受付📋",
+  category: "EVENT",
+  date: "2026-04-21",
+  start: "21:00",
+  region: "Web",
+  images: [
+    {
+      path: "/events/2026/2026-04-25_オンライン個別お話し会_1.jpg",
+      ref: "https://x.com/takanenofficial/status/2046093095792746577",
+    },
+    {
+      path: "/events/2026/2026-04-25_オンライン個別お話し会_2.jpg",
+      ref: "https://x.com/takanenofficial/status/2046093095792746577",
+    },
+  ],
+  link: {
+    text: "開催詳細",
+    url: "https://takanenonadeshiko.jp/?p=5204",
+  },
+  updatedAt: "2026-04-20",
+};
+
+export const content = /* md */ `
+  ## 開催情報
+
+  **販売開始:** 2026年04月21日 (月) 21:00 〜 順次
+
+  - 21:00 〜 🤍葉月紗蘭
+  - 21:10 〜 🧡東山恵里沙
+  - 21:20 〜 ❤️星谷美来
+  - 21:30 〜 🎀松本ももな
+  - 21:40 〜 💛城月菜央
+  - 21:50 〜 🩵涼海すう
+  - 22:00 〜 🩷橋本桃呼
+  - 22:10 〜 💜日向端ひな
+  - 22:20 〜 💙籾山ひめり
+
+  **開催日時:** 2026年04月25日 (土)
+
+  1. 13:30 〜 14:00
+  1. 14:05 〜 14:35
+  1. 14:40 〜 15:10
+  1. 15:20 〜 15:50
+  1. 15:55 〜 16:25
+  1. 16:30 〜 17:00
+
+  ## リンク
+
+  - [公式ニュース](https://takanenonadeshiko.jp/?p=5204)
+  - [公式 X 告知](https://x.com/takanenofficial/status/2046093095792746577)
+`;
