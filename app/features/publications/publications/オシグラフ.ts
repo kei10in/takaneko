5893f9f7d1cb8ada13353196e923af3f30fa1280
Keisuke Ijuin <kei10in@gmail.com>
2026-04-21 19:58:43 +0900

    雑誌を更新

--- app/features/publications/publications/オシグラフ.ts
+++ app/features/publications/publications/オシグラフ.ts
@@ -28,6 +28,10 @@ export const オシグラフVol3: Publication = {
       ref: "https://x.com/takanenofficial/status/2044362599161049276",
     },
     {
+      path: "/publications/2026/2026-05-15_オシグラフ Vol.3_橋本桃呼2.jpg",
+      ref: "https://x.com/oshigraph/status/2046539054611873982",
+    },
+    {
       path: "/publications/2026/2026-05-15_オシグラフ Vol.3_橋本桃呼抽選チェキ.webp",
       ref: "https://x.com/takanenofficial/status/2044362599161049276",
     },
@@ -38,6 +42,10 @@ export const オシグラフVol3: Publication = {
     {
       path: "/publications/2026/2026-05-15_オシグラフ Vol.3_松本ももな1.webp",
       ref: "https://x.com/takanenofficial/status/2044362599161049276",
+    },
+    {
+      path: "/publications/2026/2026-05-15_オシグラフ Vol.3_松本ももな2.jpg",
+      ref: "https://x.com/oshigraph/status/2046539006264152361",
     },
     {
       path: "/publications/2026/2026-05-15_オシグラフ Vol.3_松本ももな抽選チェキ.webp",
