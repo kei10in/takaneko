b358c14e6ea921a310e9ba412ab7ff1cab4c1995
Keisuke Ijuin <kei10in@gmail.com>
2026-04-25 15:17:07 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-27_HERO SONIC 2026.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "HERO SONIC 2026",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-06-27",
+  open: "10:00",
+  start: "11:00",
+  end: undefined,
+  region: "神奈川",
+  location: "横浜 赤レンガパーク",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-06-27_HERO SONIC 2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2047626682396500329",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://event.tiget.net/herosonic2026",
+  },
+  ticket: "https://tiget.net/events/480471",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-04-25",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2047626682396500329)
+`;
