eaccb085e5085ad68c7f5668eb6c65d32a9f3bb7
Keisuke Ijuin <kei10in@gmail.com>
2026-04-27 23:43:12 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-09_ASIA CULTURE FESTIVAL2026.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ASIA CULTURE FESTIVAL2026",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-06-09",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "東京",
+  location: "東京ガーデンシアター",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-06-09_ASIA CULTURE FESTIVAL2026.jpg",
+      ref: "https://x.com/ACF2026/status/2044946740453986442",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://acfes.jp",
+  },
+  ticket: "https://acfes.jp",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-04-27",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2048741452470251800)
+`;
