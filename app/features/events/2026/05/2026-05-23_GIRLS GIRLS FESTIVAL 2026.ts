c1472370edb121dc33612448a4bfc2c69fd7a873
Keisuke Ijuin <kei10in@gmail.com>
2026-04-27 23:51:23 +0900

    feat: GIRLS GIRLS FESTIVAL 2026のイベントを追加

---
+++ app/features/events/2026/05/2026-05-23_GIRLS GIRLS FESTIVAL 2026.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "GIRLS GIRLS FESTIVAL 2026",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-05-23",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "神奈川",
+  location: "さがみ湖MORIMORI",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-05-23_GIRLS GIRLS FESTIVAL 2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2048743967299358956",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://girls-girls-festival.com",
+  },
+  ticket: "https://tiget.net/events/483124",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-04-27",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2048743967299358956)
+`;
