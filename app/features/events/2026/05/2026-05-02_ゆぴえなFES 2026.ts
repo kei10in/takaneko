9b5af7bf0bf0492e242362fb7e2b46d8ea9853e0
Keisuke Ijuin <kei10in@gmail.com>
2026-05-02 22:56:51 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-02_ゆぴえなFES 2026.ts
+++ app/features/events/2026/05/2026-05-02_ゆぴえなFES 2026.ts
@@ -32,16 +32,25 @@ export const meta: EventMetaDescriptor = {
     lineup: undefined,
     url: undefined,
   },
-  acts: {
-    setlist: [],
-    url: "",
-  },
-  updatedAt: "2026-04-25",
+  acts: [
+    {
+      setlist: [
+        "衣装:見上げるたびに、恋をする。衣装",
+        "世界は恋に落ちている",
+        "ファンサ",
+        "美しく生きろ",
+      ],
+      url: "https://x.com/Mikuru_hositani/status/2050561496535724530",
+    },
+  ],
+  updatedAt: "2026-05-02",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [出演報告 (ダイジェスト動画あり) - 公式 X](https://x.com/takanenofficial/status/2050563813884518415)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2050211152295235971)
   - [【神回】ゆぴえなが豪華ゲストに突撃！YouTuber、アイドル、アーティスト…夢のコラボが実現しました！！！ - ゆうぴーまん YouTube](https://youtu.be/jNDtrY7Dv6k?si=xgaNuizSv7cidgxa&t=3330)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2038977194295455813)
 `;
