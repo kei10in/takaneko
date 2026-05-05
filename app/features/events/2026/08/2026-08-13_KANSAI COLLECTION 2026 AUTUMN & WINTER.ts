2e6d98e4db8e85d36c0b38bf1a6985abfa66051b
Keisuke Ijuin <kei10in@gmail.com>
2026-05-05 11:21:05 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-13_KANSAI COLLECTION 2026 AUTUMN & WINTER.ts
@@ -0,0 +1,44 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "KANSAI COLLECTION 2026 AUTUMN & WINTER",
+  category: "OTHER",
+  liveType: undefined,
+  date: "2026-08-13",
+  open: "11:30",
+  start: "13:00",
+  end: undefined,
+  region: "大阪",
+  location: "京セラドーム大阪",
+  present: ["松本ももな"],
+  images: [
+    {
+      path: "/events/2026/2026-08-13_KANSAI COLLECTION 2026 AUTUMN & WINTER.jpg",
+      ref: "https://x.com/takanenofficial/status/2050194776948195481",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://www.kansai-collection.net/",
+  },
+  ticket: "https://www.kansai-collection.net/ticket/",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-05-05",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2050194776948195481)
+`;
