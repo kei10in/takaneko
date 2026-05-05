62747bfddc762b5ad3a72217d383340952e79833
Keisuke Ijuin <kei10in@gmail.com>
2026-05-05 11:08:43 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-04_歌舞伎町UP GATE↑↑2026.ts
+++ app/features/events/2026/05/2026-05-04_歌舞伎町UP GATE↑↑2026.ts
@@ -11,6 +11,7 @@ export const meta: EventMetaDescriptor = {
   region: "東京",
   location: "Zepp Shinjuku",
   present: ["高嶺のなでしこ2"],
+  absent: ["東山恵里沙"],
   images: [
     {
       path: "/events/2026/2026-05-04_歌舞伎町UP GATE↑↑2026.webp",
@@ -21,9 +22,13 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2042860595129815306",
     },
     {
-      path: "/events/2026/2026-05-04_歌舞伎町UP GATE↑↑2026_タイムテーブル.jpg",
-      ref: "https://x.com/kabuki_upgate/status/2047601455633482102",
+      path: "/events/2026/2026-05-04_歌舞伎町UP GATE↑↑2026_タイムテーブル.webp",
+      ref: "https://x.com/takanenofficial/status/2050954303473594506",
       tags: ["timetable"],
+    },
+    {
+      path: "/events/2026/2026-05-04_歌舞伎町UP GATE↑↑2026_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2051259354603749601",
     },
   ],
   link: {
@@ -33,22 +38,54 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://ticket.rakuten.co.jp/music/jpop/idle/RTZPBBP/?scid=su_16699",
   streamings: undefined,
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["20:25"],
+    lineup: ["ミニフォトカード「私は、わたしの事が好き。」 / チェキ", "まねきねこチケット"],
+    url: "https://x.com/takanenofficial/status/2050572292640702767",
   },
-  acts: {
-    start: "19:40",
-    end: "20:10",
-    setlist: [],
-    url: "",
-  },
-  updatedAt: "2026-04-25",
+  acts: [
+    {
+      title: "歌舞伎町タワーステージ",
+      start: "16:25",
+      end: "16:45",
+      setlist: [
+        "衣装:美しく生きろ衣装",
+        "可愛くてごめん",
+        "世界は恋に落ちている",
+        "ファンサ",
+        "美しく生きろ",
+      ],
+      links: ["https://x.com/takanenofficial/status/2051226276514717940"],
+    },
+    {
+      title: "Zepp Shinjuku",
+      start: "19:40",
+      end: "20:10",
+      setlist: [
+        "衣装: 美しく生きろ衣装 (ジャケットなし)",
+        "花は誓いを忘れない",
+        "女の子は強い",
+        "初恋のこたえ。",
+        "MC",
+        "決戦スピリット",
+        "革命の女王",
+        "美しく生きろ",
+      ],
+      url: "https://x.com/Mikuru_hositani/status/2051304076282085604",
+    },
+  ],
+  updatedAt: "2026-05-05",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [出演者紹介 - 歌舞伎町UP GATE↑↑ X](https://x.com/kabuki_upgate/status/2051263068596801826)
+  - [出演報告 Zepp Shinjuku (ダイジェスト動画あり) - 公式 X](https://x.com/takanenofficial/status/2051299273627816373)
+  - [出演報告 歌舞伎町タワーステージ (ダイジェスト動画あり) - 公式 X](https://x.com/takanenofficial/status/2051226276514717940)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2051259354603749601)
+  - [東山恵里沙欠席のお知らせ - 公式 X](https://x.com/takanenofficial/status/2051161188076249550)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2050954303473594506)
+  - [まねきねこチケット販売告知 - 公式 X](https://x.com/takanenofficial/status/2050858201424511473)
   - [タイムテーブル公開 - 歌舞伎町UP GATE↑↑ X](https://x.com/kabuki_upgate/status/2047601455633482102)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2042860595129815306)
 `;
