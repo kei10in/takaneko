c19a8793ee0fc29f7792fbb2cea8ca4feaad7da3
Keisuke Ijuin <kei10in@gmail.com>
2026-05-07 22:29:14 +0900

    イベントを追加。

---
+++ app/features/events/2026/05/2026-05-07_TIF de Debut 2026 直前SHOWROOM公式番組.ts
@@ -0,0 +1,54 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "TIF de Debut 2026 直前SHOWROOM公式番組",
+  category: "STREAMING",
+  date: "2026-05-07",
+  open: undefined,
+  start: "20:00",
+  end: "21:00",
+  region: "Web",
+  location: "",
+  present: ["城月菜央"],
+  images: [
+    {
+      path: "/events/2026/2026-05-07_TIF de Debut 2026 直前SHOWROOM公式番組.webp",
+      ref: "https://www.showroom-live.com/room/profile?room_id=571753",
+    },
+    {
+      path: "/events/2026/2026-05-07_TIF de Debut 2026 直前SHOWROOM公式番組_1.jpg",
+      ref: "https://x.com/SHOWROOM_jp/status/2052362170785304679",
+    },
+  ],
+  link: {
+    text: "【公式】TIF de Debut 2026 - SHOWROOM ルームプロフィール",
+    url: "https://www.showroom-live.com/room/profile?room_id=571753",
+  },
+  ticket: "",
+  streamings: [
+    {
+      text: "SHOWROOM",
+      url: "https://www.showroom-live.com/r/tif_debut_2026",
+    },
+  ],
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-05-07",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2052370457043620219)
+  - [放送報告 - SHOWROOM X](https://x.com/SHOWROOM_jp/status/2052362170785304679)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2052239558784499906)
+`;
