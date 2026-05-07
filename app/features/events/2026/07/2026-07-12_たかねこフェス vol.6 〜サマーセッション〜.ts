07a69d0ab301569f1d90ede78de5d9bf0d29d6fc
Keisuke Ijuin <kei10in@gmail.com>
2026-05-07 23:24:32 +0900

    イベントを追加

---
+++ app/features/events/2026/07/2026-07-12_たかねこフェス vol.6 〜サマーセッション〜.ts
@@ -0,0 +1,45 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "たかねこフェス vol.6 〜サマーセッション〜",
+  category: "LIVE",
+  liveType: "HOSTED",
+  date: "2026-07-12",
+  open: "11:00",
+  start: "12:00",
+  end: undefined,
+  region: "東京",
+  location: "EX THEATER ROPPONGI",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-12_たかねこフェス vol.6 〜サマーセッション〜.jpg",
+      ref: "https://x.com/takanenofficial/status/2051981551270060290",
+    },
+  ],
+  link: {
+    text: "公式ニュース",
+    url: "https://takanenonadeshiko.jp/?p=5243",
+  },
+  ticket: "https://eplus.jp/takanenonadeshiko",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-05-07",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [公式ニュース](https://takanenonadeshiko.jp/?p=5243)
+  - [開催告知 - 公式 X](https://x.com/takanenofficial/status/2051981551270060290)
+`;
