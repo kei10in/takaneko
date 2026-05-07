92771e7deaf9df881210a16b7b3d3050063b3b79
Keisuke Ijuin <kei10in@gmail.com>
2026-05-07 23:15:53 +0900

    イベントを追加

---
+++ app/features/events/2026/08/2026-08-06_4周年 Special LIVE.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "4周年 Special LIVE",
+  category: "LIVE",
+  liveType: "SOLO",
+  date: "2026-08-06",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "神奈川",
+  location: "KT Zepp Yokohama",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-05-07",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [開催決定 - 公式 X](https://x.com/takanenofficial/status/2051982808751739008)
+`;
---
+++ app/features/events/2026/08/2026-08-07_4th ファンミーティング.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "4th ファンミーティング",
+  category: "LIVE",
+  liveType: "SOLO",
+  date: "2026-08-07",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "東京",
+  location: "ヒューリックホール東京",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-05-07",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [開催決定 - 公式 X](https://x.com/takanenofficial/status/2051982808751739008)
+`;
