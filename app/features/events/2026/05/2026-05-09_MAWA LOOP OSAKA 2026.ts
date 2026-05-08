5bfdf3b1f463d1dc113e8fc9d07f2f495315d8e2
Keisuke Ijuin <kei10in@gmail.com>
2026-05-09 08:17:57 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-09_MAWA LOOP OSAKA 2026.ts
+++ app/features/events/2026/05/2026-05-09_MAWA LOOP OSAKA 2026.ts
@@ -3,7 +3,7 @@ import { EventMetaDescriptor } from "../../eventMeta";
 export const meta: EventMetaDescriptor = {
   summary: "MAWA LOOP OSAKA 2026",
   category: "LIVE",
-  liveType: undefined,
+  liveType: "GUEST",
   date: "2026-05-09",
   open: "11:30",
   start: "12:00",
@@ -16,28 +16,41 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-05-09_MAWA LOOP OSAKA 2026.jpg",
       ref: "https://x.com/takanenofficial/status/2036026532737765462",
     },
+    {
+      path: "/takaneko/goods/2026/2026-05-03_ミニフォトカード「私は、わたしの事が好き。」.jpg",
+      ref: "https://x.com/takanenofficial/status/2052742845833089046",
+      tags: ["merch-item"],
+    },
   ],
   link: {
-    text: "",
-    url: "",
+    text: "イベント公式サイト",
+    url: "https://mawaloop.com/",
   },
-  ticket: "https://eplus.jp/sf/detail/2338770001",
-  streamings: undefined,
+  ticket: "https://mawaloop.com/tickets_info",
+  streamings: [
+    {
+      text: "ニコニコ生放送",
+      url: "https://live.nicovideo.jp/watch/lv350439374",
+    },
+  ],
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["終演後"],
+    lineup: ["ミニフォトカード「私は、わたしの事が好き。」 / チェキ", "まねきねこチケット"],
+    url: "https://x.com/takanenofficial/status/2052742845833089046",
   },
   acts: {
+    start: "18:30",
+    end: "19:10",
     setlist: [],
     url: "",
   },
-  updatedAt: "2026-03-23",
+  updatedAt: "2026-05-09",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2052742845833089046)
   - [公式モバイルアプリ](https://mawaloop.link.fespli.com/)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2036026532737765462)
 `;
