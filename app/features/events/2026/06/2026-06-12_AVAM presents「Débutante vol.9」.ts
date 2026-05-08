1f436009f355aba7dc31f567a7559eddc6bd387c
Keisuke Ijuin <kei10in@gmail.com>
2026-05-09 08:10:03 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-12_AVAM presents「Débutante vol.9」.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "AVAM presents「Débutante vol.9」",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-06-12",
+  open: "11:00",
+  start: "11:30",
+  end: undefined,
+  region: "東京",
+  location: "Zepp Diver City",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-06-12_AVAM presents「Débutante vol.9」.jpg",
+      ref: "https://x.com/takanenofficial/status/2052740573489201300",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-05-09",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2052740573489201300)
+`;
