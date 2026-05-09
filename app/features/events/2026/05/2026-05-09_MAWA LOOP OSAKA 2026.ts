b089207f921c6c1661bad85628c9509481bc2fa4
Keisuke Ijuin <kei10in@gmail.com>
2026-05-09 21:03:37 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-09_MAWA LOOP OSAKA 2026.ts
+++ app/features/events/2026/05/2026-05-09_MAWA LOOP OSAKA 2026.ts
@@ -21,6 +21,10 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2052742845833089046",
       tags: ["merch-item"],
     },
+    {
+      path: "/events/2026/2026-05-09_MAWA LOOP OSAKA 2026_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2053056018771505656",
+    },
   ],
   link: {
     text: "イベント公式サイト",
@@ -41,8 +45,21 @@ export const meta: EventMetaDescriptor = {
   acts: {
     start: "18:30",
     end: "19:10",
-    setlist: [],
-    url: "",
+    setlist: [
+      "衣装: 見上げるたびに、恋をする。衣装",
+      "初恋のひと。",
+      "初恋のこたえ。",
+      "MC",
+      "ヒロインは平均以下。",
+      "可愛くてごめん",
+      "私は、わたしの事が好き。",
+      "MC",
+      "決戦スピリット",
+      "革命の女王",
+      "ファンサ",
+      "美しく生きろ",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2053065752685818002",
   },
   updatedAt: "2026-05-09",
 };
@@ -50,6 +67,7 @@ export const meta: EventMetaDescriptor = {
 export const content = /* md */ `
   ## リンク
 
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2053056018771505656)
   - [#あしたのたかねこ](https://x.com/takanenofficial/status/2052742845833089046)
   - [公式モバイルアプリ](https://mawaloop.link.fespli.com/)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2036026532737765462)
