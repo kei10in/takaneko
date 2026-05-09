f8d520b82d9f6f9b4d36c6330f92c772a2a31926
Keisuke Ijuin <kei10in@gmail.com>
2026-05-09 11:12:16 +0900

    イベントを追加。

---
+++ app/features/events/2026/07/2026-07-26_六本木アイドルフェスティバル2026 Day2.ts
@@ -0,0 +1,48 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "六本木アイドルフェスティバル2026 Day2",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-07-26",
+  open: "10:00",
+  start: "11:00",
+  end: undefined,
+  region: "東京",
+  location: "TOKYO DREAM PARK",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-25_六本木アイドルフェスティバル2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2047593891738562806",
+    },
+    {
+      path: "/events/2026/2026-07-25_六本木アイドルフェスティバル2026_1.jpg",
+      ref: "https://x.com/takanenofficial/status/2047593891738562806",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://www.tv-asahi.co.jp/idolfes/",
+  },
+  ticket: "https://www.tv-asahi.co.jp/idolfes/#tickets",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-05-09",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [出演告知 - 公式 X](https://x.com/takanenofficial/status/2047593891738562806)
+`;
