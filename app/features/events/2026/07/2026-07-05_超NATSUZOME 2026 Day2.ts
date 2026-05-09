23ccd1df8309396e98c87f908d645e36c251fec9
Keisuke Ijuin <kei10in@gmail.com>
2026-05-09 11:22:03 +0900

    イベントを追加。

---
+++ app/features/events/2026/07/2026-07-05_超NATSUZOME 2026 Day2.ts
@@ -0,0 +1,44 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "超NATSUZOME 2026 Day2",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-07-05",
+  open: "09:00",
+  start: "10:00",
+  end: undefined,
+  region: "千葉",
+  location: "幕張海浜公園 Gブロック特設会場",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-04_超NATSUZOME 2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2047525946400583801",
+    },
+  ],
+  link: {
+    text: "", // "超NATSUZOME 公式サイト",
+    url: "", // "https://natsuzome.info/",
+  },
+  ticket: "https://r10.to/chonatsuzome2026",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-05-09",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [出演告知 - 公式 X](https://x.com/takanenofficial/status/2047525946400583801)
+`;
