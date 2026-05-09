07acdaa44227244feb08f390107bc712bd59eb9f
Keisuke Ijuin <kei10in@gmail.com>
2026-05-10 00:03:25 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-10_ICONIC MOMENTS FES!.ts
+++ app/features/events/2026/05/2026-05-10_ICONIC MOMENTS FES!.ts
@@ -26,6 +26,11 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/iml_sp/status/2046061291001806908",
       tags: ["timetable"],
     },
+    {
+      path: "/takaneko/goods/2026/2026-05-03_ミニフォトカード「私は、わたしの事が好き。」.jpg",
+      ref: "https://x.com/takanenofficial/status/2052742845833089046",
+      tags: ["merch-item"],
+    },
   ],
   link: {
     text: "イベント サイト",
@@ -40,8 +45,8 @@ export const meta: EventMetaDescriptor = {
   ],
   goods: {
     time: ["19:50", "20:50"],
-    lineup: undefined,
-    url: undefined,
+    lineup: ["ミニフォトカード「私は、わたしの事が好き。」 / チェキ", "まねきねこチケット"],
+    url: "https://x.com/takanenofficial/status/2053120369477972215",
   },
   acts: {
     start: "21:05",
@@ -49,12 +54,13 @@ export const meta: EventMetaDescriptor = {
     setlist: [],
     url: "",
   },
-  updatedAt: "2026-05-09",
+  updatedAt: "2026-05-10",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2053120369477972215)
   - [前方エリア入れ替えについて - ICONIC MOMENTS FES！ X](https://x.com/iml_sp/status/2052327590783033776)
   - [タイムテーブル公開 - ICONIC MOMENTS FES！ X](https://x.com/iml_sp/status/2046061291001806908)
   - [推しのぼり旗発売告知 - ICONIC MOMENTS FES！ X](https://x.com/iml_sp/status/2044717444544266625)
