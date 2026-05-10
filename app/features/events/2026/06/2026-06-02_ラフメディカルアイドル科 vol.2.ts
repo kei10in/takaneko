f6fb8168afa0cfd903b56bfad879b33f3fe8f530
Keisuke Ijuin <kei10in@gmail.com>
2026-05-10 22:56:15 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-02_ラフメディカルアイドル科 vol.2.ts
@@ -0,0 +1,41 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ラフメディカルアイドル科 vol.2",
+  category: "OTHER",
+  date: "2026-06-02",
+  open: "19:10",
+  start: "19:30",
+  end: undefined,
+  region: "東京",
+  location: "北沢タウンホール",
+  present: ["籾山ひめり"],
+  images: [
+    {
+      path: "/events/2026/2026-06-02_ラフメディカルアイドル科 vol.2.jpg",
+      ref: "https://x.com/takanenofficial/status/2053431126124052940",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://livepocket.jp/e/laughmedical2",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-05-10",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2053431126124052940)
+`;
