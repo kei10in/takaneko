544c92aff5974ceb8d6b949e0c86e4d39105269f
Keisuke Ijuin <kei10in@gmail.com>
2026-05-11 23:47:12 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-11_ICONIC MOMENTS LIVE!.ts
+++ app/features/events/2026/05/2026-05-11_ICONIC MOMENTS LIVE!.ts
@@ -21,6 +21,15 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/IconicM_live/status/2039266527627936233",
       tags: ["timetable"],
     },
+    {
+      path: "/takaneko/goods/2026/2026-05-03_ミニフォトカード「私は、わたしの事が好き。」.jpg",
+      ref: "https://x.com/takanenofficial/status/2053482710669344994",
+      tags: ["merch-item"],
+    },
+    {
+      path: "/events/2026/2026-05-11_ICONIC MOMENTS LIVE!_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2053792433130143902",
+    },
   ],
   link: {
     text: "イベント サイト",
@@ -35,23 +44,30 @@ export const meta: EventMetaDescriptor = {
   ],
   goods: {
     time: ["20:05", "21:05"],
-    lineup: undefined,
-    url: undefined,
+    lineup: ["ミニフォトカード「私は、わたしの事が好き。」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2053482710669344994",
   },
   acts: [
     {
       title: "1部 公開収録",
       start: "13:30",
       end: "13:50",
-      setlist: [],
       url: "",
     },
     {
       title: "2部 LIVE",
       start: "19:25",
       end: "19:50",
-      setlist: [],
-      url: "",
+      setlist: [
+        "衣装: 僕は君になれない衣装",
+        "僕は君になれない",
+        "センパイ。",
+        "MC",
+        "生きてりゃいい",
+        "小悪魔だってかまわない！",
+        "恋を知った世界",
+      ],
+      url: "https://x.com/Mikuru_hositani/status/2053803559284859230",
     },
   ],
   updatedAt: "2026-05-05",
@@ -60,6 +76,12 @@ export const meta: EventMetaDescriptor = {
 export const content = /* md */ `
   ## リンク
 
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2053792433130143902)
+  - [1部 公開収録 出演報告 - 公式 X](https://x.com/takanenofficial/status/2053717355348574626)
+  - [2部 LIVE 出演報告 - 公式 X](https://x.com/takanenofficial/status/2053809900036530395)
+  - [1部 公開収録 出演者紹介 - ICONIC MOMENTS LIVE! X](https://x.com/IconicM_live/status/2053697644434985046)
+  - [2部 LIVE 出演者紹介 - ICONIC MOMENTS LIVE! X](https://x.com/IconicM_live/status/2053785936765579419)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2053482710669344994)
   - [タイムテーブル公開 - ICONIC MOMENTS LIVE! X](https://x.com/IconicM_live/status/2039266527627936233)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2030629616218440068)
 `;
