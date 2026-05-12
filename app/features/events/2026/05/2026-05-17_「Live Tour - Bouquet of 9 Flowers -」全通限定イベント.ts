a8b7c09197b8e2d450f5bfe73ae5322a896abaf3
Keisuke Ijuin <kei10in@gmail.com>
2026-05-12 21:40:20 +0900

    イベントを追加。

---
+++ app/features/events/2026/05/2026-05-17_「Live Tour - Bouquet of 9 Flowers -」全通限定イベント.ts
@@ -0,0 +1,45 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "「Live Tour - Bouquet of 9 Flowers -」全通限定イベント",
+  category: "EVENT",
+  liveType: undefined,
+  date: "2026-05-17",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "東京",
+  location: "",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-02-14_Live Tour - Bouquet of 9 Flowers -_スタンプラリー.jpg",
+      ref: "https://x.com/takanenofficial/status/1998706785805627819",
+    },
+  ],
+  link: {
+    text: "公式ニュース",
+    url: "https://takanenonadeshiko.jp/?p=5237",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-05-12",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [公式ニュース](https://takanenonadeshiko.jp/?p=5237)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2051897245134422428)
+`;
