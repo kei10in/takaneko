558864812d562091ba1f698cc04f3a3bf19c0eef
Keisuke Ijuin <kei10in@gmail.com>
2026-05-13 23:03:22 +0900

    ラジオ出演を更新

--- app/features/events/2026/05/2026-05-12_TOKYO FM「たかねこナイト」.ts
+++ app/features/events/2026/05/2026-05-12_TOKYO FM「たかねこナイト」.ts
@@ -8,18 +8,26 @@ export const meta: EventMetaDescriptor = {
   start: "27:00",
   end: "27:30",
   region: "ラジオ",
-  present: ["東山恵里沙"],
+  present: ["葉月紗蘭", "東山恵里沙", "松本ももな"],
   images: [
     {
-      path: "",
-      ref: "",
+      path: "/events/2026/2026-05-12_TOKYO FM「たかねこナイト」.jpg",
+      ref: "https://x.com/takanenofficial/status/2054056483244101714",
+    },
+    {
+      path: "/events/2026/2026-05-12_TOKYO FM「たかねこナイト」_1.jpg",
+      ref: "https://x.com/PodcastsMs_jp/status/2054154630871826656",
+    },
+    {
+      path: "/events/2026/2026-05-12_TOKYO FM「たかねこナイト」_2.jpg",
+      ref: "https://x.com/PodcastsMs_jp/status/2054350913075110056",
     },
   ],
   link: {
     text: "番組サイト",
     url: "https://audee-membership.jp/takanenonadeshiko",
   },
-  updatedAt: "2026-04-01",
+  updatedAt: "2026-05-12",
 };
 
 export const content = /* md */ `
@@ -27,9 +35,8 @@ export const content = /* md */ `
 
   - TOKYO FM 毎週火曜日 27:00 〜 27:30
   - [Radiko 放送回](https://radiko.jp/share?sid=FMT&t=20260513030000)
-
-  <!-- - [Podcasts Membership]() -->
-  <!-- - [メンバーシップ限定 アンコール]() -->
+  - [Podcasts Membership](https://audee-membership.jp/takanenonadeshiko/audio/smAC6FgpVu3VGdswetwx7gpy)
+  - [メンバーシップ限定 アンコールトーク](https://audee-membership.jp/takanenonadeshiko/audio/smg7nyyid7kHtYXgFMn2XNix)
 
   ## 番組説明
 
@@ -52,6 +59,9 @@ export const content = /* md */ `
   ## リンク
 
   - [Radiko](https://radiko.jp/share?sid=FMT&t=20260513030000)
+  - [メンバーシップ告知 - ポッドキャストメンバーシップ X](https://x.com/PodcastsMs_jp/status/2054350913075110056)
+  - [当日告知 - ポッドキャストメンバーシップ X](https://x.com/PodcastsMs_jp/status/2054154630871826656)
+  - [当日告知 - 公式 X](https://x.com/takanenofficial/status/2054056483244101714)
   - [メッセージ投稿フォーム](https://tfm.co.jp/f/takanenonadeshiko/message)
   - [公式 X 告知](https://x.com/takanenofficial/status/1971409392299016409)
   - [Wikipedia](https://ja.wikipedia.org/wiki/高嶺のなでしこラジオ定期公演「たかねこナイト」)
