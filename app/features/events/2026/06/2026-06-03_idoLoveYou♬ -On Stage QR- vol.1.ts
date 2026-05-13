639390c587d9106888e79858d132054fbf5a512d
Keisuke Ijuin <kei10in@gmail.com>
2026-05-13 22:49:45 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "idoLoveYou♬ -On Stage QR- vol.1",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-06-03",
+  open: "10:00",
+  start: "10:30",
+  end: undefined,
+  region: "東京",
+  location: "Zepp Shinjuku",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1.jpg",
+      ref: "https://x.com/takanenofficial/status/2054534623347331372",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://www.joqr.co.jp/ic/article/175035/",
+  },
+  ticket: "https://ticketdive.com/event/idoLoveYou_0603",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-05-13",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2054534623347331372)
+`;
