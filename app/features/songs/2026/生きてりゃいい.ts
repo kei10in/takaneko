f330b832148e72835abb1ae8f1bcc793e2441cf5
Keisuke Ijuin <kei10in@gmail.com>
2026-05-13 23:38:49 +0900

    楽曲「生きてりゃいい」を追加

---
+++ app/features/songs/2026/生きてりゃいい.ts
@@ -0,0 +1,30 @@
+import { Original, Repertoire } from "../tags";
+import { SongMetaDescriptor } from "../types";
+
+export const 生きてりゃいい: SongMetaDescriptor = {
+  slug: "生きてりゃいい",
+  name: "生きてりゃいい",
+
+  lyricsBy: "shito, 中西",
+  composedBy: "shito",
+  arrangedBy: "HoneyWorks",
+
+  choreographedBy: "いどみん",
+
+  videoRelease: "2026-05-13",
+  digitalRelease: "2026-05-13",
+  liveDebut: "2026-05-06",
+
+  tags: [Original, Repertoire],
+
+  coverArt: "/takaneko/songs/2026/生きてりゃいい.jpg",
+  // ほのかえる https://x.com/honokaeru_i/status/2052043919312265553
+
+  youtube: [
+    {
+      text: "MV",
+      videoId: "https://www.youtube.com/watch?v=957yJCyB-tk",
+      publishedAt: "2026-05-13",
+    },
+  ],
+};
