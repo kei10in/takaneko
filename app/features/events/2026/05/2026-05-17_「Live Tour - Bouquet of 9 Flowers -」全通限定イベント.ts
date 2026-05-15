4ec32d2d470943e7cca281b82d29a32fc4161913
Keisuke Ijuin <kei10in@gmail.com>
2026-05-15 21:44:17 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-17_「Live Tour - Bouquet of 9 Flowers -」全通限定イベント.ts
+++ app/features/events/2026/05/2026-05-17_「Live Tour - Bouquet of 9 Flowers -」全通限定イベント.ts
@@ -5,16 +5,20 @@ export const meta: EventMetaDescriptor = {
   category: "EVENT",
   liveType: undefined,
   date: "2026-05-17",
-  open: undefined,
-  start: undefined,
+  open: "16:30",
+  start: "17:00",
   end: undefined,
   region: "東京",
-  location: "",
+  location: "新宿村LIVE",
   present: ["高嶺のなでしこ2"],
   images: [
     {
       path: "/events/2026/2026-02-14_Live Tour - Bouquet of 9 Flowers -_スタンプラリー.jpg",
       ref: "https://x.com/takanenofficial/status/1998706785805627819",
+    },
+    {
+      path: "/events/2026/2026-05-17_「Live Tour - Bouquet of 9 Flowers -」全通限定イベント_限定たかねこくじ.jpg",
+      ref: "https://x.com/takanenofficial/status/2054864296505704721",
     },
   ],
   link: {
@@ -24,9 +28,9 @@ export const meta: EventMetaDescriptor = {
   ticket: "",
   streamings: undefined,
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["14:30", "15:45"],
+    lineup: ["限定たかねこくじ"],
+    url: "https://x.com/takanenofficial/status/2054864296505704721",
   },
   acts: [
     {
@@ -34,12 +38,22 @@ export const meta: EventMetaDescriptor = {
       url: "",
     },
   ],
-  updatedAt: "2026-05-12",
+  updatedAt: "2026-05-15",
 };
 
 export const content = /* md */ `
+  ## 特典引換について
+
+  🚀 **開始:** 16:00 〜 16:15<br/>
+  📷 **特典:** 5 会場特典 ツアーグッズサイン入れ<br/>
+
+  🚀 **開始:** 限定イベント終演後<br/>
+  📷 **特典:** 6 会場特典 15秒動画撮影、7 会場 / 13 公演特典 2 ショットチェキ <br/>
+
   ## リンク
 
+  - [イベント詳細 - 公式ニュース](https://takanenonadeshiko.jp/?p=5286)
+  - [詳細告知 - 公式 X](https://x.com/takanenofficial/status/2054864296505704721)
   - [公式ニュース](https://takanenonadeshiko.jp/?p=5237)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2051897245134422428)
 `;
