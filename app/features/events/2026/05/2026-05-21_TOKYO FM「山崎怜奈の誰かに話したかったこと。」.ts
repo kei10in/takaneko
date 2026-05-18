9b301670ff4314e19cff36a5201ac84bb2837bd5
Keisuke Ijuin <kei10in@gmail.com>
2026-05-18 22:37:56 +0900

    ラジオ出演を追加。

---
+++ app/features/events/2026/05/2026-05-21_TOKYO FM「山崎怜奈の誰かに話したかったこと。」.ts
@@ -0,0 +1,29 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "TOKYO FM「山崎怜奈の誰かに話したかったこと。」",
+  category: "RADIO",
+  date: "2026-05-21",
+  start: "13:00",
+  end: "14:55",
+  region: "ラジオ",
+  present: [],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "番組サイト",
+    url: "https://www.tfm.co.jp/darehana/",
+  },
+  updatedAt: "2026-05-18",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [radiko 番組放送回](https://radiko.jp/#!/ts/FMT/20260521130000)
+  - [告知 - 山崎怜奈の誰かに話したかったこと。X](https://x.com/darehanaTFM/status/2054837538456756290)
+`;
