5d8c72cab96aea2ca23c01f3a5f1d9c60f24b746
Keisuke Ijuin <kei10in@gmail.com>
2026-05-21 22:38:49 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-21_TOKYO FM「山崎怜奈の誰かに話したかったこと。」.ts
+++ app/features/events/2026/05/2026-05-21_TOKYO FM「山崎怜奈の誰かに話したかったこと。」.ts
@@ -7,23 +7,40 @@ export const meta: EventMetaDescriptor = {
   start: "13:00",
   end: "14:55",
   region: "ラジオ",
-  present: [],
+  present: ["葉月紗蘭", "東山恵里沙"],
   images: [
     {
-      path: "",
-      ref: "",
+      path: "/events/2026/2026-05-21_TOKYO FM「山崎怜奈の誰かに話したかったこと。」.jpg",
+      ref: "https://x.com/darehanaTFM/status/2057321234681979387",
+    },
+    {
+      path: "/events/2026/2026-05-21_TOKYO FM「山崎怜奈の誰かに話したかったこと。」_1.jpg",
+      ref: "https://x.com/darehanaTFM/status/2057331485854843265",
+    },
+    {
+      path: "/events/2026/2026-05-21_TOKYO FM「山崎怜奈の誰かに話したかったこと。」_2.jpg",
+      ref: "https://x.com/takanenofficial/status/2057373820206698549",
+    },
+    {
+      path: "/events/2026/2026-05-21_TOKYO FM「山崎怜奈の誰かに話したかったこと。」_3.jpg",
+      ref: "https://x.com/takanenofficial/status/2057373820206698549",
     },
   ],
   link: {
     text: "番組サイト",
     url: "https://www.tfm.co.jp/darehana/",
   },
-  updatedAt: "2026-05-18",
+  updatedAt: "2026-05-21",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [radiko 出演時間](https://radiko.jp/#!/ts/FMT/20260521135420)
   - [radiko 番組放送回](https://radiko.jp/#!/ts/FMT/20260521130000)
+  - [出演者紹介 - 番組 X](https://x.com/darehanaTFM/status/2057331485854843265)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2057373820206698549)
+  - [直前告知 - 番組 X](https://x.com/darehanaTFM/status/2057321234681979387)
+  - [直前告知 - 公式 X](https://x.com/takanenofficial/status/2057317441563410852)
   - [告知 - 山崎怜奈の誰かに話したかったこと。X](https://x.com/darehanaTFM/status/2054837538456756290)
 `;
