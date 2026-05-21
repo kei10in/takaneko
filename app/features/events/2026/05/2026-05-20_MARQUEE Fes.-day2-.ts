2215f4588bd93993047713c0626a9c9d395090d3
Keisuke Ijuin <kei10in@gmail.com>
2026-05-21 22:45:24 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-20_MARQUEE Fes.-day2-.ts
+++ app/features/events/2026/05/2026-05-20_MARQUEE Fes.-day2-.ts
@@ -21,6 +21,15 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/MARQUEE_matsuri/status/2042467615344296324",
       tags: ["timetable"],
     },
+    {
+      path: "/takaneko/goods/2026/2026-05-03_ミニフォトカード「私は、わたしの事が好き。」.jpg",
+      ref: "https://x.com/takanenofficial/status/2052742845833089046",
+      tags: ["merch-item"],
+    },
+    {
+      path: "/events/2026/2026-05-20_MARQUEE Fes.-day2-_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2057065050536615964",
+    },
   ],
   link: {
     text: "",
@@ -30,20 +39,31 @@ export const meta: EventMetaDescriptor = {
   streamings: undefined,
   goods: {
     time: ["20:45", "21:55"],
-    lineup: undefined,
-    url: undefined,
+    lineup: ["ミニフォトカード「私は、わたしの事が好き。」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2056966931010421246",
   },
   acts: {
     start: "20:15",
     end: "20:40",
-    setlist: [],
-    url: "",
+    setlist: [
+      "衣装: アンチファン衣装",
+      "美しく生きろ",
+      "アンチファン",
+      "MC",
+      "生きてりゃいい",
+      "ファンサ",
+      "I’M YOUR IDOL",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2057076708453613988",
   },
-  updatedAt: "2026-04-10",
+  updatedAt: "2026-05-21",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2057068843286323276)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2057065050536615964)
+  - [きょうのたかねこ](https://x.com/takanenofficial/status/2056966931010421246)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2041742987651379384)
 `;
