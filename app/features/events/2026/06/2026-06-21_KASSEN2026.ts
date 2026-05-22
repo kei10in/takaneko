8941dbe51544c6815ae529297b67e7045d9e266a
Keisuke Ijuin <kei10in@gmail.com>
2026-05-22 22:13:24 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-21_KASSEN2026.ts
@@ -0,0 +1,43 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "KASSEN2026",
+  title: "NEO JAPONISM PRESENTS KASSEN2026",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-06-21",
+  open: "10:30",
+  start: "11:05",
+  end: undefined,
+  region: "東京",
+  location: "ダイバーシティ東京 プラザ フェスティバル広場",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-06-21_NEO KASSEN 2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2057680352722686370",
+    },
+  ],
+  link: {
+    text: "イベント サイト",
+    url: "https://neojaponism.com/posts/2026-05-15",
+  },
+  ticket: "https://ticketdive.com/event/kassen-2026-06-21",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-05-22",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2057680352722686370)
+`;
