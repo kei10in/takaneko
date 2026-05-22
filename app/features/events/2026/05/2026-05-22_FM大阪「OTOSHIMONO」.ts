7b22e6abf32918102df57daf4011cd122a43b649
Keisuke Ijuin <kei10in@gmail.com>
2026-05-22 21:53:13 +0900

    ラジオ出演を追加。

---
+++ app/features/events/2026/05/2026-05-22_FM大阪「OTOSHIMONO」.ts
@@ -0,0 +1,28 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "FM大阪「OTOSHIMONO」",
+  category: "RADIO",
+  date: "2026-05-22",
+  start: "20:00",
+  end: "21:00",
+  region: "ラジオ",
+  present: ["涼海すう", "東山恵里沙"],
+  images: [
+    {
+      path: "/events/2026/2026-05-22_FM大阪「OTOSHIMONO」.jpg",
+      ref: "https://x.com/takanenofficial/status/2057756455043039335",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  updatedAt: "2026-05-22",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [radiko 番組放送回](https://radiko.jp/share/?sid=FMO&t=20260522200000)
+`;
