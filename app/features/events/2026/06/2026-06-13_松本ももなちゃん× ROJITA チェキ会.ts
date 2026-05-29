2f9d3f9242eef5fda8650c2c9932f39ad6e8a922
Keisuke Ijuin <kei10in@gmail.com>
2026-05-29 22:18:46 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-13_松本ももなちゃん× ROJITA チェキ会.ts
@@ -0,0 +1,71 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "松本ももなちゃん× ROJITA チェキ会",
+  category: "EVENT",
+  liveType: undefined,
+  date: "2026-06-13",
+  open: undefined,
+  start: "15:00",
+  end: "17:30",
+  region: "東京",
+  location: "渋谷 109",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2060315768122999090",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_1.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2060315898108588094",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_2.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2060315898108588094",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_3.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2060315898108588094",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_4.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2060315898108588094",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  meetAndGreet: {
+    sessions: [
+      {
+        title: "1 部",
+        start: "15:00",
+        end: "16:00",
+        lanes: ["松本ももな"],
+      },
+      {
+        title: "2 部",
+        start: "16:30",
+        end: "17:30",
+        lanes: ["松本ももな"],
+      },
+    ],
+  },
+  updatedAt: "2026-05-29",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - ROJITA X](https://x.com/ROJITA__jp/status/2060315768122999090)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2060317729698332855)
+`;
