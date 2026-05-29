f473933188ec52c1c029d07a829e9b23b3b9d8db
Keisuke Ijuin <kei10in@gmail.com>
2026-05-29 23:27:18 +0900

    イベントを更新

--- app/features/events/2026/06/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1.ts
+++ app/features/events/2026/06/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1.ts
@@ -16,6 +16,11 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1.jpg",
       ref: "https://x.com/takanenofficial/status/2054534623347331372",
     },
+    {
+      path: "/events/2026/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1_タイムテーブル.png",
+      ref: "https://x.com/ily_joqr/status/2057053731301245176",
+      tags: ["timetable"],
+    },
   ],
   link: {
     text: "イベント公式サイト",
@@ -24,19 +29,22 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://ticketdive.com/event/idoLoveYou_0603",
   streamings: undefined,
   goods: {
-    time: undefined,
+    time: ["20:20", "21:20"],
     lineup: undefined,
     url: undefined,
   },
   acts: {
+    start: "19:25",
+    end: "19:50",
     setlist: [],
     url: "",
   },
-  updatedAt: "2026-05-13",
+  updatedAt: "2026-05-29",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [タイムテーブル公開 - idolLoveYou♬ -On The Radio- X](https://x.com/ily_joqr/status/2057053731301245176)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2054534623347331372)
 `;
