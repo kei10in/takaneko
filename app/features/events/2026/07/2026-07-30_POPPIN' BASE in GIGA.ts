35e6b964eb6c91e22d6ebabc7c49ad9d22c7726a
Keisuke Ijuin <kei10in@gmail.com>
2026-05-30 22:57:04 +0900

    feat: POPPIN' BASE in GIGA のイベントを追加

---
+++ app/features/events/2026/07/2026-07-30_POPPIN' BASE in GIGA.ts
@@ -0,0 +1,45 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "POPPIN' BASE in GIGA",
+  title: "THANKS 10TH GIGA Special LIVE「POPPIN' BASE in GIGA」",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-07-30",
+  open: "12:00",
+  start: "13:00",
+  end: undefined,
+  region: "大阪",
+  location: "おおきにアリーナ舞洲",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-30_POPPIN BASE in GIGA.jpg",
+      ref: "https://x.com/POPPINBASE/status/2060647708525019532",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://poppinbase.info/",
+  },
+  ticket: "https://eplus.jp/poppinbaseingiga/",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-05-30",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - POPPIN' BASE 公式 X](https://x.com/POPPINBASE/status/2060647708525019532)
+`;
