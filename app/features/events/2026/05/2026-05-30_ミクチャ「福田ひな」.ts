76a8d111ea8b479e4cde882d6c892883a8c411fa
Keisuke Ijuin <kei10in@gmail.com>
2026-05-31 20:59:35 +0900

    配信を追加。

---
+++ app/features/events/2026/05/2026-05-30_ミクチャ「福田ひな」.ts
@@ -0,0 +1,61 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ミクチャ「福田ひな」",
+  category: "STREAMING",
+  liveType: undefined,
+  date: "2026-05-30",
+  open: undefined,
+  start: "21:00",
+  end: "23:00",
+  region: "Web",
+  location: "",
+  present: ["城月菜央", "涼海すう"],
+  images: [
+    {
+      path: "/events/2026/2026-05-30_ミクチャ「福田ひな」.jpg",
+      ref: "https://x.com/takanenofficial/status/2059862230397555002",
+    },
+    {
+      path: "/events/2026/2026-05-30_ミクチャ「福田ひな」_1.jpg",
+      ref: "https://x.com/takanenofficial/status/2059862230397555002",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: {
+    text: "ミクチャ",
+    url: "https://mixch.tv/u/18887057",
+  },
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-05-31",
+};
+
+export const content = /* md */ `
+  ## 出演時間
+
+  - ⏰ 21:00 〜 22:00 - 城月菜央
+  - ⏰ 22:00 〜 23:00 - 涼海すう
+
+  ## その他
+
+  24日の配信には、葉月紗蘭、籾山ひめりが出演していました。
+
+  ## リンク
+
+  - [直前告知 - 公式 X](https://x.com/takanenofficial/status/2060691856305140022)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2059862230397555002)
+`;
