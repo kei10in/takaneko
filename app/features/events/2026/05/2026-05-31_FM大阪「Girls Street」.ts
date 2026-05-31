4ffa75ab88f923ef99b81a73763cc00b1f0e01e6
Keisuke Ijuin <kei10in@gmail.com>
2026-05-31 20:17:39 +0900

    ラジオ出演を追加。

---
+++ app/features/events/2026/05/2026-05-31_FM大阪「Girls Street」.ts
@@ -0,0 +1,29 @@
+import { EventMetaDescriptor } from "~/features/events/eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "FM大阪「Girls Street」",
+  category: "RADIO",
+  date: "2026-05-31",
+  start: "23:55",
+  end: "24:00",
+  region: "ラジオ",
+  present: ["橋本桃呼", "日向端ひな"],
+  images: [
+    {
+      path: "/events/2026/2026-05-31_FM大阪「Girls Street」.jpg",
+      ref: "https://x.com/takanenofficial/status/2060995498434273594",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  updatedAt: "2026-05-31",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [radiko 番組放送回](https://radiko.jp/share/?sid=FMO&t=20260531235500)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2060995498434273594)
+`;
