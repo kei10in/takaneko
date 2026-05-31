380ffd9ef9a32693ef2aff1627d496ec6a46c81f
Keisuke Ijuin <kei10in@gmail.com>
2026-05-31 15:38:40 +0900

    イベントを更新

--- app/features/events/2026/07/2026-07-12_たかねこフェス vol.6 〜サマーセッション〜.ts
+++ app/features/events/2026/07/2026-07-12_たかねこフェス vol.6 〜サマーセッション〜.ts
@@ -14,7 +14,12 @@ export const meta: EventMetaDescriptor = {
   images: [
     {
       path: "/events/2026/2026-07-12_たかねこフェス vol.6 〜サマーセッション〜.jpg",
-      ref: "https://x.com/takanenofficial/status/2051981551270060290",
+      ref: "https://x.com/takanenofficial/status/2060527234348388737",
+    },
+    {
+      path: "/events/2026/2026-07-12_たかねこフェス vol.6 〜サマーセッション〜_タイムテーブル.jpg",
+      ref: "https://x.com/takanenofficial/status/2060527234348388737",
+      tags: ["timetable"],
     },
   ],
   link: {
@@ -30,16 +35,27 @@ export const meta: EventMetaDescriptor = {
   },
   acts: [
     {
+      title: "1 ステージ目",
+      start: "12:15",
+      end: "12:30",
+      setlist: [],
+      url: "",
+    },
+    {
+      title: "2 ステージ目",
+      start: "20:35",
+      end: "21:00",
       setlist: [],
       url: "",
     },
   ],
-  updatedAt: "2026-05-07",
+  updatedAt: "2026-05-31",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2060527234348388737)
   - [公式ニュース](https://takanenonadeshiko.jp/?p=5243)
   - [開催告知 - 公式 X](https://x.com/takanenofficial/status/2051981551270060290)
 `;
