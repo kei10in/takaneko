a54728639c888b4741e77035e9261367623f6c9c
Keisuke Ijuin <kei10in@gmail.com>
2026-06-03 08:23:06 +0900

    イベントを更新

--- app/features/events/2026/06/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1.ts
+++ app/features/events/2026/06/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1.ts
@@ -5,8 +5,8 @@ export const meta: EventMetaDescriptor = {
   category: "LIVE",
   liveType: "GUEST",
   date: "2026-06-03",
-  open: "10:00",
-  start: "10:30",
+  open: "10:30",
+  start: "11:00",
   end: undefined,
   region: "東京",
   location: "Zepp Shinjuku",
@@ -17,9 +17,14 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2054534623347331372",
     },
     {
+      path: "/events/2026/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1_タイムテーブル変更版.jpg",
+      ref: "https://x.com/ily_joqr/status/2061727015687057547",
+      tags: ["timetable"],
+    },
+    {
       path: "/events/2026/2026-06-03_idoLoveYou♬ -On Stage QR- vol.1_タイムテーブル.png",
       ref: "https://x.com/ily_joqr/status/2057053731301245176",
-      tags: ["timetable"],
+      tags: [],
     },
   ],
   link: {
@@ -29,22 +34,30 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://ticketdive.com/event/idoLoveYou_0603",
   streamings: undefined,
   goods: {
-    time: ["20:20", "21:20"],
+    time: ["20:50", "21:50"],
     lineup: undefined,
     url: undefined,
   },
   acts: {
-    start: "19:25",
-    end: "19:50",
+    start: "19:55",
+    end: "20:20",
     setlist: [],
     url: "",
   },
-  updatedAt: "2026-05-29",
+  updatedAt: "2026-06-03",
 };
 
 export const content = /* md */ `
+  ## タイムテーブルの変更について
+
+  台風6号の接近に伴ってタイムテーブルが変更されています。詳細は下記を参照してください。
+
+  - https://x.com/ily_joqr/status/2061727015687057547
+  - https://x.com/takanenofficial/status/2061749671123398668
+
   ## リンク
 
+  - [台風6号 接近に伴う対応について - idolLoveYou♬ -On The Radio- X](https://x.com/ily_joqr/status/2061727015687057547)
   - [タイムテーブル公開 - idolLoveYou♬ -On The Radio- X](https://x.com/ily_joqr/status/2057053731301245176)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2054534623347331372)
 `;
