09c98ba7bdff4dbaebd3045271fcce480deb25be
Keisuke Ijuin <kei10in@gmail.com>
2026-06-03 22:33:09 +0900

    イベントを更新

--- app/features/events/2026/05/2026-05-31_Anime Festival Asia Thailand 2026.ts
+++ app/features/events/2026/05/2026-05-31_Anime Festival Asia Thailand 2026.ts
@@ -8,7 +8,7 @@ export const meta: EventMetaDescriptor = {
   open: undefined,
   start: undefined,
   end: undefined,
-  region: "タイ",
+  region: "バンコク",
   location: "Queen Sirikit National Convention Center",
   present: ["橋本桃呼", "松本ももな"],
   images: [
@@ -19,6 +19,10 @@ export const meta: EventMetaDescriptor = {
     {
       path: "/events/2026/2026-05-31_Anime Festival Asia Thailand 2026_1.jpg",
       ref: "https://x.com/takanenofficial/status/2060975911869235471",
+    },
+    {
+      path: "/events/2026/2026-05-31_Anime Festival Asia Thailand 2026_タイムテーブル.jpg",
+      ref: "https://animefestival.asia/afath26/concert/",
     },
   ],
   link: {
@@ -33,8 +37,20 @@ export const meta: EventMetaDescriptor = {
     url: undefined,
   },
   acts: {
-    setlist: [],
-    url: "",
+    start: "16:00",
+    end: "17:00",
+    setlist: [
+      "金曜日のおはよう (可憐なアイボリー)",
+      "センパイ。 (ハコニワリリィ & 可憐なアイボリー)",
+      "私は、わたしの事が好き。 (ハコニワリリィ)",
+      "可愛くてごめん (高嶺のなでしこ)",
+      "世界は恋に落ちている (CHiCO & 高嶺のなでしこ)",
+      "プライド革命 (CHiCO)",
+      "MC",
+      "決戦スピリット (全員)",
+      "ファンサ (全員)",
+    ],
+    links: ["https://prtimes.jp/main/html/rd/p/000000916.000025517.html"],
   },
   updatedAt: "2026-05-31",
 };
@@ -42,6 +58,9 @@ export const meta: EventMetaDescriptor = {
 export const content = /* md */ `
   ## リンク
 
+  - [ライブレポート](https://prtimes.jp/main/html/rd/p/000000916.000025517.html)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2061099211492614501)
+  - [出演報告 - HoneyWorks X](https://x.com/HoneyWorks_828/status/2061372191027904550)
   - [きょうのたかねこ](https://x.com/takanenofficial/status/2060975911869235471)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2035193861899985206)
 `;
