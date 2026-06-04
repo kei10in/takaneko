212d5135f5e079e42b91d25dc9d59a3650bc3f52
Keisuke Ijuin <kei10in@gmail.com>
2026-06-04 16:46:32 +0900

    refactor: メンバーカードをコンポーネント化

--- app/routes/_site.members._index/route.tsx
+++ app/routes/_site.members._index/route.tsx
@@ -5,6 +5,7 @@ import { Link, MetaFunction } from "react-router";
 import { pageBox, pageHeading, sectionHeading } from "~/components/styles";
 import { formatTitle } from "~/utils/htmlHeader";
 import { AllMembers } from "../../features/profile/members";
+import { MemberIdCard } from "./MemberIdCard";
 
 export const meta: MetaFunction = () => {
   return [
@@ -73,42 +74,7 @@ export default function Index() {
           <ul className="flex flex-wrap justify-center gap-4">
             {AllMembers.map((member) => (
               <li key={member.name} className="flex-none">
-                <Link
-                  className="flex h-44 w-80 flex-none flex-col overflow-hidden rounded-xl bg-white shadow-md"
-                  to={`/members/${member.slug}`}
-                >
-                  <div
-                    className="flex h-6 flex-none items-center justify-center font-serif text-white"
-                    style={{ backgroundColor: member.color }}
-                  >
-                    高嶺のなでしこ
-                  </div>
-                  <div className="flex flex-1 items-stretch justify-stretch gap-3 p-3">
-                    <div className="h-32 w-24 flex-none">
-                      <img className="block h-32 w-24" src={member.idPhoto.path} alt="証明写真" />
-                    </div>
-                    <div className="flex-1 bg-white/95 bg-[url('/takaneko/emblem.png')] bg-contain bg-center bg-no-repeat bg-blend-lighten">
-                      <dl className="grid grid-cols-3 items-end gap-2 py-1">
-                        <dt className="col-span-1 text-xs leading-none">学籍番号</dt>
-                        <dd className="col-span-2 text-xs leading-none">
-                          20220807{member.number.toString().padStart(2, "0")}
-                        </dd>
-                        <dt className="col-span-1 text-xs leading-none">学科</dt>
-                        <dd className="col-span-2 text-xs leading-none">{member.fanName}</dd>
-                        <dt className="col-span-1 text-xs leading-none">氏名</dt>
-                        <dd className="col-span-2 text-lg leading-none font-semibold">
-                          {member.name}
-                        </dd>
-                        <dt className="col-span-1 text-xs leading-none"></dt>
-                        <dd className="col-span-2 text-xs leading-none">{member.nickname}</dd>
-                        <dt className="col-span-1 text-xs leading-none">生年月日</dt>
-                        <dd className="col-span-2 text-xs leading-none">{member.birthday}</dd>
-                        <dt className="col-span-1 text-xs leading-none">血液型</dt>
-                        <dd className="col-span-2 text-xs leading-none">{member.bloodType}</dd>
-                      </dl>
-                    </div>
-                  </div>
-                </Link>
+                <MemberIdCard member={member} />
               </li>
             ))}
           </ul>
