6793193e8106d840b2c7abb2d4b4e1ab5c652417
Keisuke Ijuin <kei10in@gmail.com>
2026-06-05 22:31:34 +0900

    refactor: メンバー一覧ページを簡素化

--- app/routes/_site.members._index/route.tsx
+++ app/routes/_site.members._index/route.tsx
@@ -1,15 +1,12 @@
-import { BsDiscord, BsInstagram, BsTwitterX, BsYoutube } from "react-icons/bs";
-import { FaTiktok } from "react-icons/fa6";
-import { SiBereal, SiBilibili, SiSinaweibo, SiTiktok, SiXiaohongshu } from "react-icons/si";
-import { Link, MetaFunction } from "react-router";
-import { pageBox, pageHeading, sectionHeading } from "~/components/styles";
+import { MetaFunction } from "react-router";
+import { pageBox } from "~/components/styles";
 import { formatTitle } from "~/utils/htmlHeader";
 import { CurrentMembers, FormerMembers } from "../../features/profile/members";
 import { MemberIdCard } from "./MemberIdCard";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: formatTitle(`プロフィール`) },
+    { title: formatTitle(`メンバー`) },
     {
       name: "description",
       content: "高嶺のなでしこ メンバー一覧",
@@ -17,172 +14,22 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-const SocialMedia = [
-  {
-    url: "https://www.youtube.com/@official6743",
-    icon: BsYoutube,
-  },
-  {
-    url: "https://x.com/takanenofficial",
-    icon: BsTwitterX,
-  },
-  {
-    url: "https://www.instagram.com/takanenofficial/",
-    icon: BsInstagram,
-  },
-  {
-    url: "https://www.tiktok.com/@takanenofficial",
-    icon: FaTiktok,
-  },
-  {
-    url: "https://discord.gg/JE54h9trxm",
-    icon: BsDiscord,
-  },
-  {
-    url: "https://bere.al/takanenofficial",
-    icon: SiBereal,
-  },
-];
-
-const ChineseSocialMedia = [
-  {
-    url: "https://space.bilibili.com/3493257990375590",
-    icon: SiBilibili,
-  },
-  {
-    url: "https://weibo.com/u/7953892369",
-    icon: SiSinaweibo,
-  },
-  {
-    url: "https://www.xiaohongshu.com/user/profile/670366fd000000001d033fdf",
-    icon: SiXiaohongshu,
-  },
-  {
-    url: "https://v.douyin.com/iACQBTWT/",
-    icon: SiTiktok,
-  },
-];
-
 export default function Index() {
   return (
-    <div className="container mx-auto lg:max-w-5xl">
-      <section className={pageBox("px-4")}>
-        <h1 className={pageHeading()}>プロフィール</h1>
-        <section>
-          <h2 className={sectionHeading("my-4")}>メンバー</h2>
+    <div className="container mx-auto my-8 lg:max-w-5xl">
+      <section className={pageBox("space-y-16 px-4")}>
+        <h1 className="text-5xl">メンバー</h1>
 
-          <ul className="flex flex-wrap justify-center gap-4">
-            {CurrentMembers.map((member) => (
-              <li key={member.name} className="flex-none">
-                <MemberIdCard member={member} />
-              </li>
-            ))}
-          </ul>
-        </section>
-
-        <section>
-          <h2 className={sectionHeading("mt-12 mb-4")}>マネージャー</h2>
-
-          <ul className="flex flex-wrap justify-center gap-4">
-            <li className="flex-none">
-              <Link
-                className="flex h-44 w-80 flex-none flex-col overflow-hidden rounded-xl bg-white shadow-md"
-                to={`/members/manager`}
-              >
-                <div className="flex h-6 flex-none items-center justify-center bg-zinc-700 font-serif text-white">
-                  高嶺のなでしこ
-                </div>
-                <div className="flex flex-1 items-stretch justify-stretch gap-3 p-3">
-                  <div className="h-32 w-24 flex-none bg-gray-200">
-                    <img
-                      className="mx-auto block h-32 w-24 object-contain"
-                      src="/takaneko/tennya.png"
-                      alt="てんにゃ"
-                    />
-                  </div>
-                  <div className="flex-1 bg-white/95 bg-[url('/takaneko/emblem.png')] bg-contain bg-center bg-no-repeat bg-blend-lighten">
-                    <p>たかねこまねーじゃー</p>
-                    <ul className="mt-2 flex gap-2 text-nadeshiko-700"></ul>
-                  </div>
-                </div>
-              </Link>
+        <ul className="flex flex-wrap justify-center gap-4">
+          {CurrentMembers.map((member) => (
+            <li key={member.name} className="flex-none">
+              <MemberIdCard member={member} />
             </li>
-          </ul>
-        </section>
+          ))}
+        </ul>
 
-        <section>
-          <h2 className={sectionHeading("mt-12 mb-4")}>高嶺のなでしこ</h2>
-
-          <ul className="list-disc space-y-2 pl-6 marker:text-gray-300">
-            <li>
-              <Link
-                className="text-nadeshiko-700"
-                to="https://takanenonadeshiko.jp/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                オフィシャル ウェブサイト
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-nadeshiko-700"
-                to="https://takanekofc.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                オフィシャル ファン クラブ
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="text-nadeshiko-700"
-                to="https://takanenonadeshiko-ec.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                オフィシャル ショップ
-              </Link>
-            </li>
-          </ul>
-
-          <ul className="my-4 flex gap-2 px-2 text-gray-700">
-            {SocialMedia.map((media) => (
-              <li key={media.url}>
-                <Link
-                  className="flex h-5 w-5 items-center justify-center rounded"
-                  to={media.url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <media.icon className="h-full w-full" />
-                </Link>
-              </li>
-            ))}
-          </ul>
-
-          <section>
-            <h3 className="mt-8 mb-4 text-xl font-semibold text-gray-400">中国 SNS</h3>
-
-            <ul className="my-4 flex gap-2 px-2 text-gray-700">
-              {ChineseSocialMedia.map((media) => (
-                <li key={media.url}>
-                  <Link
-                    className="block h-5 w-5 rounded"
-                    to={media.url}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <media.icon className="h-full w-full" />
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </section>
-        </section>
-
-        <section className="mt-12">
-          <h2 className={sectionHeading("my-4")}>元メンバー</h2>
+        <section className="mt-24 space-y-8">
+          <h2 className="text-3xl">元メンバー</h2>
 
           <ul className="flex flex-wrap justify-center gap-4">
             {FormerMembers.map((member) => (
