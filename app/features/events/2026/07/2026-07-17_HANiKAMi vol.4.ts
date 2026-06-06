3f426f9706657ff8030cff0adf575b4b5a0bc01f
Keisuke Ijuin <kei10in@gmail.com>
2026-06-06 18:44:05 +0900

    イベントを追加。

---
+++ app/features/events/2026/07/2026-07-17_HANiKAMi vol.4.ts
@@ -0,0 +1,44 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "HANiKAMi vol.4",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-07-17",
+  open: "17:15",
+  start: "18:00",
+  end: undefined,
+  region: "東京",
+  location: "Zepp Shinjuku",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-17_HANiKAMi vol.4.jpg",
+      ref: "https://x.com/takanenofficial/status/2062831795289329914",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://w.pia.jp/t/hanikami4/",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-06-06",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2062831795289329914)
+`;
