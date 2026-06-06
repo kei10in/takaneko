b264204cef6c4f863fa7c9e9fb98f518513190bd
Keisuke Ijuin <kei10in@gmail.com>
2026-06-06 18:53:37 +0900

    ラジオ出演を追加。

---
+++ app/features/events/2026/06/2026-06-06_FM NACK5「えびしゃの「ばくばく」」.ts
@@ -0,0 +1,25 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "FM NACK5「えびしゃの「ばくばく」」",
+  category: "RADIO",
+  date: "2026-06-06",
+  start: "26:00",
+  end: "26:30",
+  region: "ラジオ",
+  present: ["星谷美来", "籾山ひめり"],
+  link: {
+    text: "",
+    url: "",
+  },
+  updatedAt: "2026-06-06",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [radiko 番組放送回](https://radiko.jp/share/?sid=NACK5&t=20260607020000)
+  - [メッセージフォーム](https://www.nack5.co.jp/message/5547/)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2062756297787769322)
+  - [告知 - えびしゃの「ばくばく」 X](https://x.com/bakubaku_795/status/2062812110510567667)
+`;
