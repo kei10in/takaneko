485dfe8290eede61e566806c4dce9f3fa8f1ab22
Keisuke Ijuin <kei10in@gmail.com>
2026-06-06 19:44:27 +0900

    イベントを更新

--- app/features/events/2026/06/2026-06-13_松本ももなちゃん× ROJITA チェキ会.ts
+++ app/features/events/2026/06/2026-06-13_松本ももなちゃん× ROJITA チェキ会.ts
@@ -6,12 +6,41 @@ export const meta: EventMetaDescriptor = {
   liveType: undefined,
   date: "2026-06-13",
   open: undefined,
-  start: "15:00",
+  start: "13:30",
   end: "17:30",
   region: "東京",
   location: "渋谷 109",
   present: ["高嶺のなでしこ2"],
   images: [
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_更新情報1.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2062785428500181078",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_更新情報2.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2062785917690257795",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_更新情報3.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2062785917690257795",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_更新情報4.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2062786056001626257",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_更新情報5.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2062786056001626257",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_更新情報6.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2062786056001626257",
+    },
+    {
+      path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会_更新情報7.jpg",
+      ref: "https://x.com/ROJITA__jp/status/2062786056001626257",
+    },
+
     {
       path: "/events/2026/2026-06-13_松本ももなちゃん× ROJITA チェキ会.jpg",
       ref: "https://x.com/ROJITA__jp/status/2060315768122999090",
@@ -48,24 +77,63 @@ export const meta: EventMetaDescriptor = {
     sessions: [
       {
         title: "1 部",
-        start: "15:00",
-        end: "16:00",
+        start: "13:30",
+        end: "14:30",
         lanes: ["松本ももな"],
+        costume: {
+          name: "私服",
+          label: "ROJITA ボウタイリボンショートブラウス & トリミングレースティアードスカート",
+        },
       },
       {
         title: "2 部",
+        start: "15:00",
+        end: "16:00",
+        lanes: ["松本ももな"],
+        costume: {
+          name: "私服",
+          label: "ROJITA デコルテリボン裾ティアードワンピース グレー",
+        },
+      },
+      {
+        title: "3 部",
         start: "16:30",
         end: "17:30",
         lanes: ["松本ももな"],
+        costume: {
+          name: "私服",
+          label: "ROJITA ビッグリボンワンピース",
+        },
       },
     ],
   },
-  updatedAt: "2026-05-29",
+  updatedAt: "2026-06-06",
 };
 
 export const content = /* md */ `
+  ## お買い物時間予約枠
+
+  [予約ページ](https://rlab-store.jp/c/event)
+
+  ## 衣装詳細
+
+  1 部
+
+  - [ROJITA ボウタイリボンショートブラウス](https://rlab-store.jp/c/new-arrival/1621456)
+  - [ROJITA トリミングレースティアードスカート](https://rlab-store.jp/c/new-arrival/1624457)
+
+  2 部
+
+  - [ROJITA デコルテリボン裾ティアードワンピース](https://rlab-store.jp/c/new-arrival/162347)
+
+  3 部
+
+  - 商品不明
+
   ## リンク
 
+  - [追加枠・お買い物時間予約券 告知 - ROJITA X](https://x.com/ROJITA__jp/status/2062785428500181078)
+  - [追加枠・お買い物時間予約券 告知 - 公式 X](https://x.com/takanenofficial/status/2062814180756037888)
   - [告知 - ROJITA X](https://x.com/ROJITA__jp/status/2060315768122999090)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2060317729698332855)
 `;
