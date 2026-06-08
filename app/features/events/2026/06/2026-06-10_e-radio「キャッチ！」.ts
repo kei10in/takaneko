e281768a06ae7f5e85a96addbbf485b17686bc3b
Keisuke Ijuin <kei10in@gmail.com>
2026-06-08 22:55:08 +0900

    feat: e-radio「キャッチ！」出演情報を追加

---
+++ app/features/events/2026/06/2026-06-10_e-radio「キャッチ！」.ts
@@ -0,0 +1,29 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "e-radio「キャッチ！」",
+  category: "RADIO",
+  date: "2026-06-10",
+  start: "14:00",
+  end: "18:00",
+  region: "ラジオ",
+  present: ["城月菜央", "日向端ひな", "籾山ひめり"],
+  link: {
+    text: "放送局サイト",
+    url: "https://www.e-radio.co.jp/",
+  },
+  updatedAt: "2026-06-08",
+};
+
+export const content = /* md */ `
+  ## 内容
+
+  番組放送枠 14:00 〜 18:00
+
+  コメント出演
+
+  ## リンク
+
+  - [radiko 番組放送回](https://radiko.jp/share/?sid=E-RADIO&t=20260610140000)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2063918959083262315)
+`;
