555cc572fb7809884857b39fc9bdc42d5bf7c7cb
Keisuke Ijuin <kei10in@gmail.com>
2026-06-09 21:57:11 +0900

    イベントを更新

--- app/features/events/2026/06/2026-06-09_ASIA CULTURE FESTIVAL2026.ts
+++ app/features/events/2026/06/2026-06-09_ASIA CULTURE FESTIVAL2026.ts
@@ -16,6 +16,10 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-06-09_ASIA CULTURE FESTIVAL2026.jpg",
       ref: "https://x.com/ACF2026/status/2044946740453986442",
     },
+    {
+      path: "/events/2026/2026-06-09_ASIA CULTURE FESTIVAL2026_1.jpg",
+      ref: "https://x.com/takanenofficial/status/2064165588306366698",
+    },
   ],
   link: {
     text: "イベント公式サイト",
@@ -31,8 +35,15 @@ export const meta: EventMetaDescriptor = {
   acts: {
     start: "19:15",
     end: "19:35",
-    setlist: [],
-    url: "",
+    setlist: [
+      "衣装: 見上げるたびに、恋をする。衣装",
+      "可愛くてごめん",
+      "決戦スピリット",
+      "MC",
+      "ファンサ",
+      "美しく生きろ",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2064310285998108833",
   },
   updatedAt: "2026-06-09",
 };
@@ -40,5 +51,8 @@ export const meta: EventMetaDescriptor = {
 export const content = /* md */ `
   ## リンク
 
+  - [ダイジェスト動画 - 公式 X](https://x.com/takanenofficial/status/2064313841748721838)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2064313499724255539)
+  - [きょうのたかねこ](https://x.com/takanenofficial/status/2064165588306366698)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2048741452470251800)
 `;
