5e37f340b178c6ddc97a13b80cb3f13ec4e66151
Keisuke Ijuin <kei10in@gmail.com>
2026-06-11 23:44:34 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-20_OKINAWA COLLECTION 2026.ts
@@ -0,0 +1,44 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "OKINAWA COLLECTION 2026",
+  category: "OTHER",
+  liveType: undefined,
+  date: "2026-06-20",
+  open: "13:00",
+  start: "14:30",
+  end: undefined,
+  region: "沖縄",
+  location: "沖縄サントリーアリーナ",
+  present: ["涼海すう"],
+  images: [
+    {
+      path: "/events/2026/2026-06-20_OKINAWA COLLECTION 2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2064921960455565380",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://okinawa-collection.jp/",
+  },
+  ticket: "https://okinawa-collection.jp/26_spring/ticket/",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      setlist: [],
+      url: "",
+    },
+  ],
+  updatedAt: "2026-06-11",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2064921960455565380)
+`;
