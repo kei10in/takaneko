5eb8f6da3a8095ac9c99bae34fc6fe32dd852cbd
Keisuke Ijuin <kei10in@gmail.com>
2026-06-11 23:15:01 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-23_メ〜テレ MUSIC DAYS.ts
@@ -0,0 +1,43 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "メ〜テレ MUSIC DAYS",
+  category: "LIVE",
+  liveType: "GUEST",
+  date: "2026-08-23",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "愛知",
+  location: "東別院テラスホール",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-23_メ〜テレ MUSIC DAYS.jpg",
+      ref: "https://x.com/event_nagoyatv/status/2064950762430459971",
+    },
+  ],
+  link: {
+    text: "メ〜テレ イベント情報",
+    url: "https://www.nagoyatv.com/event/nagoyatv_musicdays.html",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-06-11",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2064915525483086231)
+  - [告知 - メ〜チケ X](https://x.com/event_nagoyatv/status/2064950762430459971)
+`;
