2e43bd49207a3f92218ad954f9ff4837daf78fc2
Keisuke Ijuin <kei10in@gmail.com>
2026-06-14 22:50:23 +0900

    テレビ出演を追加

---
+++ app/features/events/2026/06/2026-06-20_TOKYO MX「音ボケPOPS」.ts
@@ -0,0 +1,38 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "TOKYO MX「音ボケPOPS」",
+  category: "TV",
+  date: "2026-06-20",
+  start: "21:30",
+  end: "22:00",
+  region: "テレビ",
+  present: ["城月菜央", "日向端ひな", "籾山ひめり"],
+  images: [
+    {
+      path: "/events/2026/2026-06-20_TOKYO MX「音ボケPOPS」.jpg",
+      ref: "https://x.com/takanenofficial/status/2065781235507696056",
+    },
+  ],
+  link: {
+    text: "番組サイト",
+    url: "https://s.mxtv.jp/music/otobokepops/",
+  },
+  updatedAt: "2026-06-14",
+};
+
+export const content = /* md */ `
+  ## 番組詳細
+
+  > 今週は、三田友梨佳、大地洋輔（ダイノジ）、橘ゆりかがゲストに旬なアーティストをお招きし、それぞれの音楽性や人間性を深掘りしていきます！
+  >
+  > ゲストには、 高嶺のなでしこより、城月菜央、日向端ひな、籾山ひめりが登場！<br/>
+  > 新曲「生きてりゃいい」を配信リリース！「生きてりゃいい」という言葉には、罪悪感なく自分を肯定できる感覚が込められており、頑張りすぎなくていい、そして、自分を許していい。そんなメッセージをまっすぐに届ける楽曲になっています。
+  >
+  > OTOBOKE POPS PICKには、D.O.L、なにわ男子をPICK UP予定！<br/>
+  > 乞うご期待！！
+
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2065781235507696056)
+`;
