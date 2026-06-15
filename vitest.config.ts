8d0fc0ac860d531bb0d948d11ab6a676fdca52e7
Keisuke Ijuin <kei10in@gmail.com>
2026-06-15 22:00:27 +0900

    chore: Vite 8 向けに設定を更新

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,15 @@
 import mdx from "@mdx-js/rollup";
 import gfm from "remark-gfm";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     mdx({
       remarkPlugins: [gfm],
     }),
-    tsconfigPaths(),
   ],
   test: {
     environment: "happy-dom",
