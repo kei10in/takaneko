7279903eadf9ad30dcfe1ef84cf5334d984464a5
Keisuke Ijuin <kei10in@gmail.com>
2026-06-19 22:47:27 +0900

    テレビ出演を更新

---
+++ app/features/events/2026/06/2026-06-16_山口朝日放送「YOU!どきっ」.ts
@@ -0,0 +1,36 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "山口朝日放送「YOU!どきっ」",
+  category: "TV",
+  date: "2026-06-16",
+  open: undefined,
+  start: "15:55",
+  end: "16:48",
+  region: "テレビ",
+  location: "",
+  present: ["橋本桃呼"],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "番組サイト",
+    url: "https://www.yab.co.jp/youdoki",
+  },
+  ticket: "",
+  streamings: undefined,
+  acts: {
+    url: "",
+  },
+  updatedAt: "2026-06-19",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 橋本桃呼 X](https://x.com/MomokoHashimoto/status/2066523691283030226)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2066755193824571577)
+`;
