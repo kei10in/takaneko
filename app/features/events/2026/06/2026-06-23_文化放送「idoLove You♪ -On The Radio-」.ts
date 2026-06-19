bb82e877aba0d0e03fda576d0a736a2cc9bd02b5
Keisuke Ijuin <kei10in@gmail.com>
2026-06-19 22:09:14 +0900

    ラジオ出演を追加。

---
+++ app/features/events/2026/06/2026-06-23_文化放送「idoLove You♪ -On The Radio-」.ts
@@ -0,0 +1,34 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "文化放送「idoLove You♪ -On The Radio-」",
+  category: "RADIO",
+  date: "2026-06-23",
+  start: "28:30",
+  end: "28:45",
+  region: "ラジオ",
+  present: ["涼海すう"],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "番組サイト",
+    url: "https://www.joqr.co.jp/qr/program/ily/",
+  },
+  updatedAt: "2026-06-19",
+};
+
+export const content = /* md */ `
+  ## 内容
+
+  涼海すうがゲスト出演。
+
+  ## リンク
+
+  - [radiko 番組放送回](https://radiko.jp/share/?sid=QRR&t=20260624043000)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2067121395885347044)
+  - [告知 - idoLove You♪ -On The Radio- X](https://x.com/ily_joqr/status/2067079797751042360)
+`;
