27d8f1bb10c79c3dec2305bd0a0fb63b3bcc9374
Keisuke Ijuin <kei10in@gmail.com>
2026-06-20 14:15:15 +0900

    feat: Act にステータスを追加

--- app/features/events/act.ts
+++ app/features/events/act.ts
@@ -11,10 +11,19 @@ export const ActTypeEnum = z.enum(["LIVE", "MEET_AND_GREET", "RUNWAY", "OTHER"])
 export const ActType = ActTypeEnum.enum;
 export type ActType = z.infer<typeof ActTypeEnum>;
 
+const ActStatus = z.union([
+  z.literal("RESCHEDULED"),
+  z.literal("CANCELED"),
+  z.literal("WITHDRAWN"),
+]);
+
+export type ActStatus = z.infer<typeof ActStatus>;
+
 export const Act = z
   .object({
     title: z.string().optional(),
     types: z.array(ActTypeEnum).default([]),
+    status: ActStatus.optional(),
 
     open: z.string().optional(),
     start: z.string().optional(),
