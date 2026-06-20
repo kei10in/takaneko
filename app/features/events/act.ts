30b78f8645960d90edcf08bbb82aca68811c2c3b
Keisuke Ijuin <kei10in@gmail.com>
2026-06-20 12:49:08 +0900

    feat: Act の種別を追加

--- app/features/events/act.ts
+++ app/features/events/act.ts
@@ -6,9 +6,15 @@ import { MemberIdEnum } from "../profile/types";
 import { parseSetlist } from "./setlist";
 import { MeetAngGreetLanesList } from "./timeSchedule";
 
+export const ActTypeEnum = z.enum(["LIVE", "MEET_AND_GREET", "RUNWAY", "OTHER"]);
+
+export const ActType = ActTypeEnum.enum;
+export type ActType = z.infer<typeof ActTypeEnum>;
+
 export const Act = z
   .object({
     title: z.string().optional(),
+    types: z.array(ActTypeEnum).default([]),
 
     open: z.string().optional(),
     start: z.string().optional(),
