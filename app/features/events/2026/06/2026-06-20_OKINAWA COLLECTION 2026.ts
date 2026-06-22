9a19be845eb7064919e0f2bd5ab8419550cb38ee
Keisuke Ijuin <kei10in@gmail.com>
2026-06-22 21:57:02 +0900

    イベントを更新

--- app/features/events/2026/06/2026-06-20_OKINAWA COLLECTION 2026.ts
+++ app/features/events/2026/06/2026-06-20_OKINAWA COLLECTION 2026.ts
@@ -35,13 +35,21 @@ export const meta: EventMetaDescriptor = {
     lineup: undefined,
     url: undefined,
   },
-  acts: [],
+  acts: [
+    {
+      title: "UP-T ステージ",
+      types: ["RUNWAY"],
+      description: "涼海すうがランウェイに出演",
+    },
+  ],
   updatedAt: "2026-06-19",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [あいらいふ幼稚園 (動画に涼海すうが出演 8:27 〜) - iLIFE! X](https://x.com/iLiFE_official/status/2068668010739745266)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2068307986435744188)
   - [配信告知 - OKINAWA COLLECTION X](https://x.com/okicolle/status/2067849864881250636)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2064921960455565380)
 `;
