ea2ca0470e222dabc6e5d30a396fd25d938f635f
Keisuke Ijuin <kei10in@gmail.com>
2026-06-22 21:28:18 +0900

    イベントを更新

--- app/features/events/2026/06/2026-06-21_KASSEN2026.ts
+++ app/features/events/2026/06/2026-06-21_KASSEN2026.ts
@@ -22,6 +22,14 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2066120969584845221",
       tags: ["timetable"],
     },
+    {
+      path: "/takaneko/goods/2026/2026-06-03_ミニフォトカード「花柄ワンピース」.jpg",
+      ref: "https://x.com/takanenofficial/status/2065237651305931022",
+    },
+    {
+      path: "/events/2026/2026-06-21_KASSEN2026_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2068603210265440365",
+    },
   ],
   link: {
     text: "イベント サイト",
@@ -31,22 +39,33 @@ export const meta: EventMetaDescriptor = {
   streamings: undefined,
   goods: {
     time: ["16:55", "18:25"],
-    lineup: undefined,
-    url: undefined,
+    lineup: ["ミニフォトカード「花柄ワンピース」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2068531894564901288",
   },
   acts: {
     types: ["LIVE"],
     start: "16:20",
     end: "16:45",
-    setlist: [],
-    url: "",
+    setlist: [
+      "衣装:2025 春衣装",
+      "可愛くてごめん",
+      "女の子は強い",
+      "MC",
+      "ハートブーケ",
+      "ファンサ",
+      "美しく生きろ",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2068616548735230391",
   },
-  updatedAt: "2026-06-14",
+  updatedAt: "2026-06-22",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2068622604953702425)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2068603210265440365)
+  - [きょうのたかねこ](https://x.com/takanenofficial/status/2068531894564901288)
   - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2066120969584845221)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2057680352722686370)
 `;
