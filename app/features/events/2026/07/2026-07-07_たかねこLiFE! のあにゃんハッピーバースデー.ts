13f0206e786a126fa16d603b7e9775147564e6fa
Keisuke Ijuin <kei10in@gmail.com>
2026-06-24 23:25:50 +0900

    イベントを追加。

---
+++ app/features/events/2026/07/2026-07-07_たかねこLiFE! のあにゃんハッピーバースデー.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "たかねこLiFE! のあにゃんハッピーバースデー",
+  category: "LIVE",
+  liveType: "JOINT",
+  date: "2026-07-07",
+  open: "17:00",
+  start: "18:00",
+  end: undefined,
+  region: "東京",
+  location: "品川ステラボール",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-07_たかねこLiFE! のあにゃんハッピーバースデー.jpg",
+      ref: "https://x.com/takanenofficial/status/2069752488358563974",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://ticketdive.com/event/TakaNekoLiFE0707",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-06-24",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2069752488358563974)
+`;
