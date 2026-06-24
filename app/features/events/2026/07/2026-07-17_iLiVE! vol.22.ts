345dcceab4e729b436afed938766449b6711378b
Keisuke Ijuin <kei10in@gmail.com>
2026-06-24 23:16:32 +0900

    イベントを追加。

---
+++ app/features/events/2026/07/2026-07-17_iLiVE! vol.22.ts
@@ -0,0 +1,46 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "iLiVE! vol.22",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-07-17",
+  open: "10:00",
+  start: "10:40",
+  end: undefined,
+  region: "東京",
+  location: "Kanadevia Hall",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-17_iLiVE! vol.22.jpg",
+      ref: "https://x.com/takanenofficial/status/2069398405798060135",
+      tags: ["timetable"],
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://t-dv.com/ilive-vol22",
+  streamings: undefined,
+  goods: {
+    time: ["14:20", "15:20"],
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    types: ["LIVE"],
+    start: "13:40",
+    end: "14:10",
+    setlist: [],
+    url: "",
+  },
+  updatedAt: "2026-06-24",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2069398405798060135)
+`;
