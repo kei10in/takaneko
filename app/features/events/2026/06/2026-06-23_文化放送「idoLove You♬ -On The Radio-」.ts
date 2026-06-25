54d772fe5e6ad83bd29fa1ebbc990569c0d61863
Keisuke Ijuin <kei10in@gmail.com>
2026-06-25 21:58:03 +0900

    ラジオ出演を更新

--- app/features/events/2026/06/2026-06-23_文化放送「idoLove You♬ -On The Radio-」.ts
+++ app/features/events/2026/06/2026-06-23_文化放送「idoLove You♬ -On The Radio-」.ts
@@ -10,15 +10,19 @@ export const meta: EventMetaDescriptor = {
   present: ["涼海すう"],
   images: [
     {
-      path: "",
-      ref: "",
+      path: "/events/2026/2026-06-23_文化放送「idoLove You♬ -On The Radio-」.jpg",
+      ref: "https://x.com/takanenofficial/status/2069357146031190081",
+    },
+    {
+      path: "/events/2026/2026-06-23_文化放送「idoLove You♬ -On The Radio-」_1.jpg",
+      ref: "https://x.com/ily_joqr/status/2069359816380698696",
     },
   ],
   link: {
     text: "番組サイト",
     url: "https://www.joqr.co.jp/qr/program/ily/",
   },
-  updatedAt: "2026-06-19",
+  updatedAt: "2026-06-23",
 };
 
 export const content = /* md */ `
@@ -29,6 +33,8 @@ export const content = /* md */ `
   ## リンク
 
   - [radiko 番組放送回](https://radiko.jp/share/?sid=QRR&t=20260624043000)
+  - [前日告知 - idoLove You♬ -On The Radio- X](https://x.com/ily_joqr/status/2069359816380698696)
+  - [本日オンエア - 公式 X](https://x.com/takanenofficial/status/2069357146031190081)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2067121395885347044)
   - [告知 - idoLove You♬ -On The Radio- X](https://x.com/ily_joqr/status/2067079797751042360)
 `;
