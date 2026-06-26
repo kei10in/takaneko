db1b95f2c4cf614c1cba58a4b30bd037824f38da
Keisuke Ijuin <kei10in@gmail.com>
2026-06-26 22:46:27 +0900

    ライブカレゴリの表示にアイコンを追加。色調整

--- app/components/SmallBadges.tsx
+++ app/components/SmallBadges.tsx
@@ -1,5 +1,8 @@
 import { clsx } from "clsx";
-import { HiSparkles, HiUser } from "react-icons/hi2";
+import { FaCrown, FaPenNib } from "react-icons/fa6";
+import { GiCompactDisc } from "react-icons/gi";
+import { HiSparkles, HiUser, HiUsers } from "react-icons/hi2";
+import { IoDiamond } from "react-icons/io5";
 import { LiveType, liveTypeColor, liveTypeLabel } from "~/features/events/EventType";
 
 export const FirstPerformanceBadge: React.FC = () => {
@@ -29,6 +32,22 @@ interface LiveTypeBadgeProps {
 }
 
 export const LiveTypeBadge: React.FC<LiveTypeBadgeProps> = ({ liveType }: LiveTypeBadgeProps) => {
+  if (liveType === LiveType.SOLO) {
+    return <SoloConcertBadge />;
+  }
+  if (liveType === LiveType.HOSTED) {
+    return <HostedJointLiveBadge />;
+  }
+  if (liveType == LiveType.FESTIVAL || liveType == LiveType.JOINT || liveType == LiveType.GUEST) {
+    return <JointLiveBadge />;
+  }
+  if (liveType == LiveType.EVENT_LIVE) {
+    return <EventLiveBadge />;
+  }
+  if (liveType == LiveType.RELEASE_EVENT) {
+    return <ReleaseEventBadge />;
+  }
+
   return (
     <div
       className={clsx(
@@ -37,6 +56,51 @@ export const LiveTypeBadge: React.FC<LiveTypeBadgeProps> = ({ liveType }: LiveTy
       )}
     >
       {liveTypeLabel(liveType)}
+    </div>
+  );
+};
+
+export const SoloConcertBadge: React.FC = () => {
+  return (
+    <div className="box-border flex items-center justify-center gap-0.5 rounded-full border border-nadeshiko-800 bg-white px-1 text-xs">
+      <FaCrown className="text-nadeshiko-800" />
+      <span className="text-nowrap text-nadeshiko-800">ワンマン</span>
+    </div>
+  );
+};
+
+export const HostedJointLiveBadge: React.FC = () => {
+  return (
+    <div className="box-border flex items-center justify-center gap-0.5 rounded-full border border-blue-400 bg-white px-1 text-xs">
+      <FaPenNib className="text-blue-400" />
+      <span className="text-nowrap text-blue-400">主催対バン</span>
+    </div>
+  );
+};
+
+export const JointLiveBadge: React.FC = () => {
+  return (
+    <div className="box-border flex items-center justify-center gap-0.5 rounded-full border border-amber-500 bg-white px-1 text-xs">
+      <HiUsers className="text-amber-500" />
+      <span className="text-nowrap text-amber-500">対バン</span>
+    </div>
+  );
+};
+
+export const EventLiveBadge: React.FC = () => {
+  return (
+    <div className="box-border flex items-center justify-center gap-0.5 rounded-full border border-amber-500 bg-white px-1 text-xs">
+      <IoDiamond className="text-amber-500" />
+      <span className="text-nowrap text-amber-500">イベント出演</span>
+    </div>
+  );
+};
+
+export const ReleaseEventBadge: React.FC = () => {
+  return (
+    <div className="box-border flex items-center justify-center gap-0.5 rounded-full border border-violet-400 bg-white px-1 text-xs">
+      <GiCompactDisc className="text-violet-400" />
+      <span className="text-nowrap text-violet-400">リリースイベント</span>
     </div>
   );
 };
