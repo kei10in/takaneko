1985489a34022cb37480aa391157b74c22ca45b7
Keisuke Ijuin <kei10in@gmail.com>
2026-06-27 11:07:42 +0900

    バッヂの種類を追加。

--- app/components/SmallBadges.tsx
+++ app/components/SmallBadges.tsx
@@ -1,8 +1,9 @@
 import { clsx } from "clsx";
 import { FaCrown, FaPenNib } from "react-icons/fa6";
-import { GiCompactDisc } from "react-icons/gi";
-import { HiSparkles, HiUser, HiUsers } from "react-icons/hi2";
+import { GiCompactDisc, GiMicrophone } from "react-icons/gi";
+import { HiRadio, HiSparkles, HiTv, HiUser, HiUsers } from "react-icons/hi2";
 import { IoDiamond } from "react-icons/io5";
+import { IconType } from "react-icons/lib";
 import { LiveType, liveTypeColor, liveTypeLabel } from "~/features/events/EventType";
 
 export const FirstPerformanceBadge: React.FC = () => {
@@ -101,6 +102,51 @@ export const ReleaseEventBadge: React.FC = () => {
     <div className="box-border flex h-4 items-center justify-center gap-0.5 rounded-full border border-violet-400 bg-white px-1 text-xs">
       <GiCompactDisc className="text-violet-400" />
       <span className="text-nowrap text-violet-400">リリースイベント</span>
+    </div>
+  );
+};
+
+interface Props {
+  large?: boolean;
+}
+
+export const LiveBadge: React.FC<Props> = ({ large }: Props) => {
+  return <Badge icon={GiMicrophone} text="ライブ" color="nadeshiko" large={large} />;
+};
+
+export const TvAppearanceBadge: React.FC<Props> = ({ large }: Props) => {
+  return <Badge icon={HiTv} text="テレビ" color="light-blue" large={large} />;
+};
+
+export const RadioAppearanceBadge: React.FC<Props> = ({ large }: Props) => {
+  return <Badge icon={HiRadio} text="ラジオ" color="light-blue" large={large} />;
+};
+
+interface BadgeProps {
+  icon: IconType;
+  text: string;
+  color: "light-blue" | "nadeshiko";
+  large?: boolean;
+}
+
+const Badge: React.FC<BadgeProps> = (props: BadgeProps) => {
+  const { icon: Icon, text, color, large = false } = props;
+
+  const borderColor = color === "light-blue" ? "border-blue-400" : "border-nadeshiko-800";
+  const textColor = color === "light-blue" ? "text-blue-400" : "text-nadeshiko-800";
+
+  const sizeClass = large ? "h-5 text-sm px-1.5" : "h-4 text-xs px-1";
+
+  return (
+    <div
+      className={clsx(
+        "box-border inline-flex items-center justify-center gap-0.5 rounded-full border bg-white",
+        sizeClass,
+        borderColor,
+      )}
+    >
+      <Icon className={textColor} />
+      <span className={clsx("text-nowrap", textColor)}>{text}</span>
     </div>
   );
 };
