e97900304885252e61bb65638dde2e170dd040af
Keisuke Ijuin <kei10in@gmail.com>
2026-06-28 22:07:22 +0900

    誕生日を追加。

---
+++ app/features/events/2026/06/2026-06-28_橋本桃呼誕生日.ts
@@ -0,0 +1,47 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "橋本桃呼誕生日🩷",
+  category: "BIRTHDAY",
+  date: "2026-06-28",
+  region: "🎂",
+  images: [
+    {
+      path: "/events/2026/2026-06-28_橋本桃呼誕生日.jpg",
+      ref: "https://www.instagram.com/p/DaILSfLEVwh",
+    },
+    {
+      path: "/events/2026/2026-06-28_橋本桃呼誕生日_1.jpg",
+      ref: "https://www.instagram.com/p/DaILSfLEVwh",
+    },
+    {
+      path: "/takaneko/birthday-goods/2026-06-28_橋本桃呼誕生日記念グッズ.jpg",
+      ref: "https://x.com/takanenofficial/status/2071203384485425430",
+    },
+  ],
+  updatedAt: "2026-06-28",
+};
+
+export const content = /* md */ `
+  ## 誕生日写真
+
+  - [🩷橋本桃呼 X 1](https://x.com/MomokoHashimoto/status/2070885052334940198)
+  - [🩷橋本桃呼 Instagram 1](https://www.instagram.com/p/DaILSfLEVwh)
+
+  ## 公式 X
+
+  - [公式 X](https://x.com/takanenofficial/status/2070885236246987135)
+  - [誕生日記念グッズ](https://x.com/takanenofficial/status/2071203384485425430)
+
+  ## お祝いメッセージ
+
+  - [💛城月菜央](https://x.com/nao_kizuki/status/2071209027712045278)
+  - [🩵涼海すう](https://x.com/su_suzumi_/status/2071125720752124110)
+    <!-- - [🩷橋本桃呼]() -->
+  - [🤍葉月紗蘭](https://x.com/saara_hazuki/status/2070885369101595034)
+  - [🧡東山恵里沙](https://x.com/erisahigasiyama/status/2070885032684822929)
+  - [💜日向端ひな](https://x.com/hina_hinahata/status/2071085156321169737)
+  - [❤️星谷美来](https://x.com/Mikuru_hositani/status/2071195242678862072)
+  - [🎀松本ももな](https://x.com/momonamatsumoto/status/2071133151460786570)
+  - [💙籾山ひめり](https://x.com/himeri_momiyama/status/2071191143501693075)
+`;
