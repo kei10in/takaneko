0d766d00170a3bf3722b14ab96f3fc4f542d9436
Keisuke Ijuin <kei10in@gmail.com>
2026-06-28 10:14:09 +0900

    イベントを追加。

---
+++ app/features/events/2026/06/2026-06-27_SHOWROOMリレー配信.ts
@@ -0,0 +1,61 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "SHOWROOM リレー配信",
+  category: "STREAMING",
+  liveType: undefined,
+  date: "2026-06-27",
+  open: undefined,
+  start: "12:00",
+  end: "21:00",
+  region: "Web",
+  location: "",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-06-27_SHOWROOMリレー配信.jpg",
+      ref: "https://x.com/takanenofficial/status/2070389103053685063",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: {
+    text: "SHOWROOM",
+    url: "https://www.showroom-live.com/",
+  },
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-06-28",
+};
+
+export const content = /* md */ `
+  ## 内容
+
+  HERO SONIC 2026の中止に伴い、SHOWROOMにてリレー配信が行われます。
+
+  ## 出演時間
+
+  | 時間  | メンバー                                                               |
+  | ----- | ---------------------------------------------------------------------- |
+  | 12:00 | [東山恵里沙](https://x.com/takanenofficial/status/2070704980336808353) |
+  | 13:00 | [日向端ひな](https://x.com/takanenofficial/status/2070718764404920481) |
+  | 14:00 | [星谷美来](https://x.com/takanenofficial/status/2070733868320035054)   |
+  | 15:00 | [松本ももな](https://x.com/takanenofficial/status/2070748970125578314) |
+  | 16:00 | [城月菜央](https://x.com/takanenofficial/status/2070764069733151102)   |
+  | 17:00 | [葉月紗蘭](https://x.com/takanenofficial/status/2070779170901090738)   |
+  | 18:00 | [涼海すう](https://x.com/takanenofficial/status/2070794269942829496)   |
+  | 19:00 | [橋本桃呼](https://x.com/takanenofficial/status/2070809362583724247)   |
+  | 20:00 | [籾山ひめり](https://x.com/takanenofficial/status/2070824473650135484) |
+
+  ## リンク
+
+  - [終了報告 - 公式 X](https://x.com/takanenofficial/status/2070842077508743278)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2070389103053685063)
+`;
