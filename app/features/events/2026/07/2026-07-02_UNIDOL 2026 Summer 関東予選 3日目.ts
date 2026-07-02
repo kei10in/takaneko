dc4671bec0ed99c94911582b7ca43be7dc520ed6
Keisuke Ijuin <kei10in@gmail.com>
2026-07-02 22:03:29 +0900

    イベントを作成

---
+++ app/features/events/2026/07/2026-07-02_UNIDOL 2026 Summer 関東予選 3日目.ts
@@ -0,0 +1,53 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "UNIDOL 2026 Summer 関東予選 3日目",
+  title: "UNIDOL 2026 Summer supported by 17LIVE 関東予選 3日目",
+  category: "LIVE",
+  liveType: "EVENT_LIVE",
+  date: "2026-07-02",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "",
+  location: "",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-02_UNIDOL 2026 Summer 関東予選 3日目.jpg",
+      ref: "https://x.com/UNIDOL_EXCO/status/2072646758409367845",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    title: "シークレットゲスト",
+    types: ["LIVE"],
+    setlist: [
+      "衣装: Bouquet of 9 Flowers 衣装",
+      "可愛くてごめん",
+      "初恋のひと。",
+      "決戦スピリット",
+      "美しく生きろ",
+      "MC",
+    ],
+    links: ["https://x.com/4glico/status/2072651353311383893"],
+  },
+  updatedAt: "2026-07-02",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2072667450517438906)
+  - [告知 - UNIDOL 実行委員会 X](https://x.com/UNIDOL_EXCO/status/2072646758409367845)
+`;
