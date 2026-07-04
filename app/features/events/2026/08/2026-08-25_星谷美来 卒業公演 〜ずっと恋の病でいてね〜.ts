1b8f768f1e76dcba54650eae54a913b4f486337e
Keisuke Ijuin <kei10in@gmail.com>
2026-07-04 21:49:58 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-25_星谷美来 卒業公演 〜ずっと恋の病でいてね〜.ts
@@ -0,0 +1,43 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "星谷美来 卒業公演 〜ずっと恋の病でいてね❤️〜",
+  category: "LIVE",
+  liveType: "SOLO",
+  date: "2026-08-25",
+  open: "17:30",
+  start: "18:30",
+  end: undefined,
+  region: "東京",
+  location: "恵比寿ガーデンホール",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "",
+      ref: "",
+    },
+  ],
+  link: {
+    text: "イベント詳細",
+    url: "https://takanenonadeshiko.jp/?p=5400",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-04",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [詳細告知 - 公式 X](https://x.com/takanenofficial/status/2073232831061676274)
+  - [開場/開演時間変更のお知らせ - 公式 X](https://x.com/takanenofficial/status/2073260517960110299)
+  - [イベント詳細 - 公式ニュース](https://takanenonadeshiko.jp/?p=5400)
+  - [星谷美来の活動に関するご報告 - 公式ニュース](https://takanenonadeshiko.jp/?p=5385)
+  - [星谷美来の活動に関するご報告 - 公式 X](https://x.com/takanenofficial/status/2067200709196738789)
+`;
