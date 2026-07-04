56c7ccab20a9f035dcd34092779421455402db0f
Keisuke Ijuin <kei10in@gmail.com>
2026-07-04 19:23:18 +0900

    イベントを追加。

---
+++ app/features/events/2026/10/2026-10-18_HoneyWorks Presents ハニフェス 2026 〜女子校文化祭 ハロウィンパーティ〜.ts
@@ -0,0 +1,51 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ハニフェス 2026 〜女子校文化祭 ハロウィンパーティ〜",
+  title: "HoneyWorks Presents ハニフェス 2026 〜女子校文化祭 ハロウィンパーティ〜",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-10-18",
+  open: "16:30",
+  start: "17:30",
+  end: undefined,
+  region: "東京",
+  location: "Shibuya LOVEZ",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-10-18_HoneyWorks Presents ハニフェス 2026 〜女子校文化祭 ハロウィンパーティ〜.jpg",
+      ref: "https://honeyworks.jp/news/11619/",
+    },
+    {
+      path: "/events/2026/2026-10-18_HoneyWorks Presents ハニフェス 2026 〜女子校文化祭 ハロウィンパーティ〜_チケット.jpg",
+      ref: "https://honeyworks.jp/news/11619/",
+    },
+    {
+      path: "/events/2026/2026-10-18_HoneyWorks Presents ハニフェス 2026 〜女子校文化祭 ハロウィンパーティ〜_座席表.jpg",
+      ref: "https://honeyworks.jp/news/11619/",
+    },
+  ],
+  link: {
+    text: "HoneyWorks 公式ニュース",
+    url: "https://honeyworks.jp/news/11619/",
+  },
+  ticket: "https://l-tike.com/st1/hwfes2026_takanenonadeshiko",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-04",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [HoneyWorks 公式ニュース](https://honeyworks.jp/news/11619/)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2072985255406112912)
+  - [開催告知 - HoneyWorks 公式 X](https://x.com/HoneyWorks_828/status/207298369811430992)
+  - [開催告知 - ハニフェス女子校文化祭 公式 X](https://x.com/HoneyFes_828/status/2072984535978189043)
+`;
