3812318fe492ff21f514b3e14fc84763ee78fb34
Keisuke Ijuin <kei10in@gmail.com>
2026-07-04 22:52:58 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-01_TOKYO IDOL FESTIVAL 2026.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "TOKYO IDOL FESTIVAL 2026",
+  keywords: ["TIF", "TIF2026"],
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-08-01",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "東京",
+  location: "お台場・青海周辺エリア",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-31_TOKYO IDOL FESTIVAL 2026.webp",
+      ref: "https://official.idolfes.com/s/tif2026/",
+    },
+  ],
+  link: {
+    text: "イベントサイト",
+    url: "https://official.idolfes.com/s/tif2026/",
+  },
+  ticket: "https://official.idolfes.com/s/tif2026/page/ticket",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-04",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [出演日決定 - 公式 X](https://x.com/takanenofficial/status/2063834688842821651)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2041751260945379650)
+  - [出演者情報 - イベントサイト](https://official.idolfes.com/s/tif2026/page/lineup)
+`;
