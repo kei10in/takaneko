605cc0a1dbef81dacdbc0e5e8dc0509f4229f49e
Keisuke Ijuin <kei10in@gmail.com>
2026-07-05 22:49:19 +0900

    イベントを更新

--- app/features/events/2026/07/2026-07-05_超NATSUZOME 2026 Day2.ts
+++ app/features/events/2026/07/2026-07-05_超NATSUZOME 2026 Day2.ts
@@ -21,6 +21,14 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2066400821529792954",
       tags: ["timetable"],
     },
+    {
+      path: "/takaneko/goods/2026/2026-06-03_ミニフォトカード「花柄ワンピース」.jpg",
+      ref: "https://x.com/takanenofficial/status/2073391391103951033",
+    },
+    {
+      path: "/events/2026/2026-07-05_超NATSUZOME 2026 Day2_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2073653167812907229",
+    },
   ],
   link: {
     text: "超NATSUZOME 公式サイト",
@@ -42,32 +50,44 @@ export const meta: EventMetaDescriptor = {
       types: ["LIVE"],
       start: "11:50",
       end: "13:00",
-      setlist: [],
-      url: "",
+      setlist: ["衣装: 見上げるたびに、恋をする。衣装", "女の子は強い"],
+      url: "https://x.com/Mikuru_hositani/status/2073717194706542779",
     },
     {
       title: "超ステージ",
       types: ["LIVE"],
       start: "14:50",
       end: "15:10",
-      setlist: [],
-      url: "",
+      setlist: [
+        "衣装: 見上げるたびに、恋をする。衣装",
+        "初恋のこたえ。",
+        "決戦スピリット",
+        "ハートブーケ",
+        "美しく生きろ",
+      ],
+      url: "https://x.com/Mikuru_hositani/status/2073717194706542779",
     },
     {
       title: "SOMARIステージ",
       types: ["LIVE"],
       start: "16:55",
       end: "17:10",
-      setlist: [],
-      url: "",
+      setlist: ["衣装: わたし色に染まれ衣装", "花は誓いを忘れない", "恋を知った世界", "ファンサ"],
+      url: "https://x.com/Mikuru_hositani/status/2073717194706542779",
     },
   ],
-  updatedAt: "2026-06-15",
+  updatedAt: "2026-07-05",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [SOMARIステージ (動画あり) - NATSUZOME X](https://x.com/natsuzome_info/status/2073675087396630560)
+  - [超ステージ (動画あり) - NATSUZOME X](https://x.com/natsuzome_info/status/2073643803400094066)
+  - [SOMARI ステージ出演報告 - 公式 X](https://x.com/takanenofficial/status/2073704726978560350)
+  - [超ステージ出演報告 (ダイジェスト動画) - 公式 X](https://x.com/takanenofficial/status/2073672932522651833)
+  - [一曲入魂ステージ出演報告 (ダイジェスト動画) - 公式 X](https://x.com/takanenofficial/status/2073622021859344680)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2073653167812907229)
   - [#あしたのたかねこ](https://x.com/takanenofficial/status/2073391391103951033)
   - [ニコ生配信告知 - 公式 X](https://x.com/takanenofficial/status/2072523154266726713)
   - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2066400821529792954)
