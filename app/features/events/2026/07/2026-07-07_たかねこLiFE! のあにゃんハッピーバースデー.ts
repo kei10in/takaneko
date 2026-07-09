ae159c1017225cf2f6d323efcc14e7a6fcf7bcba
Keisuke Ijuin <kei10in@gmail.com>
2026-07-09 19:24:36 +0900

    イベントを更新

--- app/features/events/2026/07/2026-07-07_たかねこLiFE! のあにゃんハッピーバースデー.ts
+++ app/features/events/2026/07/2026-07-07_たかねこLiFE! のあにゃんハッピーバースデー.ts
@@ -16,6 +16,14 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-07-07_たかねこLiFE! のあにゃんハッピーバースデー.jpg",
       ref: "https://x.com/takanenofficial/status/2069752488358563974",
     },
+    {
+      path: "/takaneko/goods/2026/2026-06-03_ミニフォトカード「花柄ワンピース」.jpg",
+      ref: "https://x.com/takanenofficial/status/2074108605830197479",
+    },
+    {
+      path: "/events/2026/2026-07-07_たかねこLiFE! のあにゃんハッピーバースデー_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2074450476167536719",
+    },
   ],
   link: {
     text: "",
@@ -24,16 +32,56 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://ticketdive.com/event/TakaNekoLiFE0707",
   streamings: undefined,
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["終演後〜売り切れ次第終了"],
+    lineup: ["ミニフォトカード「花柄ワンピース」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2074108605830197479",
   },
-  acts: [],
-  updatedAt: "2026-06-24",
+  acts: [
+    {
+      title: "高嶺のなでしこ",
+      types: ["LIVE"],
+      setlist: [
+        "衣装: Bouquet of 9 Flowers 衣装",
+        "初恋のこたえ。",
+        "僕は君になれない",
+        "乙女どもよ。",
+        "初恋のひと。",
+        "MC",
+        "ハートブーケ",
+        "女の子は強い",
+        "MC",
+        "アンチファン",
+        "花は誓いを忘れない",
+        "ファンサ",
+        "美しく生きろ",
+      ],
+      url: "https://x.com/Mikuru_hositani/status/2074461481853411634",
+    },
+    {
+      title: "松本ももなちゃん×若葉のあ特別ユニット",
+      types: ["LIVE"],
+      setlist: [
+        "衣装: 全国お招きツアー衣装",
+        "ガールズルール (乃木坂46 カバー)",
+        "大嫌いなはずだった。 (HoneyWorks カバー)",
+      ],
+      links: [
+        {
+          text: "iLiFE!マネージャー X",
+          url: "https://x.com/iLiFE_STAFF/status/2074510432619078069",
+        },
+      ],
+    },
+  ],
+  updatedAt: "2026-07-09",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2074450476167536719)
+  - [#たかねこセトリ](https://x.com/Mikuru_hositani/status/2074461481853411634)
+  - [松本ももなちゃん×若葉のあ特別ユニット セットリスト](https://x.com/iLiFE_STAFF/status/2074510432619078069)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2074108605830197479)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2069752488358563974)
 `;
