d08305720fb86a92847c84710904858631794fc6
Keisuke Ijuin <kei10in@gmail.com>
2026-07-18 00:13:22 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-08_高嶺のなでしこ 4周年記念イベント.ts
@@ -0,0 +1,89 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "高嶺のなでしこ 4周年記念イベント",
+  title: "高嶺のなでしこ 4周年記念 『個別2ショット撮影会』・『個別TikTok撮影会』",
+  category: "MEET_AND_GREET",
+  liveType: undefined,
+  meetAndGreetTypes: ["撮影会", "TikTok 撮影会"],
+  date: "2026-08-08",
+  open: undefined,
+  start: "11:00",
+  end: "18:30",
+  region: "東京",
+  location: "TFTホール1000",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-08_高嶺のなでしこ 4周年記念イベント.jpg",
+      ref: "https://x.com/takanenofficial/status/2078047162576023781",
+    },
+  ],
+  link: {
+    text: "イベント詳細",
+    url: "https://takanenonadeshiko.jp/?p=5420",
+  },
+  ticket: "https://ch.chara-ani.com/entry",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  meetAndGreet: {
+    sessions: [
+      {
+        title: "1 部 個別 2 ショット撮影会",
+        start: "11:00",
+        end: "12:00",
+        lastEntry: "11:40",
+        lanes: ["涼海すう", "籾山ひめり", "葉月紗蘭", "松本ももな"],
+      },
+      {
+        title: "2 部 個別 2 ショット撮影会",
+        start: "12:10",
+        end: "13:10",
+        lastEntry: "12:50",
+        lanes: ["橋本桃呼", "星谷美来", "東山恵里沙", "日向端ひな", "城月菜央"],
+      },
+      {
+        title: "3 部 個別 TikTok 撮影会",
+        start: "13:20",
+        end: "14:20",
+        lastEntry: "14:00",
+        lanes: ["涼海すう", "籾山ひめり", "葉月紗蘭", "松本ももな"],
+      },
+      {
+        title: "4 部 個別 TikTok 撮影会",
+        start: "14:30",
+        end: "15:30",
+        lastEntry: "15:10",
+        lanes: ["橋本桃呼", "星谷美来", "東山恵里沙", "日向端ひな", "城月菜央"],
+      },
+      {
+        title: "5 部 個別 2 ショット撮影会",
+        start: "16:20",
+        end: "17:20",
+        lastEntry: "17:00",
+        lanes: ["涼海すう", "籾山ひめり", "葉月紗蘭", "松本ももな"],
+      },
+      {
+        title: "6 部 個別 2 ショット撮影会",
+        start: "17:30",
+        end: "18:30",
+        lastEntry: "18:10",
+        lanes: ["橋本桃呼", "星谷美来", "東山恵里沙", "日向端ひな", "城月菜央"],
+      },
+    ],
+  },
+  acts: [],
+  updatedAt: "2026-07-18",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [イベント詳細 - 公式ニュース](https://takanenonadeshiko.jp/?p=5420)
+  - [抽選受付 - キャラアニ・チャンス](https://ch.chara-ani.com/entry)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2078047162576023781)
+`;
