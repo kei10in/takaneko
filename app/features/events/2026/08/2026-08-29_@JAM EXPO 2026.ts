0a08d4e205e7ef7d88252ece47c98ccb1ab84969
Keisuke Ijuin <kei10in@gmail.com>
2026-07-17 22:19:00 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-29_@JAM EXPO 2026.ts
@@ -0,0 +1,41 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "@JAM EXPO 2026",
+  title: "@JAM EXPO 2026 supported by UP-T",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-08-29",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "神奈川",
+  location: "横浜アリーナ",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-29_@JAM EXPO 2026.png",
+      ref: "https://atjam.jp/expo2026",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://atjam.jp/expo2026",
+  },
+  ticket: "https://atjam.jp/expo2026/ticket",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-17",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [出演日公開 - 公式 X](https://x.com/takanenofficial/status/2078057226007408776)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2076539619949375542)
+`;
