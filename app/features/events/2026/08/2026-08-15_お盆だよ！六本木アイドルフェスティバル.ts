53188acef7429ebb59ae1d30b85bcef4eca0e6b6
Keisuke Ijuin <kei10in@gmail.com>
2026-07-17 22:29:55 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-15_お盆だよ！六本木アイドルフェスティバル.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "お盆だよ！六本木アイドルフェスティバル",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-08-15",
+  open: "13:15",
+  start: "14:00",
+  end: "20:00",
+  region: "東京",
+  location: "コカ・コーラ SUMMER FES LIVEアリーナ（六本木ヒルズアリーナ）",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-15_お盆だよ！六本木アイドルフェスティバル.jpg",
+      ref: "https://x.com/idolotakarakuji/status/2078026920772641017",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://www.tv-asahi.co.jp/summerstation/arena/special/idolfes/",
+  },
+  ticket: "https://ticket.tv-asahi.co.jp/ex/project/rif2026_obon",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-17",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [出演者発表 - 六本木アイドルフェス X](https://x.com/idolotakarakuji/status/2078026920772641017)
+`;
