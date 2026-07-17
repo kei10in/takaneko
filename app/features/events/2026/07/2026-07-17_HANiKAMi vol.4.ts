89396174ccc7dcdb257127d1a76c0d671598913c
Keisuke Ijuin <kei10in@gmail.com>
2026-07-17 21:45:06 +0900

    イベントを更新

--- app/features/events/2026/07/2026-07-17_HANiKAMi vol.4.ts
+++ app/features/events/2026/07/2026-07-17_HANiKAMi vol.4.ts
@@ -11,6 +11,7 @@ export const meta: EventMetaDescriptor = {
   region: "東京",
   location: "Zepp Shinjuku",
   present: ["高嶺のなでしこ2"],
+  absent: ["城月菜央"],
   images: [
     {
       path: "/events/2026/2026-07-17_HANiKAMi vol.4.jpg",
@@ -21,6 +22,14 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2071458656827097378",
       tags: ["timetable"],
     },
+    {
+      path: "/takaneko/goods/2026/2026-07-12_ミニフォトカード「たかねこフェス vol.6」.jpg",
+      ref: "https://x.com/takanenofficial/status/2077736254645674231",
+    },
+    {
+      path: "/events/2026/2026-07-17_HANiKAMi vol.4_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2078076374246674722",
+    },
   ],
   link: {
     text: "",
@@ -30,22 +39,25 @@ export const meta: EventMetaDescriptor = {
   streamings: undefined,
   goods: {
     time: ["20:30", "21:30"],
-    lineup: undefined,
-    url: "https://x.com/takanenofficial/status/2071458656827097378",
+    lineup: ["ミニフォトカード「浴衣2026」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2077736254645674231",
   },
   acts: {
     types: ["LIVE"],
     start: "19:45",
-    end: "20:10",
+    end: "20:15",
     setlist: [],
     url: "",
   },
-  updatedAt: "2026-07-02",
+  updatedAt: "2026-07-17",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2078076374246674722)
+  - [城月菜央欠席のお知らせ - 公式 X](https://x.com/takanenofficial/status/2077906123730858392)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2077736254645674231)
   - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2071458656827097378)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2062831795289329914)
 `;
--- app/features/events/2026/07/2026-07-17_iLiVE! vol.22.ts
+++ app/features/events/2026/07/2026-07-17_iLiVE! vol.22.ts
@@ -11,11 +11,20 @@ export const meta: EventMetaDescriptor = {
   region: "東京",
   location: "Kanadevia Hall",
   present: ["高嶺のなでしこ2"],
+  absent: ["城月菜央"],
   images: [
     {
       path: "/events/2026/2026-07-17_iLiVE! vol.22.jpg",
       ref: "https://x.com/takanenofficial/status/2069398405798060135",
       tags: ["timetable"],
+    },
+    {
+      path: "/takaneko/goods/2026/2026-07-12_ミニフォトカード「たかねこフェス vol.6」.jpg",
+      ref: "https://x.com/takanenofficial/status/2077736254645674231",
+    },
+    {
+      path: "/events/2026/2026-07-17_iLiVE! vol.22_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2077986817417400646",
     },
   ],
   link: {
@@ -26,21 +35,37 @@ export const meta: EventMetaDescriptor = {
   streamings: undefined,
   goods: {
     time: ["14:20", "15:20"],
-    lineup: undefined,
-    url: undefined,
+    lineup: ["ミニフォトカード「浴衣2026」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2077736254645674231",
   },
   acts: {
     types: ["LIVE"],
     start: "13:40",
     end: "14:10",
-    setlist: [],
-    url: "",
+    setlist: [
+      "衣装: 高嶺のなでしこ 2024 衣装",
+      "ハートブーケ",
+      "初恋のひと。",
+      "乙女どもよ。",
+      "MC",
+      "革命の女王",
+      "決戦スピリット",
+      "ファンサ",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2078004471523868994",
   },
-  updatedAt: "2026-06-24",
+  updatedAt: "2026-07-17",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [#たかねこセトリ](https://x.com/Mikuru_hositani/status/2078004471523868994)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2077998004221657482)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2077986817417400646)
+  - [出演者紹介 - iLiVE! X ](https://x.com/info_iLiVE/status/2077996661843136942)
+  - [出演者紹介 (動画あり) - iLiVE! X](https://x.com/info_iLiVE/status/2077995715373256899)
+  - [城月菜央欠席のお知らせ - 公式 X](https://x.com/takanenofficial/status/2077906123730858392)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2077736254645674231)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2069398405798060135)
 `;
