8632a14050798fd88f95bafb1aed037da9dfb0b7
Keisuke Ijuin <kei10in@gmail.com>
2026-07-17 21:59:21 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-19_キュープリ -Queen&Princess-.ts
@@ -0,0 +1,43 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "#キュープリ -Queen&Princess-",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-08-19",
+  open: "14:20",
+  start: "15:00",
+  end: undefined,
+  region: "東京",
+  location: "Spotify O-EAST",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-19_キュープリ -Queen&Princess-.jpg",
+      ref: "https://x.com/takanenofficial/status/2076631532446556458",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://t-dv.com/queen_princess",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-17",
+};
+
+export const content = /* md */ `
+  ## 概要
+
+  ヒロインズ主催女性限定ライブ
+
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2076631532446556458)
+`;
