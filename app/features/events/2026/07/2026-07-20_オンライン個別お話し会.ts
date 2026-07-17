e925c1d83969b885c9d3cac4737d970ce28006b8
Keisuke Ijuin <kei10in@gmail.com>
2026-07-17 23:36:57 +0900

    イベントを追加。

---
+++ app/features/events/2026/07/2026-07-20_オンライン個別お話し会.ts
@@ -0,0 +1,68 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "オンライン個別お話し会📢",
+  category: "MEET_AND_GREET",
+  meetAndGreetTypes: ["オンライン お話し会"],
+  date: "2026-07-20",
+  start: "13:00",
+  end: "20:30",
+  region: "Web",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-20_オンライン個別お話し会_1.png",
+      ref: "https://x.com/takanenofficial/status/2076638082498208220",
+    },
+    {
+      path: "/events/2026/2026-07-20_オンライン個別お話し会_2.png",
+      ref: "https://x.com/takanenofficial/status/2076638082498208220",
+    },
+  ],
+  link: {
+    text: "開催詳細",
+    url: "https://takanenonadeshiko.jp/?p=5415",
+  },
+  updatedAt: "2026-07-17",
+};
+
+export const content = /* md */ `
+  ## 開催情報
+
+  **販売開始:** 2026年07月14日 (火) 21:00 〜 順次
+
+  - 21:00 〜 💛城月菜央
+  - 21:10 〜 🩵涼海すう
+  - 21:20 〜 🤍葉月紗蘭
+  - 21:30 〜 🧡東山恵里沙
+  - 21:40 〜 🎀松本ももな
+  - 21:50 〜 🩷橋本桃呼
+  - 22:00 〜 💜日向端ひな
+  - 22:10 〜 ❤️星谷美来
+  - 22:20 〜 💙籾山ひめり
+
+  **開催日時:** 2026年07月20日 (月・祝)
+
+  城月菜央💛 / 涼海すう🩵 / 葉月紗蘭🤍 / 東山恵里沙🧡 / 松本ももな🎀
+
+  1. 13:00 〜 13:30
+  1. 13:35 〜 14:05
+  1. 14:10 〜 14:40
+  1. 14:50 〜 15:20
+  1. 15:25 〜 15:55
+  1. 16:00 〜 16:30
+
+  橋本桃呼🩷 / 日向端ひな💜 / 星谷美来❤️ / 籾山ひめり💙
+
+  1. 17:00 〜 17:30
+  1. 17:35 〜 18:05
+  1. 18:10 〜 18:40
+  1. 18:50 〜 19:20
+  1. 19:25 〜 19:55
+  1. 20:00 〜 20:30
+
+  ## リンク
+
+  - [公式ニュース](https://takanenonadeshiko.jp/?p=5415)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2076638082498208220)
+`;
