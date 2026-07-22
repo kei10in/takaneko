9d5794cb834298767f4c31a816b29021d475a1e9
Keisuke Ijuin <kei10in@gmail.com>
2026-07-22 22:25:48 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-11_GIANNA Plus DONNA発売記念イベント.ts
@@ -0,0 +1,70 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "GIANNA Plus DONNA発売記念イベント",
+  category: "MEET_AND_GREET",
+  meetAndGreetTypes: ["撮影会", "サイン会", "お渡し会"],
+  liveType: undefined,
+  date: "2026-08-11",
+  open: undefined,
+  start: "14:00",
+  end: "19:00",
+  region: "東京",
+  location: "HMV&BOOKS SHIBUYA 5FイベントスペースA",
+  present: ["松本ももな"],
+  images: [
+    {
+      path: "/events/2026/2026-08-11_GIANNA Plus DONNA発売記念イベント.jpg",
+      ref: "https://x.com/HmvBooksShibuya/status/2078301369401065525",
+    },
+  ],
+  link: {
+    text: "イベント詳細 - HMV&BOOKS",
+    url: "https://www.hmv.co.jp/store/event/55890/",
+  },
+  ticket: "https://l-tike.com/st1/hmvbooks-26071703",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  meetAndGreet: {
+    sessions: [
+      {
+        title: "Special Stage",
+        start: "14:00",
+        end: "15:00",
+        lanes: ["松本ももな"],
+      },
+      {
+        title: "特典会 1 部",
+        start: "16:00",
+        end: "17:00",
+        lanes: ["松本ももな"],
+      },
+      {
+        title: "特典会 2 部",
+        start: "18:00",
+        end: "19:00",
+        lanes: ["松本ももな"],
+      },
+    ],
+  },
+  updatedAt: "2026-07-18",
+};
+
+export const content = /* md */ `
+  ## 特典会内容
+
+  - 1冊券: サインなし本誌1冊お渡し
+  - 2冊券: 直筆サイン入り本誌1冊お渡し + サインなし本誌1冊
+  - 3冊券: 直筆サイン入り本誌1冊お渡し + サインなし本誌2冊 + スマホ記念撮影1枚
+
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2078370708191895980)
+  - [告知 - HMV&BOOKS SHIBUYA X](https://x.com/HmvBooksShibuya/status/2078301369401065525)
+  - [イベント詳細 - HMV&BOOKS](https://www.hmv.co.jp/store/event/55890/)
+`;
