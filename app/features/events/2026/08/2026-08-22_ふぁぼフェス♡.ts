bbc20385413a41b52f45e2755239a27cdc7a3f48
Keisuke Ijuin <kei10in@gmail.com>
2026-07-25 20:14:15 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-22_ふぁぼフェス♡.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ふぁぼフェス♡",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-08-22",
+  open: "12:30",
+  start: "13:30",
+  end: undefined,
+  region: "東京",
+  location: "Zepp Shinjuku",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-22_ふぁぼフェス♡.jpg",
+      ref: "https://x.com/_fav_me_/status/2080956221813641527",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-25",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [開催決定 - fav me X](https://x.com/_fav_me_/status/2080956221813641527)
+`;
