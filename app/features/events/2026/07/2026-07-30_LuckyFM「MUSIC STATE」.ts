35cc9fb4ba3025569b1fd702fffd3f94e4689809
Keisuke Ijuin <kei10in@gmail.com>
2026-07-25 21:53:17 +0900

    イベントを更新

---
+++ app/features/events/2026/07/2026-07-30_LuckyFM「MUSIC STATE」.ts
@@ -0,0 +1,34 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "LuckyFM「MUSIC STATE」",
+  category: "RADIO",
+  date: "2026-07-30",
+  start: "13:00",
+  end: "16:00",
+  region: "ラジオ",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-07-30_LuckyFM「MUSIC STATE」.jpg",
+      ref: "https://x.com/luckyfm_ibaraki/status/2080548536727597444",
+    },
+  ],
+  link: {
+    text: "番組ホームページ",
+    url: "https://lucky-ibaraki.com/ims/",
+  },
+  updatedAt: "2026-07-24",
+};
+
+export const content = /* md */ `
+  ## 内容
+
+  コメント出演
+
+  ## リンク
+
+  - [radiko 番組放送回](https://radiko.jp/share/?sid=IBS&t=20260730130000)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2080563719705416090)
+  - [告知 - LuckyFM X](https://x.com/luckyfm_ibaraki/status/2080548536727597444)
+`;
