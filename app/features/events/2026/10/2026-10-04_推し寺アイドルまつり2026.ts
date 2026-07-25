8dacfe5e510984d5eb54e586dae2fbebc9ef11e8
Keisuke Ijuin <kei10in@gmail.com>
2026-07-25 22:00:28 +0900

    イベントを追加

---
+++ app/features/events/2026/10/2026-10-04_推し寺アイドルまつり2026.ts
@@ -0,0 +1,40 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "推し寺アイドルまつり2026",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-10-04",
+  open: "14:00",
+  start: "15:00",
+  end: undefined,
+  region: "愛知",
+  location: "東別院テラスホール",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-10-04_推し寺アイドルまつり2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2080493358410342527",
+    },
+  ],
+  link: {
+    text: "メ〜テレ イベント情報",
+    url: "https://www.nagoyatv.com/event/oshideraidolfes2026.html",
+  },
+  ticket: "https://fan.pia.jp/nagoyatv-event/ticket/detail/154",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-25",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [メ〜テレ イベント情報](https://www.nagoyatv.com/event/oshideraidolfes2026.html)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2080493358410342527)
+`;
