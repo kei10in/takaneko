e6e2e09a1179b7e0432dc1c103cdc90a92d63f90
Keisuke Ijuin <kei10in@gmail.com>
2026-07-30 22:47:59 +0900

    イベントを更新

--- app/features/events/2026/07/2026-07-30_POPPIN' BASE in GIGA.ts
+++ app/features/events/2026/07/2026-07-30_POPPIN' BASE in GIGA.ts
@@ -30,6 +30,14 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-07-30_POPPIN BASE in GIGA.jpg",
       ref: "https://x.com/POPPINBASE/status/2060647708525019532",
     },
+    {
+      path: "/takaneko/goods/2026/2026-07-12_ミニフォトカード「たかねこフェス vol.6」.jpg",
+      ref: "https://x.com/takanenofficial/status/2082466189041897832",
+    },
+    {
+      path: "/events/2026/2026-07-30_POPPIN' BASE in GIGA_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2082753615727632631",
+    },
   ],
   link: {
     text: "イベント公式サイト",
@@ -39,22 +47,36 @@ export const meta: EventMetaDescriptor = {
   streamings: undefined,
   goods: {
     time: ["18:05", "19:35"],
-    lineup: undefined,
-    url: "https://x.com/takanenofficial/status/2073240417525702999",
+    lineup: ["ミニフォトカード「たかねこフェス vol.6」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2082466189041897832",
   },
   acts: {
     types: ["LIVE"],
     start: "17:05",
     end: "17:35",
-    setlist: [],
-    url: "",
+    setlist: [
+      "衣装: 見上げるたびに、恋をする。衣装",
+      "初恋のひと。",
+      "乙女どもよ。",
+      "決戦スピリット",
+      "MC",
+      "月曜日の憂鬱",
+      "ファンサ",
+      "美しく生きろ",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2082781650724004002",
   },
-  updatedAt: "2026-07-04",
+  updatedAt: "2026-07-30",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [#たかねこセトリ](https://x.com/Mikuru_hositani/status/2082781650724004002)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2082750700682088461)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2082753615727632631)
+  - [出演報告 - POPPIN' BASE X](https://x.com/POPPINBASE/status/2082739525621809611)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2082466189041897832)
   - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2073240417525702999)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2061319511396237428)
   - [告知 - POPPIN' BASE 公式 X](https://x.com/POPPINBASE/status/2060647708525019532)
