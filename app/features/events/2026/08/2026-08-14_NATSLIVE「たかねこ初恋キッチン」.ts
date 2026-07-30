bf6fa8d56a48f67386bf72f0594e93c23f97ea0e
Keisuke Ijuin <kei10in@gmail.com>
2026-07-31 01:12:12 +0900

    イベントを更新

---
+++ app/features/events/2026/08/2026-08-14_NATSLIVE「たかねこ初恋キッチン」.ts
@@ -0,0 +1,63 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "NATSLIVE「たかねこ初恋キッチン。#8 初恋冷しゃぶ。」",
+  category: "VARIETY",
+  liveType: undefined,
+  date: "2026-08-14",
+  open: "18:30",
+  start: "19:00",
+  end: "20:00",
+  region: "東京",
+  location: "NATSLIVE CAFE 表参道",
+  present: ["橋本桃呼", "日向端ひな", "星谷美来"],
+  images: [
+    {
+      path: "/events/2026/2026-01-23_NATSLIVE「たかねこ初恋キッチン」.jpg",
+      ref: "https://x.com/takanenofficial/status/2081986931932733485",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://fortune-cookie.natslive.jp/lotteries/agSFVy/entry_sessions/new",
+  streamings: {
+    text: "NATSLIVE",
+    url: "https://natslive.jp/episodes/18002",
+  },
+  goods: {
+    time: undefined,
+    lineup: [
+      // "レシピカード「チリコンカンナッツタコス カード A タイプ」 660 円 (税込)",
+      // "レシピカード「チリコンカンナッツタコス カード B タイプ」 660 円 (税込)",
+      // "レシピカード「チリコンカンナッツタコス カード C タイプ」 660 円 (税込)",
+    ],
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-07-31",
+};
+
+export const content = /* md */ `
+  ## 内容
+
+  生配信。ライブ観覧あり。
+
+  配信の視聴には NATSLIVE アプリのインストールが必要です。
+
+  ## アーカイブ配信
+
+  無料会員は配信後 7 日間アーカイブ視聴可能。
+
+  ゴールド会員は配信後 1 年間アーカイブ視聴可能。
+
+  ## グッズの販売期間
+
+  2026年08月14日 19:00 〜 2026年08月15日 18:59
+
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2081986931932733485)
+  - [告知 - NATSLIVE X](https://x.com/NATSLIVE_app/status/2081982991639650516)
+`;
