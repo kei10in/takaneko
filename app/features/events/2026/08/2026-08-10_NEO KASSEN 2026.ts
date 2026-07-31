fa18c59cbc42fd782dc83bda63d0360980daedcc
Keisuke Ijuin <kei10in@gmail.com>
2026-08-01 00:10:26 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-10_NEO KASSEN 2026.ts
+++ app/features/events/2026/08/2026-08-10_NEO KASSEN 2026.ts
@@ -5,8 +5,8 @@ export const meta: EventMetaDescriptor = {
   category: "LIVE",
   liveType: "FESTIVAL",
   date: "2026-08-10",
-  open: undefined,
-  start: undefined,
+  open: "10:00",
+  start: "11:30",
   end: undefined,
   region: "東京",
   location: "Spotify O-EAST",
@@ -15,6 +15,11 @@ export const meta: EventMetaDescriptor = {
     {
       path: "/events/2026/2026-08-10_NEO KASSEN 2026.jpg",
       ref: "https://x.com/NEOJAPONISMinfo/status/2043600076262838480",
+    },
+    {
+      path: "/events/2026/2026-08-10_NEO KASSEN 2026_タイムテーブル.jpg",
+      ref: "https://x.com/takanenofficial/status/2082443523471655290",
+      tags: ["timetable"],
     },
   ],
   link: {
@@ -28,13 +33,19 @@ export const meta: EventMetaDescriptor = {
     lineup: undefined,
     url: undefined,
   },
-  acts: [],
-  updatedAt: "2026-04-19",
+  acts: {
+    title: "KASSEN STAGE",
+    types: ["LIVE"],
+    start: "17:35",
+    end: "18:05",
+  },
+  updatedAt: "2026-08-01",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2082443523471655290)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2044672227694854420)
   - [告知 - NEO JAPONISM X](https://x.com/NEOJAPONISMinfo/status/2043600076262838480)
 `;
