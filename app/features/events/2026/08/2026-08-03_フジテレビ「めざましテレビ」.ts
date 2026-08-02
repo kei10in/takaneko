8bcd647d81bd15735d851316dda73447c26e3fc5
Keisuke Ijuin <kei10in@gmail.com>
2026-08-02 21:06:10 +0900

    テレビ出演を追加。

---
+++ app/features/events/2026/08/2026-08-03_フジテレビ「めざましテレビ」.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "フジテレビ「めざましテレビ」",
+  category: "TV",
+  date: "2026-08-03",
+  start: "07:20",
+  end: "07:35",
+  region: "テレビ",
+  present: ["松本ももな"],
+  images: [
+    {
+      path: "/events/2026/2026-08-03_フジテレビ「めざましテレビ」.jpg",
+      ref: "https://x.com/cx_mezamashi/status/2083840991463915924",
+    },
+  ],
+  link: {
+    text: "番組サイト",
+    url: "https://www.fujitv.co.jp/meza/",
+  },
+  updatedAt: "2026-08-02",
+};
+
+export const content = /* md */ `
+  ## 出演内容
+
+  ココ調に出演
+
+  > みんながやってる「滝汗対策」と🧊<br/>
+  > 汗で絶対に崩したくない「前髪」をココ調🔍<br/>
+  > 踊っても崩れないアイドル前髪の秘密🤫<br/>
+  > SWEET STEADY 奥田彩友さん と高嶺のなでしこ 松本ももなさんにキープ術を聞いてきたよ🎤
+
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/208386371722253024)
+  - [告知 - めざましテレビ X](https://x.com/cx_mezamashi/status/2083840991463915924)
+  - [番組サイト](https://www.fujitv.co.jp/meza/)
+`;
