97d6e96c43a11711f2a2290446213d14e4102a97
Keisuke Ijuin <kei10in@gmail.com>
2026-08-09 01:54:51 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-07_4th ファンミーティング 〜決起会〜.ts
+++ app/features/events/2026/08/2026-08-07_4th ファンミーティング 〜決起会〜.ts
@@ -13,8 +13,9 @@ export const meta: EventMetaDescriptor = {
   present: ["高嶺のなでしこ2"],
   images: [
     {
-      path: "",
-      ref: "",
+      path: "/takaneko/live-goods/2026-08-07_4th ファンミーティング 〜決起会〜_グッズメニュー.jpg",
+      ref: "https://x.com/takanenofficial/status/2084950290004918768",
+      tags: ["goods"],
     },
   ],
   link: {
@@ -22,11 +23,31 @@ export const meta: EventMetaDescriptor = {
     url: "https://takanenonadeshiko.jp/?p=5345",
   },
   ticket: "",
-  streamings: undefined,
+  streamings: {
+    text: "ニコニコ生放送",
+    url: "https://live.nicovideo.jp/watch/lv351080273",
+  },
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["12:30", "終演後30分程度"],
+    lineup: [
+      "Tシャツ (4th ファンミ ver.)",
+      "個別ペンライトチャーム (4th Anniversary ver.)",
+      "個別マフラータオル (4th Anniversary ver.)",
+      "個別トレカケース (4th Anniversary ver.)",
+      "ペンライト (4th Anniversary ver.)",
+      "個別 BIG うちわ (4th Anniversary ver.)",
+      "会場限定キラキラトレカ (4th ファンミ ver.)",
+      "個別アクリルキーホルダー (4th Anniversary ver.)",
+      "個別アクリルスタンド (4th Anniversary ver.)",
+      "ランダムキラキラ缶バッジ (4th ファンミ ver.)",
+      "メモリアルアクリルパズル (4th ファンミ ver.)",
+      "オーロラクリアマルチケース",
+      "ポータブルファン",
+      "にゃでしこマスコットキーホルダー",
+      "生写真 (4th Anniversary ver.)",
+      "ミニフォトカード (4th Anniversary ver.)",
+    ],
+    url: "https://x.com/takanenofficial/status/2084950290004918768",
   },
   acts: [
     {
@@ -46,12 +67,14 @@ export const meta: EventMetaDescriptor = {
       url: "",
     },
   ],
-  updatedAt: "2026-07-27",
+  updatedAt: "2026-08-09",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [きょうのたかねこ](https://x.com/takanenofficial/status/2085532799143145553)
+  - [GOODS MENU 公開 - 公式 X](https://x.com/takanenofficial/status/2084950290004918768)
   - [詳細告知 - 公式ニュース](https://takanenonadeshiko.jp/?p=5345)
   - [詳細告知 - 公式 X](https://x.com/takanenofficial/status/2060677853046616478)
   - [開催決定 - 公式 X](https://x.com/takanenofficial/status/2051982808751739008)
