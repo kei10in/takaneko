e60c7bdc607a3046c6d49ae4b393957e45969b16
Keisuke Ijuin <kei10in@gmail.com>
2026-08-09 00:29:18 +0900

    イベントを更新

---
+++ app/features/events/2026/08/2026-08-30_24時間テレビ 日産チャリティーイベント.ts
@@ -0,0 +1,40 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "24時間テレビ 日産チャリティーイベント",
+  category: "OTHER",
+  liveType: "EVENT_LIVE",
+  date: "2026-08-30",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "神奈川",
+  location: "日産グローバル本社ギャラリー",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-30_24時間テレビ 日産チャリティーイベント.jpg",
+      ref: "https://x.com/NissanJP/status/2085297328051880377",
+    },
+  ],
+  link: {
+    text: "イベント ホームページ",
+    url: "https://www2.nissan.co.jp/24/2026/",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-09",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2085589250070761652)
+  - [出演者情報・優先観覧エリア抽選案内 - 日産自動車 X](https://x.com/NissanJP/status/2085297328051880377)
+`;
