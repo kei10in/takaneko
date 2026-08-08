01209eb72f85037159b1c670496d55181a486d2d
Keisuke Ijuin <kei10in@gmail.com>
2026-08-09 01:21:17 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-06_4th Anniversary Special LIVE 〜STELLA〜.ts
+++ app/features/events/2026/08/2026-08-06_4th Anniversary Special LIVE 〜STELLA〜.ts
@@ -16,6 +16,11 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-08-06_4th Anniversary Special LIVE 〜STELLA〜.jpg",
       ref: "https://x.com/takanenofficial/status/2081575326501740755",
     },
+    {
+      path: "/takaneko/live-goods/2026-08-06_4th Anniversary Special LIVE 〜STELLA〜_グッズメニュー.jpg",
+      ref: "https://x.com/takanenofficial/status/2084950039655252319",
+      tags: ["goods"],
+    },
   ],
   link: {
     text: "公式ニュース",
@@ -24,17 +29,72 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://eplus.jp/takanenonadeshiko/",
   streamings: undefined,
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["14:30", "終演後30分程度"],
+    lineup: [
+      "Tシャツ (STELLA ver.)",
+      "個別ペンライトチャーム (4th Anniversary ver.)",
+      "個別マフラータオル (4th Anniversary ver.)",
+      "個別トレカケース (4th Anniversary ver.)",
+      "ペンライト (4th Anniversary ver.)",
+      "個別 BIG うちわ (4th Anniversary ver.)",
+      "会場限定キラキラトレカ (STELLA ver.)",
+      "個別アクリルキーホルダー (4th Anniversary ver.)",
+      "個別アクリルスタンド (4th Anniversary ver.)",
+      "ランダムキラキラ缶バッジ (STELLA ver.)",
+      "ラメアクリルロゴキーホルダー (STELLA ver.)",
+      "オーロラクリアマルチケース",
+      "ポータブルファン",
+      "にゃでしこマスコットキーホルダー",
+      "生写真 (4th Anniversary ver.)",
+      "ミニフォトカード (4th Anniversary ver.)",
+    ],
+    url: "https://x.com/takanenofficial/status/2084950039655252319",
   },
-  acts: [],
-  updatedAt: "2026-07-31",
+  acts: [
+    {
+      setlist: [
+        "衣装: 見上げるたびに恋をする衣装",
+        "アイドル衣装",
+        "I’M YOUR IDOL",
+        "初恋のひと。",
+        "MC",
+        "アンチファン",
+        "革命の女王",
+        "私は怪物",
+        "衣装: サマーセッション衣装",
+        "可愛いって言われたい",
+        "僕は君になれない",
+        "僕らの青",
+        "MC: オーディション当時の写真公開",
+        "恋を知った世界",
+        "女の子は強い",
+        "初恋のこたえ。",
+        "MC",
+        "約束",
+        "花は誓いを忘れない",
+        "MC",
+        "美しく生きろ",
+        "アンコール",
+        "衣装: T シャツ (STELLA ver.)",
+        "ハートブーケ",
+        "ユメムスビ",
+        "ファンサ",
+      ],
+    },
+  ],
+  updatedAt: "2026-08-09",
 };
 
 export const content = /* md */ `
+  ## ライブレポート
+
+  - [オフィシャルレポート](https://prtimes.jp/main/html/rd/p/000000949.000025517.html)
+
   ## リンク
 
+  - [ダイジェスト動画 - 公式 X](https://x.com/takanenofficial/status/2085373590812397972)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2085014211487293589)
+  - [GOODS MENU 公開 - 公式 X](https://x.com/takanenofficial/status/2084950039655252319)
   - [FC 抽選会 / セトリ予想クイズ 詳細 - 公式 FC News](https://takanekofc.com/news/public/_/p28pk0cpsk8ego0d.html)
   - [FC 抽選会 / セトリ予想クイズ お知らせ - 公式 X](https://x.com/takanenofficial/status/2082753064004436178)
   - [公演タイトル公開 - 公式 X](https://x.com/takanenofficial/status/2081575326501740755)
