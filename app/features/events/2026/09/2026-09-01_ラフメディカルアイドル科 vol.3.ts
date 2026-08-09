7d4e7ac9ddf43f26d0e495c80465a00abaa22e68
Keisuke Ijuin <kei10in@gmail.com>
2026-08-10 00:18:00 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-01_ラフメディカルアイドル科 vol.3.ts
@@ -0,0 +1,38 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ラフメディカルアイドル科 vol.3",
+  category: "OTHER",
+  date: "2026-09-01",
+  open: "18:25",
+  start: "18:45",
+  end: "20:15",
+  region: "東京",
+  location: "渋谷ユーロライブ",
+  present: ["橋本桃呼"],
+  images: [
+    {
+      path: "/events/2026/2026-09-01_ラフメディカルアイドル科 vol.3.jpg",
+      ref: "https://x.com/takanenofficial/status/2085596799817945409",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://tiget.net/events/511976",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-10",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2085596799817945409)
+`;
