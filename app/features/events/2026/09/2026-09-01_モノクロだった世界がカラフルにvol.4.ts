c33566f7f932017790955f9fe00962e11152c04d
Keisuke Ijuin <kei10in@gmail.com>
2026-08-10 00:27:11 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-01_モノクロだった世界がカラフルにvol.4.ts
@@ -0,0 +1,38 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "モノクロだった世界がカラフルにvol.4",
+  category: "OTHER",
+  date: "2026-09-01",
+  open: "20:40",
+  start: "21:00",
+  end: "22:15",
+  region: "東京",
+  location: "渋谷ユーロライブ",
+  present: ["東山恵里沙"],
+  images: [
+    {
+      path: "/events/2026/2026-09-01_モノクロだった世界がカラフルにvol.4.jpg",
+      ref: "https://x.com/takanenofficial/status/2085600255685398592",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://tiget.net/events/512009",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-10",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2085600255685398592)
+`;
