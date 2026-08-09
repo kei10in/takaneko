c9e6ce7dba11e9d98113ca2b9e251eae7190b4e6
Keisuke Ijuin <kei10in@gmail.com>
2026-08-10 08:34:22 +0900

    楽曲を更新

--- app/features/songs/2024/I’M YOUR IDOL.ts
+++ app/features/songs/2024/I’M YOUR IDOL.ts
@@ -27,6 +27,11 @@ export const IM_YOUR_IDOL: SongMetaDescriptor = {
       publishedAt: "2024-11-27",
     },
     {
+      text: "Dance Practice Video",
+      videoId: "https://www.youtube.com/watch?v=1SakX8WpzZc",
+      publishedAt: "2026-08-03",
+    },
+    {
       text: "LIVE",
       videoId: "7NK49pi5BFQ",
       publishedAt: "2024-11-30",
