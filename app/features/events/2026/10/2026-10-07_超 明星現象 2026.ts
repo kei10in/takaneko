e34feb546cdd41060ad6e74b60983a2e670a7891
Keisuke Ijuin <kei10in@gmail.com>
2026-08-10 08:44:31 +0900

    イベントを追加。

---
+++ app/features/events/2026/10/2026-10-07_超 明星現象 2026.ts
@@ -0,0 +1,44 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "超 明星現象 2026",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-10-07",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "東京",
+  location: "Shibuya LOVEZ",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-10-07_超 明星現象 2026.jpg",
+      ref: "https://x.com/cho_myojo/status/2081333861221609767",
+    },
+    {
+      path: "/events/2026/2026-10-07_超 明星現象 2026_第4弾出演者.jpg",
+      ref: "https://x.com/takanenofficial/status/2084249385421221917",
+    },
+  ],
+  link: {
+    text: "イベント公式 X",
+    url: "https://x.com/cho_myojo",
+  },
+  ticket: "https://ticketdive.com/event/chomyojo_1007",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-10",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2084249385421221917)
+  - [開催告知 - イベント X](https://x.com/cho_myojo/status/2081333861221609767)
+`;
