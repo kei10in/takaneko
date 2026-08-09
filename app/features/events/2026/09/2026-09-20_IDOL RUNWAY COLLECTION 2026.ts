01758808f856e9bc6ee3742fd10704220b808ca8
Keisuke Ijuin <kei10in@gmail.com>
2026-08-09 22:45:34 +0900

    イベントを追加

---
+++ app/features/events/2026/09/2026-09-20_IDOL RUNWAY COLLECTION 2026.ts
@@ -0,0 +1,45 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "IDOL RUNWAY COLLECTION 2026 AUTUMN/WINTER",
+  title: "第4回 IDOL RUNWAY COLLECTION 2026 AUTUMN/WINTER Supported by TGC",
+  category: "FASHION",
+  liveType: "EVENT_LIVE",
+  date: "2026-09-20",
+  open: "11:00",
+  start: "12:00",
+  end: "16:00",
+  region: "神奈川",
+  location: "横浜アリーナ",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-09-20_IDOL RUNWAY COLLECTION 2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2085554080139952378",
+    },
+    {
+      path: "/events/2026/2026-09-20_IDOL RUNWAY COLLECTION 2026_2.jpg",
+      ref: "https://x.com/takanenofficial/status/2085554080139952378",
+    },
+  ],
+  link: {
+    text: "IDOL RUNWAY COLLECTION 公式サイト",
+    url: "https://idolrunwaycollection.jp/",
+  },
+  ticket: "https://idolrunwaycollection.jp/#tickets",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-09",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2085554080139952378)
+  - [告知 - IDOL RUNWAY COLLECTION X](https://x.com/idolrunwaycolle/status/2085659717771161736)
+`;
