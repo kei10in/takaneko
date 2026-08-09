9ccf74ffebbd2b6db4ba426dee5609b6943d72f2
Keisuke Ijuin <kei10in@gmail.com>
2026-08-10 08:30:27 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-20_アイふた vol.77.ts
@@ -0,0 +1,41 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "アイふた vol.77",
+  category: "OTHER",
+  date: "2026-08-20",
+  open: "18:15",
+  start: "19:00",
+  end: undefined,
+  region: "東京",
+  location: "LOFT9 Shibuya",
+  present: ["星谷美来"],
+  images: [
+    {
+      path: "/events/2026/2026-08-20_アイふた vol.77.jpg",
+      ref: "https://x.com/takanenofficial/status/2084229037216661782",
+    },
+  ],
+  link: {
+    text: "イベント詳細",
+    url: "https://www.loft-prj.co.jp/schedule/loft9/361822",
+  },
+  ticket: "",
+  streamings: {
+    text: "ツイキャスプレミア",
+    url: "https://premier.twitcasting.tv/loft9shibuya/shopcart/447143",
+  },
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-10",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2084229037216661782)
+`;
