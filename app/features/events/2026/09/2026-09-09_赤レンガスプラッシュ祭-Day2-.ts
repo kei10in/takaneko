ce69548393565c7f189e86575f88b70a48c5bac1
Keisuke Ijuin <kei10in@gmail.com>
2026-08-10 22:48:35 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-09_赤レンガスプラッシュ祭-Day2-.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "赤レンガスプラッシュ祭-Day2-",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-09-09",
+  open: "11:00",
+  start: "11:30",
+  end: undefined,
+  region: "神奈川",
+  location: "横浜赤レンガ倉庫イベント広場B",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-09-09_赤レンガスプラッシュ祭-Day2-.jpg",
+      ref: "https://x.com/takanenofficial/status/2086784634969538610",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://idol-spot.tstar.jp/event/rig5abg/?scid=su_16699",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-10",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2086784634969538610)
+`;
