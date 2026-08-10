aebc1ea42c90e14969827931bde159bcbda7bfd7
Keisuke Ijuin <kei10in@gmail.com>
2026-08-10 22:26:32 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-10_NEO KASSEN 2026.ts
+++ app/features/events/2026/08/2026-08-10_NEO KASSEN 2026.ts
@@ -21,30 +21,57 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2082443523471655290",
       tags: ["timetable"],
     },
+    {
+      path: "/takaneko/goods/2026/2026-07-12_ミニフォトカード「たかねこフェス vol.6」.jpg",
+      ref: "https://x.com/takanenofficial/status/2077736254645674231",
+    },
+    {
+      path: "/events/2026/2026-08-10_NEO KASSEN 2026_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2086696292429041891",
+    },
   ],
   link: {
     text: "イベント公式サイト",
     url: "https://neojaponism.com/neokassen/2026",
   },
   ticket: "https://l-tike.com/concert/mevent/?mid=781806",
-  streamings: undefined,
+  streamings: {
+    text: "YouTube Live",
+    url: "https://www.youtube.com/live/4R_-uQpr_Us?si=Pak7kSC8Ac26Ms4z&t=21200",
+  },
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["16:25", "17:25"],
+    lineup: ["ミニフォトカード「たかねこフェス vol.6」/ チェキ"],
+    url: "https://x.com/takanenofficial/status/2086672644842369103",
   },
   acts: {
     title: "KASSEN STAGE",
     types: ["LIVE"],
     start: "17:35",
     end: "18:05",
+    setlist: [
+      "衣装:高嶺のなでしこ 2024 衣装",
+      "僕らの青",
+      "ハートブーケ",
+      "MC",
+      "LOVE ANTHEM",
+      "革命の女王",
+      "決戦スピリット",
+      "I’M YOUR IDOL",
+      "MC",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2086753455251824820",
   },
-  updatedAt: "2026-08-01",
+  updatedAt: "2026-08-10",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2086755882176803110)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2086696292429041891)
+  - [きょうのたかねこ](https://x.com/takanenofficial/status/2086672644842369103)
+  - [無料生配信告知 - NEO JAPONISM X](https://x.com/NEOJAPONISMinfo/status/2086059909624582500)
   - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2082443523471655290)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2044672227694854420)
   - [告知 - NEO JAPONISM X](https://x.com/NEOJAPONISMinfo/status/2043600076262838480)
