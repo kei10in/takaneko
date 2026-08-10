a54e93b1e7c435530e133ae014b7339bf451ce0c
Keisuke Ijuin <kei10in@gmail.com>
2026-08-10 22:55:57 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-16_MARQUEE Fes.-day2-.ts
@@ -0,0 +1,45 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "MARQUEE Fes.-day2-",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-09-16",
+  open: "14:00",
+  start: "14:30",
+  end: undefined,
+  region: "東京",
+  location: "Spotify O-EAST",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-09-16_MARQUEE Fes.-day2-.jpg",
+      ref: "https://x.com/MARQUEE_matsuri/status/2086286343228068342",
+      tags: ["timetable"],
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://eplus.jp/marquee-matsuri/",
+  streamings: undefined,
+  goods: {
+    time: ["20:45", "21:55"],
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    types: ["LIVE"],
+    start: "20:15",
+    end: "20:40",
+  },
+  updatedAt: "2026-08-10",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [タイムテーブル公開 - MARQUEE祭 X](https://x.com/MARQUEE_matsuri/status/2086286343228068342)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2085591768763097555)
+`;
