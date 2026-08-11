1abdf260acc8a8af0765ed7f1cfc64b172e000d9
Keisuke Ijuin <kei10in@gmail.com>
2026-08-11 22:46:19 +0900

    イベントを追加。

---
+++ app/features/events/2026/10/2026-10-06_TOKYO GIRLS GIRLS.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "TOKYO GIRLS GIRLS",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-10-06",
+  open: "11:00",
+  start: "11:20",
+  end: undefined,
+  region: "東京",
+  location: "Shibuya LOVEZ",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-10-06_TOKYO GIRLS GIRLS.jpg",
+      ref: "https://x.com/takanenofficial/status/2087157080797155564",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://ticketdive.com/event/tgg_1006",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-11",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2087157080797155564)
+`;
