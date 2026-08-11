05f9ee7835874209f3b4c20cc31094c1b4e9f913
Keisuke Ijuin <kei10in@gmail.com>
2026-08-11 23:06:51 +0900

    イベントを追加。

---
+++ app/features/events/2026/10/2026-10-31_ZOMBIE FES 2026 DAY3.ts
@@ -0,0 +1,53 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ZOMBIE FES 2026 DAY3",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-10-31",
+  open: "13:00",
+  start: "14:00",
+  end: undefined,
+  region: "神奈川",
+  location: "横須賀うみかぜ公園 特設ステージ",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-10-31_ZOMBIE FES 2026 DAY3.jpg",
+      ref: "https://x.com/takanenofficial/status/2087106979621974348",
+    },
+  ],
+  link: {
+    text: "イベント サイト",
+    url: "https://zombiefes.jp/",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      title: "1部",
+      types: ["LIVE"],
+      open: "13:00",
+      start: "14:00",
+    },
+    {
+      title: "2部",
+      types: ["LIVE"],
+      open: "17:00",
+      start: "18:00",
+    },
+  ],
+  updatedAt: "2026-08-11",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [イベント サイト](https://zombiefes.jp/)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2087106979621974348)
+`;
