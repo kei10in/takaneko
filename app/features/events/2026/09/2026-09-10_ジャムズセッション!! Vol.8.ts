af0a6839abcefb0ecbc269d77599020cad2daf08
Keisuke Ijuin <kei10in@gmail.com>
2026-08-12 22:04:19 +0900

    イベントを更新

---
+++ app/features/events/2026/09/2026-09-10_ジャムズセッション!! Vol.8.ts
@@ -0,0 +1,44 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ジャムズセッション!! Vol.8",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-09-10",
+  open: "16:00",
+  start: "16:30",
+  end: undefined,
+  region: "東京",
+  location: "品川ステラボール",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-09-10_ジャムズセッション!! Vol.8.jpg",
+      ref: "https://x.com/takanenofficial/status/2087517261095452675",
+      tags: ["timetable"],
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://t-dv.com/JamsSessionVol8",
+  streamings: undefined,
+  goods: {
+    time: ["終演後"],
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: {
+    types: ["LIVE"],
+    start: "19:45",
+    end: "20:10",
+  },
+  updatedAt: "2026-08-12",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2087517261095452675)
+`;
