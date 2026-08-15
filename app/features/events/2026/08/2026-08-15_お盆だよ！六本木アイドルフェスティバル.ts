c9a4aafedf71a9e04278313e23daf4d663a0ed2e
Keisuke Ijuin <kei10in@gmail.com>
2026-08-16 00:44:04 +0900

    イベントを追加。

--- app/features/events/2026/08/2026-08-15_お盆だよ！六本木アイドルフェスティバル.ts
+++ app/features/events/2026/08/2026-08-15_お盆だよ！六本木アイドルフェスティバル.ts
@@ -21,6 +21,18 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2082400841244496230",
       tags: ["timetable"],
     },
+    {
+      path: "/events/2026/2026-08-15_お盆だよ！六本木アイドルフェスティバル_物販・特典会スケジュール.jpg",
+      ref: "https://x.com/takanenofficial/status/2088400276549976066",
+    },
+    {
+      path: "/takaneko/goods/2026/2026-08-06_ミニフォトカード「4th Anniversary」.jpg",
+      ref: "https://x.com/takanenofficial/status/2088400276549976066",
+    },
+    {
+      path: "/events/2026/2026-08-15_お盆だよ！六本木アイドルフェスティバル_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2088488202495762782",
+    },
   ],
   link: {
     text: "イベント公式サイト",
@@ -32,15 +44,25 @@ export const meta: EventMetaDescriptor = {
     url: "https://abema.go.link/6TqTX",
   },
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["13:45", "14:45"],
+    lineup: ["ミニフォトカード / チェキ"],
+    url: "https://x.com/takanenofficial/status/2088400276549976066",
   },
   acts: [
     {
       types: ["LIVE"],
       start: "15:05",
       end: "15:30",
+      setlist: [
+        "衣装: 2026 夏衣装",
+        "初恋のひと。",
+        "女の子は強い",
+        "MC",
+        "僕らの青",
+        "ファンサ",
+        "美しく生きろ",
+      ],
+      url: "https://x.com/Mikuru_hositani/status/2088570855458791666",
     },
     {
       title: "カラオケコラボ",
@@ -48,40 +70,47 @@ export const meta: EventMetaDescriptor = {
       start: "16:50",
       end: "17:05",
       description: `
-        - **ユニット2:**
+        - **ユニット2: 「8月生まれ」ユニット:**
           - 愛白かなた (AOAO)
           - 橘るあ (THE ORCHESTRA TOKYO)
           - 籾山ひめり (高嶺のなでしこ)
           - 里仲菜月 (Task have Fun)
           - 石浜芽衣 (虹のコンキスタドール)
-        - **ユニット3:**
+        - **ユニット3: 「名前に食べ物がつく」ユニット:**
           - 塩澤みさき (AOAO)
           - 橋本桃呼 (高嶺のなでしこ)
           - 松本ももな (高嶺のなでしこ)
           - 瀬崎くるみ (ChumToto)
           - 栗原舞優 (虹のコンキスタドール)
           - 高梨結 (ラフ×ラフ)
-        - **ユニット4:**
+        - **ユニット4: 「北関東出身」ユニット:**
           - はのんまゆ (INUWASI)
           - 裕木真生 (THE ORCHESTRA TOKYO)
           - 涼海すう (高嶺のなでしこ)
           - 一宮ゆい (虹のコンキスタドール)
           - 小柴あいり (FES☆TIVE)
       `,
+      costume: "2026 夏衣装",
       links: [
         "https://x.com/takanenofficial/status/2087873089778630879",
         "https://x.com/idolotakarakuji/status/2087449256395481339",
         "https://x.com/idolotakarakuji/status/2087449507826933987",
         "https://x.com/idolotakarakuji/status/2087449759657128256",
+        "https://x.com/takanenofficial/status/2088567732531069111",
       ],
     },
   ],
-  updatedAt: "2026-08-14",
+  updatedAt: "2026-08-15",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [#たかねこセトリ](https://x.com/Mikuru_hositani/status/2088570855458791666)
+  - [カラオケコラボステージ出演報告 - 公式 X](https://x.com/takanenofficial/status/2088567732531069111)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2088531300500300176)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2088488202495762782)
+  - [きょうのたかねこ](https://x.com/takanenofficial/status/2088400276549976066)
   - [カラオケコラボ出演告知 - 公式 X](https://x.com/takanenofficial/status/2087873089778630879)
   - [配信タイムテーブル - ABEMA X](https://x.com/ABEMA/status/2087378540455161990)
   - [カラオケコラボ ユニット2参加メンバー発表 - 六本木アイドルフェス X](https://x.com/idolotakarakuji/status/2087449256395481339)
