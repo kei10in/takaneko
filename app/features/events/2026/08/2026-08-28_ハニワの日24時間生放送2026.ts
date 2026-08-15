235110210c71ab4cedce25e34068658b81e9c2f0
Keisuke Ijuin <kei10in@gmail.com>
2026-08-16 01:05:11 +0900

    イベントを追加。

---
+++ app/features/events/2026/08/2026-08-28_ハニワの日24時間生放送2026.ts
@@ -0,0 +1,47 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ハニワの日24時間生放送2026",
+  category: "STREAMING",
+  date: "2026-08-28",
+  start: undefined,
+  end: "21:00",
+  region: "Web",
+  location: "",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-08-27_ハニワの日24時間生放送2026.jpg",
+      ref: "https://x.com/HoneyWorks_828/status/2086059864858444212",
+    },
+  ],
+  link: undefined,
+  ticket: "",
+  streamings: [
+    {
+      text: "YouTube Live",
+      url: "https://www.youtube.com/live/wVx3WnoKAU",
+    },
+    {
+      text: "ニコニコ生放送",
+      url: "https://live.nicovideo.jp/watch/lv351123185",
+    },
+  ],
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-16",
+};
+
+export const content = /* md */ `
+  ## 配信時間
+
+  2026年8月27日 (木) 21:00 〜 2026年8月28日 (金) 21:00
+
+  ## リンク
+
+  - [HoneyWorks X 告知](https://x.com/HoneyWorks_828/status/2086059864858444212)
+`;
