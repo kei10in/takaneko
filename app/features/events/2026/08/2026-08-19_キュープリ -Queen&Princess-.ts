b14e6c246ef11b112f6a5bc9e7828f9373d7047e
Keisuke Ijuin <kei10in@gmail.com>
2026-08-19 22:29:04 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-19_キュープリ -Queen&Princess-.ts
+++ app/features/events/2026/08/2026-08-19_キュープリ -Queen&Princess-.ts
@@ -16,6 +16,20 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-08-19_キュープリ -Queen&Princess-.jpg",
       ref: "https://x.com/takanenofficial/status/2076631532446556458",
     },
+    {
+      path: "/events/2026/2026-08-19_キュープリ -Queen&Princess-_タイムテーブル.jpg",
+      ref: "https://x.com/takanenofficial/status/2089714283198472597",
+      tags: ["timetable"],
+    },
+    {
+      path: "/takaneko/goods/2026/2026-08-06_ミニフォトカード「4th Anniversary」.jpg",
+      ref: "https://x.com/takanenofficial/status/2089714283198472597",
+      tags: ["merch-item"],
+    },
+    {
+      path: "/events/2026/2026-08-19_キュープリ -Queen&Princess-_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2090046871637221584",
+    },
   ],
   link: {
     text: "",
@@ -24,12 +38,26 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://t-dv.com/queen_princess",
   streamings: undefined,
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["終演後"],
+    lineup: ["ミニフォトカード「4th Anniversary」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2089714283198472597",
   },
-  acts: [],
-  updatedAt: "2026-07-17",
+  acts: {
+    types: ["LIVE"],
+    start: "19:55",
+    end: "20:25",
+    setlist: [
+      "初恋のこたえ。",
+      "病名恋ワズライ",
+      "推しの魔法",
+      "MC",
+      "誇り高きアイドル",
+      "美しく生きろ",
+      "ファンサ",
+    ],
+    url: "https://x.com/Mikuru_hositani/status/2090056799923974237",
+  },
+  updatedAt: "2026-08-19",
 };
 
 export const content = /* md */ `
@@ -39,5 +67,10 @@ export const content = /* md */ `
 
   ## リンク
 
+  - [#たかねこセトリ](https://x.com/Mikuru_hositani/status/2090056799923974237)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2090050099913302235)
+  - [出演報告 - HEROINES FES X](https://x.com/HEROINES_FES/status/2090039590795301363)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2090046871637221584)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2089714283198472597)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2076631532446556458)
 `;
