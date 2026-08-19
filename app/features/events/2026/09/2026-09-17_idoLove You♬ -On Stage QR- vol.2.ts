959345eada34e74a95846e9a1d18c513ffb81da9
Keisuke Ijuin <kei10in@gmail.com>
2026-08-19 22:35:45 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-17_idoLove You♬ -On Stage QR- vol.2.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "idoLove You♬ -On Stage QR- vol.2",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-09-17",
+  open: "09:30",
+  start: "10:00",
+  end: undefined,
+  region: "東京",
+  location: "品川ステラボール",
+  present: ["高嶺のなでしこ2"],
+  images: [
+    {
+      path: "/events/2026/2026-09-17_idoLove You♬ -On Stage QR- vol.2.jpg",
+      ref: "https://x.com/ily_joqr/status/2090031018061267241",
+    },
+  ],
+  link: {
+    text: "イベント公式サイト",
+    url: "https://www.joqr.co.jp/qr/program/ily/blog/180892/",
+  },
+  ticket: "https://ticketdive.com/event/idoLoveYou_0917",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-08-19",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - idoLove You♬ -On The Radio- X](https://x.com/ily_joqr/status/2090031018061267241)
+`;
