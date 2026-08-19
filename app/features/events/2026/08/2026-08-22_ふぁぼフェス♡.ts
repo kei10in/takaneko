4352d882f5f46fefc9c1bf68edc714e0893515e7
Keisuke Ijuin <kei10in@gmail.com>
2026-08-19 22:38:39 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-22_ふぁぼフェス♡.ts
+++ app/features/events/2026/08/2026-08-22_ふぁぼフェス♡.ts
@@ -16,6 +16,11 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-08-22_ふぁぼフェス♡.jpg",
       ref: "https://x.com/_fav_me_/status/2080956221813641527",
     },
+    {
+      path: "/events/2026/2026-08-22_ふぁぼフェス♡_タイムテーブル.jpg",
+      ref: "https://x.com/takanenofficial/status/2089557900335239585",
+      tags: ["timetable"],
+    },
   ],
   link: {
     text: "",
@@ -24,17 +29,24 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://w.pia.jp/t/favfes/",
   streamings: undefined,
   goods: {
-    time: undefined,
+    time: ["19:10", "20:10"],
     lineup: undefined,
     url: undefined,
   },
-  acts: [],
-  updatedAt: "2026-07-25",
+  acts: [
+    {
+      types: ["LIVE"],
+      start: "18:10",
+      end: "18:35",
+    },
+  ],
+  updatedAt: "2026-08-19",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2089557900335239585)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2081000360387440701)
   - [開催決定 - fav me X](https://x.com/_fav_me_/status/2080956221813641527)
 `;
