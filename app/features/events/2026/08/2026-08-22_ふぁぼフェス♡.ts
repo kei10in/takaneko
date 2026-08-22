9d0d4a5ddad57797a1f60006a5d17f87da22a159
Keisuke Ijuin <kei10in@gmail.com>
2026-08-22 22:18:33 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-22_ふぁぼフェス♡.ts
+++ app/features/events/2026/08/2026-08-22_ふぁぼフェス♡.ts
@@ -21,6 +21,15 @@ export const meta: EventMetaDescriptor = {
       ref: "https://x.com/takanenofficial/status/2089557900335239585",
       tags: ["timetable"],
     },
+    {
+      path: "/takaneko/goods/2026/2026-08-06_ミニフォトカード「4th Anniversary」.jpg",
+      ref: "https://x.com/takanenofficial/status/2090803609034453344",
+      tags: ["merch-item"],
+    },
+    {
+      path: "/events/2026/2026-08-22_ふぁぼフェス♡_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2091106505739448334",
+    },
   ],
   link: {
     text: "",
@@ -30,22 +39,36 @@ export const meta: EventMetaDescriptor = {
   streamings: undefined,
   goods: {
     time: ["19:10", "20:10"],
-    lineup: undefined,
-    url: undefined,
+    lineup: ["ミニフォトカード「4th Anniversary」 / チェキ"],
+    url: "https://x.com/takanenofficial/status/2090803609034453344",
   },
   acts: [
     {
       types: ["LIVE"],
       start: "18:10",
       end: "18:35",
+      setlist: [
+        "衣装: 僕は君になれない衣装",
+        "ヒロインは平均以下。",
+        "女の子は強い",
+        "MC",
+        "恋を知った世界",
+        "ファンサ",
+        "I’M YOUR IDOL",
+      ],
+      url: "https://x.com/Mikuru_hositani/status/2091139751109951954",
     },
   ],
-  updatedAt: "2026-08-19",
+  updatedAt: "2026-08-22",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [#たかねこセトリ](https://x.com/Mikuru_hositani/status/2091139751109951954)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2091100709978792057)
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2091106505739448334)
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2090803609034453344)
   - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2089557900335239585)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2081000360387440701)
   - [開催決定 - fav me X](https://x.com/_fav_me_/status/2080956221813641527)
