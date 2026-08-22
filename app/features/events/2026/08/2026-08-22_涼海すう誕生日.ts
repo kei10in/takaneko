195d2eca9c620d8638470663201d69f19bd7f6fd
Keisuke Ijuin <kei10in@gmail.com>
2026-08-22 23:10:37 +0900

    誕生日を追加。

---
+++ app/features/events/2026/08/2026-08-22_涼海すう誕生日.ts
@@ -0,0 +1,44 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "涼海すう誕生日🩵",
+  category: "BIRTHDAY",
+  date: "2026-08-22",
+  region: "🎂",
+  images: [
+    {
+      path: "/events/2026/2026-08-22_涼海すう誕生日.jpg",
+      ref: "https://x.com/su_suzumi_/status/2090952118891122971",
+    },
+    {
+      path: "/takaneko/birthday-goods/2026-08-22_涼海すう誕生日記念グッズ.jpg",
+      ref: "https://x.com/takanenofficial/status/2091134036362268850",
+    },
+  ],
+  updatedAt: "2026-08-22",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  ## 誕生日写真
+
+  - [🩵涼海すう X](https://x.com/su_suzumi_/status/2090952118891122971)
+
+  ## 公式 X
+
+  - [公式 X](https://x.com/takanenofficial/status/2091004936213668264)
+  - [誕生日記念グッズ](https://x.com/takanenofficial/status/2091134036362268850)
+
+  ## お祝いメッセージ
+
+  - [💛城月菜央](https://x.com/nao_kizuki/status/2091115601892966509)
+    <!-- - [🩵涼海すう]() -->
+  - [🩷橋本桃呼](https://x.com/MomokoHashimoto/status/2091005921329820022)
+  - [🤍葉月紗蘭](https://x.com/saara_hazuki/status/2091011554175304176)
+  - [🧡東山恵里沙](https://x.com/erisahigasiyama/status/2090816273408315776)
+  - [💜日向端ひな](https://x.com/hina_hinahata/status/2091008753906995482)
+  - [❤️星谷美来](https://x.com/Mikuru_hositani/status/2091069707571040560)
+  - [🎀松本ももな](https://x.com/momonamatsumoto/status/2091065524788855061)
+  - [💙籾山ひめり](https://x.com/himeri_momiyama/status/2091062119857086722)
+`;
