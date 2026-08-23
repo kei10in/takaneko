25030fea55c43ce12e748359e4d16ce33ab88940
Keisuke Ijuin <kei10in@gmail.com>
2026-08-23 23:05:48 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-25_星谷美来 卒業公演 〜ずっと恋の病でいてね〜.ts
+++ app/features/events/2026/08/2026-08-25_星谷美来 卒業公演 〜ずっと恋の病でいてね〜.ts
@@ -20,6 +20,10 @@ export const meta: EventMetaDescriptor = {
       path: "/events/2026/2026-08-25_星谷美来 卒業公演 〜ずっと恋の病でいてね〜_FC会員限定プレゼントキャンペーン.png",
       ref: "https://x.com/takanenofficial/status/2089283998124318734",
     },
+    {
+      path: "/takaneko/live-goods/2026-08-25_星谷美来 卒業公演 〜ずっと恋の病でいてね〜.jpg",
+      ref: "https://x.com/takanenofficial/status/2091498185726334568",
+    },
   ],
   link: {
     text: "イベント詳細",
@@ -28,17 +32,31 @@ export const meta: EventMetaDescriptor = {
   ticket: "",
   streamings: undefined,
   goods: {
-    time: undefined,
-    lineup: undefined,
-    url: undefined,
+    time: ["14:00", "終演後30分程度"],
+    lineup: [
+      "恋わずらいアクリルスタンド (14cm)",
+      "みくるんとお揃いニャン♪ 前髪ピン",
+      "恋わずらいタペストリー",
+      "卒コン限定!! ポーチでもサコッシュでも♡",
+      "365日ずっと一緒❤︎ 卓上カレンダー",
+      "特別♡卒業記念生写真セット",
+      "ワッペンポロシャツ",
+      "にゃでしこマスコットキーホルダー",
+      "オーロラクリアマルチケース",
+      "ポータブルファン",
+      "生写真 (ボタニカル衣装 ver.)",
+      "ミニフォト (ボタニカル衣装 ver.)",
+    ],
+    url: "https://x.com/takanenofficial/status/2091498185726334568",
   },
   acts: [],
-  updatedAt: "2026-08-17",
+  updatedAt: "2026-08-23",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [GOODS MENU 公開 - 公式 X](https://x.com/takanenofficial/status/2091498185726334568)
   - [FC 会員限定 卒業メッセージ募集・プレゼントキャンペーン 告知 - 公式 X](https://x.com/takanenofficial/status/2089283998124318734)
   - [一般販売開始 - 公式 X](https://x.com/takanenofficial/status/2083357041314402597)
   - [詳細告知 - 公式 X](https://x.com/takanenofficial/status/2073232831061676274)
