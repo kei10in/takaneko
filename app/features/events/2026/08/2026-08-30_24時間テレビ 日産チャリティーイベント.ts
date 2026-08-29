d9a9d9c2d87d9ea59b26d7f22a47a86339761899
Keisuke Ijuin <kei10in@gmail.com>
2026-08-29 23:20:18 +0900

    イベントを更新

--- app/features/events/2026/08/2026-08-30_24時間テレビ 日産チャリティーイベント.ts
+++ app/features/events/2026/08/2026-08-30_24時間テレビ 日産チャリティーイベント.ts
@@ -5,9 +5,9 @@ export const meta: EventMetaDescriptor = {
   category: "OTHER",
   liveType: "EVENT_LIVE",
   date: "2026-08-30",
-  open: undefined,
-  start: undefined,
-  end: undefined,
+  open: "09:50",
+  start: "10:00",
+  end: "21:00",
   region: "神奈川",
   location: "日産グローバル本社ギャラリー",
   present: ["高嶺のなでしこ2"],
@@ -15,6 +15,16 @@ export const meta: EventMetaDescriptor = {
     {
       path: "/events/2026/2026-08-30_24時間テレビ 日産チャリティーイベント.jpg",
       ref: "https://x.com/NissanJP/status/2085297328051880377",
+    },
+    {
+      path: "/events/2026/2026-08-30_24時間テレビ 日産チャリティーイベント_タイムテーブル1.jpg",
+      ref: "https://x.com/takanenofficial/status/2093558023277150654",
+      tags: ["timetable"],
+    },
+    {
+      path: "/events/2026/2026-08-30_24時間テレビ 日産チャリティーイベント_タイムテーブル2.jpg",
+      ref: "https://x.com/takanenofficial/status/2093558023277150654",
+      tags: ["timetable"],
     },
   ],
   link: {
@@ -28,13 +38,53 @@ export const meta: EventMetaDescriptor = {
     lineup: undefined,
     url: undefined,
   },
-  acts: [],
-  updatedAt: "2026-08-09",
+  acts: [
+    {
+      title: "ウッディ!?と愉快な仲間たちの謎解き大冒険",
+      types: ["OTHER"],
+      description:
+        "**出演:** ラパルフェ、土佐兄弟、高嶺のなでしこ (橋本・涼海・葉月・城月)、カカロニ、今夜も星が綺麗、紺野ぶるま",
+      start: "11:10",
+      end: "11:35",
+    },
+    {
+      title: "芸能人趣味-1 GP",
+      types: ["OTHER"],
+      description: `
+        **サポーター:** みかん
+
+        **プレゼンター:** BALLISTIK BOYZ (砂田・松井・海沼)、ぱーてぃーちゃん 信子、ガリベンズ矢野
+
+        **審査員:** ONE LOVE ONE HEART (佐々木・相原・矢嶋)、高嶺のなでしこ (籾山・松本・日向端・東山)
+        `,
+      start: "15:00",
+      end: "15:25",
+    },
+    {
+      title: "LIVE & オークション",
+      types: ["LIVE"],
+      description: "出演後にエアハイタッチ会を実施予定",
+      start: "15:25",
+      end: "16:05",
+    },
+    {
+      title: "2分30秒で成功させろ",
+      types: ["OTHER"],
+      description:
+        "**出演:** ONE LOVE ONE HEART (藤咲・洸瑛・笹原)、高嶺のなでしこ (橋本・城月)、きしたかの、ぱーてぃーちゃん",
+      start: "20:05",
+      end: "20:53",
+    },
+  ],
+  updatedAt: "2026-08-29",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [#あしたのたかねこ](https://x.com/takanenofficial/status/2093700205182914655)
+  - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2093558023277150654)
+  - [イベントタイムスケジュール詳細 - 日産自動車](https://www2.nissan.co.jp/24/2026/PDF/event_schedule.pdf)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2085589250070761652)
   - [出演者情報・優先観覧エリア抽選案内 - 日産自動車 X](https://x.com/NissanJP/status/2085297328051880377)
 `;
