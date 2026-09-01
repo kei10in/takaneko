c1d1b9f80dfbb871a1e5bd601d9cd3d3058f2bb9
Keisuke Ijuin <kei10in@gmail.com>
2026-09-01 22:15:22 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-13_ワンマンライブ 〜CROWNED IN BLOOM〜.ts
@@ -0,0 +1,73 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "ワンマンライブ 〜CROWNED IN BLOOM〜",
+  category: "LIVE",
+  liveType: "SOLO",
+  date: "2026-09-13",
+  open: "13:30",
+  start: "14:30",
+  end: undefined,
+  region: "東京",
+  location: "Shibuya LOVEZ",
+  present: ["高嶺のなでしこ3"],
+  images: [
+    {
+      path: "/events/2026/2026-09-13_ワンマンライブ 〜CROWNED IN BLOOM〜.jpg",
+      ref: "https://x.com/takanenofficial/status/2094440635038364024",
+    },
+  ],
+  link: {
+    text: "開催詳細",
+    url: "https://takanenonadeshiko.jp/?p=5528",
+  },
+  ticket: "https://eplus.jp/takanenonadeshiko/",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  meetAndGreet: {
+    title: "個別 2 ショット撮影会",
+    sessions: [
+      {
+        title: "1 部",
+        start: "17:00",
+        end: "17:45",
+        lastEntry: "17:30",
+        lanes: ["涼海すう", "葉月紗蘭", "松本ももな", "籾山ひめり"],
+      },
+      {
+        title: "2 部",
+        start: "18:00",
+        end: "18:45",
+        lastEntry: "18:30",
+        lanes: ["城月菜央", "橋本桃呼", "東山恵里沙", "日向端ひな"],
+      },
+      {
+        title: "3 部",
+        start: "19:00",
+        end: "19:45",
+        lastEntry: "17:30",
+        lanes: ["涼海すう", "葉月紗蘭", "松本ももな", "籾山ひめり"],
+      },
+      {
+        title: "4 部",
+        start: "20:00",
+        end: "20:45",
+        lastEntry: "18:30",
+        lanes: ["城月菜央", "橋本桃呼", "東山恵里沙", "日向端ひな"],
+      },
+    ],
+  },
+  updatedAt: "2026-09-01",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [公式ニュース](https://takanenonadeshiko.jp/?p=5528)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2094440635038364024)
+`;
