a045659235926a6e366e5b9036f6f09b0df31ee2
Keisuke Ijuin <kei10in@gmail.com>
2026-09-04 23:42:44 +0900

    イベントを追加。

---
+++ app/features/events/2026/11/2026-11-05_でびぱっぱ 〜Road to 幕張イベントホール〜.ts
@@ -0,0 +1,40 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "でびぱっぱ 〜Road to 幕張イベントホール〜",
+  title: "Devil ANTHEM. presents でびぱっぱ 〜Road to 幕張イベントホール〜",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-11-05",
+  open: "12:00",
+  start: "13:00",
+  end: undefined,
+  region: "東京",
+  location: "Shibuya LOVEZ",
+  present: ["高嶺のなでしこ3"],
+  images: [
+    {
+      path: "/events/2026/2026-11-05_でびぱっぱ 〜Road to 幕張イベントホール〜.jpg",
+      ref: "https://x.com/takanenofficial/status/2095836861600244214",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://ticketvillage.jp/events/13876",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-09-04",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2095836861600244214)
+`;
