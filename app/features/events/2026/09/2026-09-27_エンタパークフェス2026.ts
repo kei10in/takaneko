bcae43475cfec46708d4cc91aed55f9fd0d71e13
Keisuke Ijuin <kei10in@gmail.com>
2026-09-04 23:50:34 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-27_エンタパークフェス2026.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "エンタパークフェス2026",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-09-27",
+  open: undefined,
+  start: "10:00",
+  end: "20:00",
+  region: "東京",
+  location: "イオンモールむさし村山 つむぐ広場特設会場",
+  present: ["高嶺のなでしこ3"],
+  images: [
+    {
+      path: "/events/2026/2026-09-27_エンタパークフェス2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2095829445781406169",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-09-04",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2095829445781406169)
+`;
