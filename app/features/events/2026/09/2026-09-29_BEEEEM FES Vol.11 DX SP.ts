82e79feca01945ba1913a96356fef300f81a688d
Keisuke Ijuin <kei10in@gmail.com>
2026-09-08 06:57:18 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-29_BEEEEM FES Vol.11 DX SP.ts
@@ -0,0 +1,39 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "BEEEEM FES Vol.11 DX SP",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-09-29",
+  open: "16:30",
+  start: "17:30",
+  end: undefined,
+  region: "東京",
+  location: "Zepp DiverCity (TOKYO)",
+  present: ["高嶺のなでしこ3"],
+  images: [
+    {
+      path: "/events/2026/2026-09-29_BEEEEM FES Vol.11 DX SP.jpg",
+      ref: "https://x.com/takanenofficial/status/2096917646302138657",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://w.pia.jp/t/beeeemfes/",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-09-08",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2096917646302138657)
+`;
