ab6bab29f218dddf781c98f05a22c582d1ab2db3
Keisuke Ijuin <kei10in@gmail.com>
2026-09-10 22:15:20 +0900

    feat: ミニフォトカード「秋服 2026」をトレード画像に追加

--- app/features/products/productImages.ts
+++ app/features/products/productImages.ts
@@ -177,15 +177,18 @@ import { _4thファンミ_缶バッジ } from "./2026/2026-08-06_缶バッジ「
 import { _4thAnniversarySTELLA_缶バッジ } from "./2026/2026-08-07_缶バッジ「STELLA」";
 import { 星谷美来_特別卒業記念生写真セット_生写真 } from "./2026/2026-08-25_特別♡卒業記念生写真セット";
 import { ボタニカル衣装_生写真 } from "./2026/2026-08-25_生写真「ボタニカル衣装」";
+import { 秋服2026_ミニフォト } from "./2026/2026-09-09_ミニフォトカード「秋服 2026」";
 import { ItemDescription, ProductLine, RandomGoods, TradeTextType } from "./product";
 
 export const TAKANEKO_PHOTOS_FEATURED: RandomGoods[] = [
+  秋服2026_ミニフォト,
   星谷美来_特別卒業記念生写真セット_生写真,
   ボタニカル衣装_生写真,
   ボタニカル衣装_ミニフォト,
 ];
 
 export const TAKANEKO_PHOTOS: RandomGoods[] = [
+  秋服2026_ミニフォト,
   星谷美来_特別卒業記念生写真セット_生写真,
   ボタニカル衣装_生写真,
   _4thAnniversary_生写真,
