a07907f2b18d2651e0a6e4efb2b69c83c3f48ae8
Keisuke Ijuin <kei10in@gmail.com>
2026-09-10 21:31:32 +0900

    イベントを更新

--- app/features/events/2026/09/2026-09-10_ジャムズセッション!! Vol.8.ts
+++ app/features/events/2026/09/2026-09-10_ジャムズセッション!! Vol.8.ts
@@ -21,6 +21,10 @@ export const meta: EventMetaDescriptor = {
       path: "/takaneko/goods/2026/2026-09-09_ミニフォトカード「秋服 2026」.jpg",
       ref: "https://x.com/takanenofficial/status/2097694012807262457",
     },
+    {
+      path: "/events/2026/2026-09-10_ジャムズセッション!! Vol.8_本日のおチェキ.jpg",
+      ref: "https://x.com/takanekomanager/status/2098012663897071966",
+    },
   ],
   link: {
     text: "",
@@ -37,6 +41,15 @@ export const meta: EventMetaDescriptor = {
     types: ["LIVE"],
     start: "19:45",
     end: "20:10",
+    setlist: [
+      "衣装: 2026 夏衣装",
+      "初恋のひと。",
+      "女の子は強い",
+      "ハートブーケ",
+      "恋を知った世界",
+      "ファンサ",
+    ],
+    links: ["https://x.com/takanenofficial/status/2098008584269250651"],
   },
   updatedAt: "2026-09-10",
 };
@@ -44,6 +57,8 @@ export const meta: EventMetaDescriptor = {
 export const content = /* md */ `
   ## リンク
 
+  - [本日のおチェキ](https://x.com/takanekomanager/status/2098012663897071966)
+  - [出演報告 - 公式 X](https://x.com/takanenofficial/status/2098008584269250651)
   - [#あしたのたかねこ](https://x.com/takanenofficial/status/2097694012807262457)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2087517261095452675)
 `;
