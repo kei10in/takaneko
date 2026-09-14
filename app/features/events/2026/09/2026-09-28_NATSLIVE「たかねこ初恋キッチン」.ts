01d5190126b30a0ecedd6d380524472ba2d5eb5f
Keisuke Ijuin <kei10in@gmail.com>
2026-09-14 22:56:21 +0900

    イベントを追加。

---
+++ app/features/events/2026/09/2026-09-28_NATSLIVE「たかねこ初恋キッチン」.ts
@@ -0,0 +1,65 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "NATSLIVE「たかねこ初恋キッチン。#9」",
+  category: "VARIETY",
+  liveType: undefined,
+  date: "2026-09-28",
+  open: "18:30",
+  start: "19:00",
+  end: "20:00",
+  region: "東京",
+  location: "NATSLIVE CAFE 表参道",
+  present: ["城月菜央", "東山恵里沙", "松本ももな"],
+  images: [
+    {
+      path: "/events/2026/2026-09-28_NATSLIVE「たかねこ初恋キッチン」.jpg",
+      ref: "https://x.com/NATSLIVE_app/status/2099377607431352805",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "",
+  streamings: {
+    text: "NATSLIVE",
+    url: "",
+  },
+  goods: {
+    time: undefined,
+    lineup: [
+      // "レシピカード「カード A タイプ」",
+      // "レシピカード「カード B タイプ」",
+      // "レシピカード「カード C タイプ」",
+    ],
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-09-14",
+};
+
+export const content = /* md */ `
+  ## 内容
+
+  生配信。ライブ観覧あり。
+
+  配信の視聴には NATSLIVE アプリのインストールが必要です。
+
+  ## アーカイブ配信
+
+  無料会員は配信後 7 日間アーカイブ視聴可能。
+
+  ゴールド会員は配信後 1 年間アーカイブ視聴可能。
+
+  <!--
+  ## グッズの販売期間
+
+  2026年09月28日 19:00 〜 2026年09月29日 19:00
+  -->
+
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2099430454658998744)
+  - [告知 - NATSLIVE X](https://x.com/NATSLIVE_app/status/2099377607431352805)
+`;
