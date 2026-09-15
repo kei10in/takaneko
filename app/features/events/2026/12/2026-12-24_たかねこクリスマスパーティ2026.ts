ce91f82ac18ed5f14e4aa9aa0068ff6add2a569f
Keisuke Ijuin <kei10in@gmail.com>
2026-09-15 21:59:21 +0900

    イベントを追加。

---
+++ app/features/events/2026/12/2026-12-24_たかねこクリスマスパーティ2026.ts
@@ -0,0 +1,54 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "たかねこクリスマスパーティ2026",
+  category: "LIVE",
+  liveType: "SOLO",
+  date: "2026-12-24",
+  open: "15:00",
+  start: "16:00",
+  end: undefined,
+  region: "東京",
+  location: "ヒューリックホール東京",
+  present: ["高嶺のなでしこ3"],
+  images: [
+    {
+      path: "/events/2026/2026-12-24_たかねこクリスマスパーティ2026.jpg",
+      ref: "https://x.com/takanenofficial/status/2099031644103668121",
+    },
+  ],
+  link: {
+    text: "イベント詳細 - 公式ニュース",
+    url: "https://takanenonadeshiko.jp/?p=5557",
+  },
+  ticket: "https://eplus.jp/takanenonadeshiko/",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [
+    {
+      title: "第一部",
+      types: ["LIVE"],
+      open: "15:00",
+      start: "16:00",
+    },
+    {
+      title: "第二部",
+      types: ["LIVE"],
+      open: "18:30",
+      start: "19:30",
+    },
+  ],
+  updatedAt: "2026-09-15",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [イベント詳細 - 公式ニュース](https://takanenonadeshiko.jp/?p=5557)
+  - [FC 先行チケット](https://takanekofc.com/blogs/news/2MpW4XsiYW1wD2eccQFjK)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2099031644103668121)
+`;
