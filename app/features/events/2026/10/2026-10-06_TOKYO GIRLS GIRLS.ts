3b7e186fc98b434c80b204ea19a6680b27873129
Keisuke Ijuin <kei10in@gmail.com>
2026-09-15 21:22:50 +0900

    イベントを更新

--- app/features/events/2026/10/2026-10-06_TOKYO GIRLS GIRLS.ts
+++ app/features/events/2026/10/2026-10-06_TOKYO GIRLS GIRLS.ts
@@ -5,16 +5,25 @@ export const meta: EventMetaDescriptor = {
   category: "LIVE",
   liveType: "FESTIVAL",
   date: "2026-10-06",
-  open: "11:00",
-  start: "11:20",
-  end: undefined,
+  open: "10:30",
+  start: "11:30",
+  end: "21:00",
   region: "東京",
   location: "Shibuya LOVEZ",
   present: ["高嶺のなでしこ3"],
   images: [
     {
+      path: "/events/2026/2026-10-06_TOKYO GIRLS GIRLS_出演者一覧.jpg",
+      ref: "https://x.com/Girls_Girlsinfo/status/2099831693264011295",
+    },
+    {
       path: "/events/2026/2026-10-06_TOKYO GIRLS GIRLS.jpg",
       ref: "https://x.com/takanenofficial/status/2087157080797155564",
+    },
+    {
+      path: "/events/2026/2026-10-06_TOKYO GIRLS GIRLS_タイムテーブル.jpg",
+      ref: "https://x.com/Girls_Girlsinfo/status/2099831693264011295",
+      tags: ["timetable"],
     },
   ],
   link: {
@@ -24,16 +33,24 @@ export const meta: EventMetaDescriptor = {
   ticket: "https://ticketdive.com/event/tgg_1006",
   streamings: undefined,
   goods: {
-    time: undefined,
+    time: ["終演後"],
     lineup: undefined,
     url: undefined,
   },
-  acts: [],
-  updatedAt: "2026-08-11",
+  acts: [
+    {
+      types: ["LIVE"],
+      start: "20:10",
+      end: "20:35",
+    },
+  ],
+  updatedAt: "2026-09-15",
 };
 
 export const content = /* md */ `
   ## リンク
 
+  - [タイムテーブル修正版 - GIRLS GIRLS X](https://x.com/Girls_Girlsinfo/status/2099831693264011295)
+  - [タイムテーブル公開 - 公式 X](https://x.com/takanenofficial/status/2099823039886299266)
   - [告知 - 公式 X](https://x.com/takanenofficial/status/2087157080797155564)
 `;
