1912a7a540ab4c446719984ecafba1364fa4694b
Keisuke Ijuin <kei10in@gmail.com>
2026-09-15 22:20:48 +0900

    イベントを追加。

---
+++ app/features/events/2026/10/2026-10-25_中央学院大学 第58回あびこ祭.ts
@@ -0,0 +1,42 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "中央学院大学 第58回あびこ祭",
+  category: "LIVE",
+  liveType: "EVENT_LIVE",
+  date: "2026-10-25",
+  open: undefined,
+  start: "10:00",
+  end: "16:30",
+  region: "千葉",
+  location: "中央学院大学",
+  present: ["高嶺のなでしこ3"],
+  images: [
+    {
+      path: "/events/2026/2026-10-25_中央学院大学 第58回あびこ祭.jpg",
+      ref: "https://x.com/takanenofficial/status/2098260245592338798",
+    },
+  ],
+  link: {
+    text: "あびこ祭（大学祭） | 中央学院大学",
+    url: "https://www.cgu.ac.jp/campuslife/abiko-fes.html",
+  },
+  ticket: "",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-09-15",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [中央学院大学](https://www.cgu.ac.jp/)
+  - [あびこ祭（大学祭） | 中央学院大学](https://www.cgu.ac.jp/campuslife/abiko-fes.html)
+  - [第58回あびこ祭 SPECIAL LIVE 開催決定 | 中央学院大学](https://www.cgu.ac.jp/news/nid00001458.html)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2098260245592338798)
+`;
