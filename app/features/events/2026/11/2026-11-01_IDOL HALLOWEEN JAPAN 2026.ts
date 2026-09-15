1ffe7cf4bc6c22c263956c1407d137296b81411a
Keisuke Ijuin <kei10in@gmail.com>
2026-09-15 21:51:18 +0900

    イベントを追加。

---
+++ app/features/events/2026/11/2026-11-01_IDOL HALLOWEEN JAPAN 2026.ts
@@ -0,0 +1,40 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "IDOL HALLOWEEN JAPAN 2026",
+  title: "IDOL HALLOWEEN JAPAN 2026 Supported by Malymoon",
+  category: "LIVE",
+  liveType: "FESTIVAL",
+  date: "2026-11-01",
+  open: undefined,
+  start: undefined,
+  end: undefined,
+  region: "埼玉",
+  location: "埼玉スタジアム2002 野外特設会場",
+  present: ["高嶺のなでしこ3"],
+  images: [
+    {
+      path: "/events/2026/2026-11-01_IDOL HALLOWEEN JAPAN 2026.jpg",
+      ref: "https://x.com/IDOLHJ_official/status/2099075624216244675",
+    },
+  ],
+  link: {
+    text: "IDOL HALLOWEEN JAPAN 2026",
+    url: "https://idolhalloweenjapan.com/",
+  },
+  ticket: "https://t-dv.com/IHJ2026",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-09-15",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [第3弾出演者発表 - IDOL HALLOWEEN JAPAN X](https://x.com/IDOLHJ_official/status/2099075624216244675)
+`;
