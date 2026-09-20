14a7525d3e37a5a964918e9377f6e5bc9e83b98a
Keisuke Ijuin <kei10in@gmail.com>
2026-09-20 18:00:57 +0900

    イベントを追加。

---
+++ app/features/events/2026/10/2026-10-01_SPECIAL DRAMATIC POP Load to LOVEZ.ts
@@ -0,0 +1,40 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "SPECIAL DRAMATIC POP Load to LOVEZ",
+  title: "ドラマチックレコード presents SPECIAL DRAMATIC POP Load to LOVEZ",
+  category: "LIVE",
+  liveType: "JOINT",
+  date: "2026-10-01",
+  open: "18:15",
+  start: "19:00",
+  end: undefined,
+  region: "東京",
+  location: "GARDEN新木場FACTORY",
+  present: ["高嶺のなでしこ3"],
+  images: [
+    {
+      path: "/events/2026/2026-10-01_SPECIAL DRAMATIC POP Load to LOVEZ.jpg",
+      ref: "https://x.com/takanenofficial/status/2101529282937766324",
+    },
+  ],
+  link: {
+    text: "",
+    url: "",
+  },
+  ticket: "https://tiget.net/events/524277",
+  streamings: undefined,
+  goods: {
+    time: undefined,
+    lineup: undefined,
+    url: undefined,
+  },
+  acts: [],
+  updatedAt: "2026-09-20",
+};
+
+export const content = /* md */ `
+  ## リンク
+
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2101529282937766324)
+`;
