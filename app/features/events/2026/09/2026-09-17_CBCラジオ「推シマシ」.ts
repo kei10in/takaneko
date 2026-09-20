edcc6022e6e7abba67e4dad6a2db1be954266d71
Keisuke Ijuin <kei10in@gmail.com>
2026-09-20 23:26:01 +0900

    ラジオ出演を追加

---
+++ app/features/events/2026/09/2026-09-17_CBCラジオ「推シマシ」.ts
@@ -0,0 +1,36 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "CBCラジオ「推シマシ」",
+  category: "RADIO",
+  date: "2026-09-17",
+  start: "22:00",
+  end: "24:00",
+  region: "ラジオ",
+  present: ["東山恵里沙"],
+  images: [
+    {
+      path: "/events/2026/2026-09-17_CBCラジオ「推シマシ」.png",
+      ref: "https://hicbc.com/radio/oshimashi/",
+    },
+  ],
+  link: {
+    text: "番組ホームページ",
+    url: "https://hicbc.com/radio/oshimashi/",
+  },
+  updatedAt: "2026-09-20",
+};
+
+export const content = /* md */ `
+  ## 内容
+
+  東山恵里沙が 22:30 ごろからリモート生出演。
+
+  ## リンク
+
+  - [radiko 出演時間](https://radiko.jp/share/?sid=CBC&t=20260917223120)
+  - [radiko 番組放送回](https://radiko.jp/share/?sid=CBC&t=20260917220000)
+  - [出演後 - 公式 X](https://x.com/takanenofficial/status/2100590597879075222)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2100568096927019028)
+  - [告知 - 推シマシ X](https://x.com/oshi_mashi/status/2100552538823250392)
+`;
