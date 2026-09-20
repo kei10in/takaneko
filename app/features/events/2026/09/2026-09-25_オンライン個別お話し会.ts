660276eaa3897ae97ee3cf952f8ec0529fe3347e
Keisuke Ijuin <kei10in@gmail.com>
2026-09-20 19:05:06 +0900

    イベントを更新

---
+++ app/features/events/2026/09/2026-09-25_オンライン個別お話し会.ts
@@ -0,0 +1,54 @@
+import { EventMetaDescriptor } from "../../eventMeta";
+
+export const meta: EventMetaDescriptor = {
+  summary: "オンライン個別お話し会📢",
+  category: "MEET_AND_GREET",
+  meetAndGreetTypes: ["オンライン お話し会"],
+  date: "2026-09-25",
+  start: "19:00",
+  end: "22:30",
+  region: "Web",
+  present: ["橋本桃呼", "日向端ひな", "籾山ひめり"],
+  images: [
+    {
+      path: "/events/2026/2026-09-25_オンライン個別お話し会.webp",
+      ref: "https://x.com/takanenofficial/status/2101476440508338521",
+    },
+  ],
+  link: {
+    text: "開催詳細",
+    url: "https://takanenonadeshiko.jp/?p=5601",
+  },
+  updatedAt: "2026-09-20",
+};
+
+export const content = /* md */ `
+  ## 開催情報
+
+  **販売開始:** 2026年09月20日 (日) 21:00 〜 順次
+
+  - 21:00 〜 💛城月菜央
+  - 21:10 〜 🩵涼海すう
+  - 21:20 〜 🤍葉月紗蘭
+  - 21:30 〜 🧡東山恵里沙
+  - 21:40 〜 🎀松本ももな
+  - 21:50 〜 🩷橋本桃呼
+  - 22:00 〜 💜日向端ひな
+  - 22:10 〜 💙籾山ひめり
+
+  ### 2026年09月25日 (金)
+
+  橋本桃呼🩷 / 日向端ひな💜 / 籾山ひめり💙
+
+  1. 19:00 〜 19:30
+  1. 19:35 〜 20:05
+  1. 20:10 〜 20:40
+  1. 20:50 〜 21:20
+  1. 21:25 〜 21:55
+  1. 22:00 〜 22:30
+
+  ## リンク
+
+  - [公式ニュース](https://takanenonadeshiko.jp/?p=5601)
+  - [告知 - 公式 X](https://x.com/takanenofficial/status/2101476440508338521)
+`;
