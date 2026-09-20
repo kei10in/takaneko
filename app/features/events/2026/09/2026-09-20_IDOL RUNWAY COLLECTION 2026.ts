fda934c9f297b62c66686d32f35e5a226b5db0d3
Keisuke Ijuin <kei10in@gmail.com>
2026-09-20 18:23:24 +0900

    イベントを更新

--- app/features/events/2026/09/2026-09-20_IDOL RUNWAY COLLECTION 2026.ts
+++ app/features/events/2026/09/2026-09-20_IDOL RUNWAY COLLECTION 2026.ts
@@ -40,7 +40,7 @@ export const meta: EventMetaDescriptor = {
     url: "https://idolrunwaycollection.jp/",
   },
   ticket: "https://idolrunwaycollection.jp/#tickets",
-  streamings: undefined,
+  streamings: [{ text: "IRC APP", url: "https://x.gd/YiywJ" }],
   goods: {
     time: undefined,
     lineup: undefined,
@@ -48,22 +48,50 @@ export const meta: EventMetaDescriptor = {
   },
   acts: [
     {
+      title: "IRC SPECIAL COLLECTION STAGE",
+      types: ["RUNWAY"],
+      start: "12:05",
+      end: "12:20",
+      description: "涼海すう、松本ももな",
+
+      links: ["https://x.com/takanenofficial/status/2101524249794003151"],
+    },
+    {
       title: "SPECIAL LIVE",
       types: ["LIVE"],
-      start: "14:08",
-      end: "14:23",
+      start: "14:05",
+      end: "14:20",
+      setlist: ["2026 秋衣装", "初恋のひと。", "ファンサ", "美しく生きろ"],
+      links: ["https://x.com/takanenofficial/status/2101559483365535961"],
     },
   ],
-  updatedAt: "2026-09-15",
+  updatedAt: "2026-09-20",
 };
 
 export const content = /* md */ `
-  ## 出演 - ファッションステージ
+  ## レポート
 
-  **出演者:** 涼海すう / 松本ももな
+  ### シネマライフ
+
+  - [松本ももな - シネマライフ X](https://x.com/cinemalife_web/status/2101524683120377860)
+  - [高嶺のなでしこスペシャルライブ- シネマライフ X](https://x.com/cinemalife_web/status/2101553797562839056)
+  - [━IDOL RUNWAY COLLECTION 2026 A/W━ 高嶺のなでしこ・松本ももな、爽やかパステルカラーコーデでランウェイ | ACTOR&ACTRESS,ARTIST | CINEMA Life! シネマライフ｜エンタメ情報](https://www.cinema-life.net/p260920_irc_matsumotomomona/)
+  - [━IDOL RUNWAY COLLECTION 2026 A/W━ 高嶺のなでしこ、ライブパフォーマンス披露 | ACTOR&ACTRESS,ARTIST | CINEMA Life! シネマライフ｜エンタメ情報](https://www.cinema-life.net/p260920_irc_takanenonadeshiko/)
+
+  ### mireia
+
+  - [松本ももな - mireia X ](https://x.com/mireia_media/status/2101517751869403422)
+  - [涼海すう - mireia X ](https://x.com/mireia_media/status/2101526017169056190)
+  - [高嶺のなでしこスペシャルライブ - mireia X ](https://x.com/mireia_media/status/2101580476171608191)
+  - [【IRC 2026 A/W】高嶺のなでしこ・松本ももな 和洋ミックスの華やかルックでランウェイ – mireia](https://mireia.style/matsumoto-momona-irc-2026/)
+  - [【IRC 2026 A/W】高嶺のなでしこ・涼海すう 鮮やかブルーのコーデでランウェイ – mireia](https://mireia.style/suzumi-suu-irc-2026/)
+  - [【IRC 2026 A/W】高嶺のなでしこ、横浜アリーナで「初恋のひと。」「ファンサ」「美しく生きろ」披露 ランウェイも使ったライブで会場沸かす – mireia](https://mireia.style/takanenonadeshiko-live-irc-2026aw/)
 
   ## リンク
 
+  - [ライブ出演報告 (セットリスト・動画あり) - 公式 X](https://x.com/takanenofficial/status/2101559483365535961)
+  - [ファッションステージ出演報告 - 公式 X](https://x.com/takanenofficial/status/2101524249794003151)
+  - [#きょうのたかねこ](https://x.com/takanenofficial/status/2101446242085847393)
   - [ファッションステージ出演告知 - 公式 X](https://x.com/takanenofficial/status/2098237594446537103)
   - [出演者メッセージ (松本ももな、籾山ひめり) - IDOL RUNWAY COLLECTION X](https://x.com/idolrunwaycolle/status/2099407803865862301)
   - [出演者メッセージ動画 - IDOL RUNWAY COLLECTION X](https://x.com/idolrunwaycolle/status/2096871098939777175)
