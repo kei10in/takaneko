9008ea41e5753246c8d150baae2d391e21b4e038
Keisuke Ijuin <kei10in@gmail.com>
2026-09-22 00:05:26 +0900

    feat: Cloudflare Image Transformation を使うようにする。

--- app/components/thumbnail-image/ThumbnailImage.tsx
+++ app/components/thumbnail-image/ThumbnailImage.tsx
@@ -1,7 +1,12 @@
+import { CloudflareThumbnailImage } from "~/components/thumbnail-image/CloudflareThumbnailImage";
 import { StaticThumbnailImage } from "~/components/thumbnail-image/StaticThumbnailImage";
 
 type Props = React.ComponentProps<typeof StaticThumbnailImage>;
 
 export const ThumbnailImage: React.FC<Props> = (props: Props) => {
-  return <StaticThumbnailImage {...props} />;
+  if (import.meta.env.DEV) {
+    return <StaticThumbnailImage {...props} />;
+  }
+
+  return <CloudflareThumbnailImage {...props} />;
 };
